45d89e3beeefd54bca73856a296eed47d9907c9d
Bernhard M. Wiedemann <bernhard@black.zq1.de>
2010-01-09 04:20:24 +0100

    add zsync part ; fix kvm call

--- startnetinst.sh
+++ startnetinst.sh
@@ -3,8 +3,9 @@ mkdir -p /vm/raid1
 for i in $(seq 1 2) ; do
 	qemu-img create /vm/raid1/$i 4G
 done
+(cd /home/bernhard/temp/ ; zsync http://www3.zq1.de/bernhard/mirror/opensuse/factory/iso/openSUSE-NET-i586-current-Media.iso.zsync)
 
-( cd /vm && qemu-kvm -m 2048 -net nic,model=e1000,macaddr=52:54:00:12:34:56 -net vde -smp 4 -drive file=raid/1,if=virtio,boot=on -drive file=raid/2,if=virtio -drive file=raid/3,if=virtio -drive file=raid/4,if=virtio -cdrom /home/bernhard/temp/openSUSE-NET-i586-Build0032-Media.iso -boot d -monitor tcp:127.0.0.1:15222,server,nowait )&
+( cd /vm && qemu-kvm -m 2048 -net nic,model=e1000,macaddr=52:54:00:12:34:56 -net vde -smp 4 -drive file=raid1/1,if=virtio,boot=on -drive file=raid1/2,if=virtio -cdrom /home/bernhard/temp/openSUSE-NET-i586-current-Media.iso -boot d -monitor tcp:127.0.0.1:15222,server,nowait )&
 sleep 8
 perl autoinst-net.pl | netcat localhost 15222
 
