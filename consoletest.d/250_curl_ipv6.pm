5a3b9babf9c9e40122daafff67dc673bbeec3086
Bernhard M. Wiedemann <bernhard@delta.zq1.de>
2010-06-05 10:51:20 +0200

    curl bug test update

--- consoletest.d/250_curl_ipv6.pm
+++ consoletest.d/250_curl_ipv6.pm
@@ -4,8 +4,10 @@ use bmwqemu;
 # test for bug https://bugzilla.novell.com/show_bug.cgi?id=598574
 sub run()
 {
+	my $self=shift;
 	script_run('curl www3.zq1.de/test.txt');
 	sleep 2;
+	$self->take_screenshot;
 	script_run('rpm -q curl libcurl4');
 	sleep 2;
 }
