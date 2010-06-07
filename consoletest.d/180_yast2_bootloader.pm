1d75ba51f4c363fb268193eb130464d418df8df9
Bernhard M. Wiedemann <bernhard@delta.zq1.de>
2010-06-07 21:10:02 +0200

    add yast2_bootloader regression test

---
+++ consoletest.d/180_yast2_bootloader.pm
@@ -0,0 +1,28 @@
+package yast2_bootloader;
+use base "basetest";
+use bmwqemu;
+# test yast2 bootloader functionality
+# https://bugzilla.novell.com/show_bug.cgi?id=610454
+
+
+sub run()
+{
+	my $self=shift;
+	script_sudo("/sbin/yast2 bootloader");
+	sleep 3;
+	$self->take_screenshot;
+	sendkey "ctrl-l";
+	script_run('echo $?');
+	$self->take_screenshot;
+	script_run('rpm -q hwinfo');
+}
+
+sub checklist()
+{
+	# return hashref:
+	return {qw(
+		1e58b9fb6cd585dfc84d1aa82e1429d7 fail
+	)}
+}
+
+1;
