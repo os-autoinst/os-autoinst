eb8577e02c2289e169811751cdd8880d86f79f76
Bernhard M. Wiedemann <bernhard@delta.zq1.de>
2010-06-08 11:34:56 +0200

    add lxde and xfce tests+shutdown

--- inst/lxde.pm
+++ inst/lxde.pm
@@ -1,21 +1,11 @@
 use bmwqemu;
 
-waitinststage "xdm-loginscreen";
+waitinststage "LXDE";
 waitidle;
 sleep 5;
 do "inst/consoletest.pm";
 
 sleep 5;
-# log in
-sendautotype $username."\n";
-sleep 1;
-sendautotype $password."\n";
-waitidle;
-sleep 3;
-
-sendkey "alt-f4"; # close hint popup
-waitidle;
-sleep 2;
 
 sendkey "alt-f2";
 waitidle;
@@ -28,9 +18,23 @@ sleep 3;
 sendkey "alt-f4";
 sleep 1;
 sendkey "ret"; # save+quit
+sendkey "alt-f4"; # close xterm
 
-
+sleep 3;
+sendkey "ctrl-alt-delete"; # does open task manager instead of reboot
 sleep 5;
+
+sendkey "alt-f2";
+waitidle;
+sleep 2;
+sendautotype "xterm\n";
+waitidle;
+script_sudo "/sbin/reboot";
+
+sleep 10;
+waitinststage "LXDE";
+sleep 10;
+
 qemusend "system_powerdown";
 
 1;
