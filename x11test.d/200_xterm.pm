75af13757d9e970fe1c6498fc28898ae298225f8
Bernhard M. Wiedemann <bernhard@delta.zq1.de>
2010-06-22 11:56:25 +0200

    finetune xterm test

--- x11test.d/200_xterm.pm
+++ x11test.d/200_xterm.pm
@@ -5,6 +5,7 @@ sub run()
 {
 	my $self=shift;
 	x11_start_program("xterm");
+	sleep 2;
 	$self->take_screenshot;
 	sendkey "alt-f4"; sleep 2;
 }
@@ -13,6 +14,7 @@ sub checklist()
 {
 	# return hashref:
 	return {qw(
+		89eb3584933cacec266886cf5bc4094c OK
 	)}
 }
 
