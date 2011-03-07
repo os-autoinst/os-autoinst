4237790e2ec4e3bdd6f9a42fe20a212714e18691
Bernhard M. Wiedemann <bernhard+osautoinst lsmod de>
2011-03-07 16:25:34 +0100

    merge openqa improvements ; cleanup

--- distri/opensuse/check.pm
+++ distri/opensuse/check.pm
@@ -5,27 +5,31 @@ use bmwqemu;
 use autotest;
 
 sub check() {
-        my @stages=(qw"splashscreen welcome timezone disk usersettings installationoverview performinstallation automaticconfiguration booted");
-        my $results=\%::results;
-        foreach my $s (@stages) {
-                my $found=0;
-                foreach(keys(%::stageseen)) {if(m/$s/){$found=1;last;}}
-                $results->{$s}=($found?"OK":"unknown");
-                ::printresult $s;
-        }
+	my %notinupgrade=qw(timezone 1 disk 1 usersettings 1 automaticconfiguration 1);
+	my %notinlive=(%notinupgrade,qw(welcome 1 installationoverview 1 performinstallation 1));
+	my @stages=(qw"splashscreen welcome timezone disk usersettings installationoverview performinstallation automaticconfiguration booted");
+	my $results=\%::results;
+	foreach my $s (@stages) {
+		next if($notinupgrade{$s} && $ENV{UPGRADE});
+		next if($notinlive{$s} && $ENV{LIVETEST});
+		my $found=0;
+		foreach(keys(%::stageseen)) {if(m/$s/){$found=1;last;}}
+		$results->{$s}=($found?"OK":"unknown");
+		::printresult $s;
+	}
 
-        autotest::runtest("$scriptdir/distri/$ENV{DISTRI}/inst.d/010_initenv.pm",sub{my $test=shift;$test->run;});
+	autotest::runtest("$scriptdir/distri/$ENV{DISTRI}/inst.d/010_initenv.pm",sub{my $test=shift;$test->run;});
 
-        autotest::runtestdir("$scriptdir/inst.d", \&::checkfunc);
-        autotest::runtestdir("$scriptdir/consoletest.d", \&::checkfunc);
-        autotest::runtestdir("$scriptdir/x11test.d", \&::checkfunc);
+	autotest::runtestdir("$scriptdir/inst.d", \&::checkfunc);
+	autotest::runtestdir("$scriptdir/consoletest.d", \&::checkfunc);
+	autotest::runtestdir("$scriptdir/x11test.d", \&::checkfunc);
 
-        my $overall=(::is_ok($results->{xterm}) or ::is_ok($results->{firefox}));
-        for my $test (qw(automaticconfiguration booted zypper_in zypper_up yast2_lan isosize)) {
-                next if($test eq "automaticconfiguration" && ($ENV{UPGRADE}||$ENV{LIVETEST}));
-                $overall=0 unless ::is_ok $results->{$test};
-        }
-        return $overall;
+	my $overall=(::is_ok($results->{xterm}) or ::is_ok($results->{firefox}));
+	for my $test (qw(automaticconfiguration booted zypper_in zypper_up yast2_lan isosize)) {
+		next if($test eq "automaticconfiguration" && ($ENV{UPGRADE}||$ENV{LIVETEST}));
+		$overall=0 unless ::is_ok $results->{$test};
+	}
+	return $overall;
 }
 
 1;
--- inst/xfce.pm
+++ inst/xfce.pm
@@ -1,47 +1,8 @@
 use bmwqemu;
 
-waitinststage "xdm-loginscreen";
 mousemove_raw(31000, 31000); # move mouse off screen again
 waitidle;
 sleep 5;
 do "inst/consoletest.pm";
-
-sleep 5;
-
-#sendkey "alt-f2";
-#waitidle;
-#sleep 2;
-#sendautotype "xterm\n";
-#waitidle;
-#script_run "firefox&";
-#sendkey "alt-f4"; # default browser setting popup
-#sleep 3;
-#sendkey "alt-f4";
-#waitidle;
-#sendkey "ret"; # save+quit
-#waitidle;
-#sendkey "alt-f4"; # close xterm
-
-
-# reboot
-waitidle;
-sendkey "alt-f4";
-waitidle;
-sendkey "tab";
-waitidle;
-sendkey "ret";
-sleep 10;
-my $ok=waitinststage "xdm-loginscreen", 80;
-if($ok) {
-	waitidle;
-
-	# log in
-	sendautotype $username."\n";
-	sleep 1;
-	sendautotype $password."\n";
-	waitinststage "XFCE", 40;
-	sleep 5;
-}
-qemusend "system_powerdown";
 
 1;
