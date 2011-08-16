9bee9502fc1d2d62e65edec1ccb268352b94bee6
Bernhard M. Wiedemann <bernhard+osautoinst lsmod de>
2011-08-16 10:44:27 +0200

    write instsys and initrd package lists to log
    this allows to better check and compare versions
    to find what might have introduced a bug
    or if the new version with a proposed fix is already included.

---
+++ distri/opensuse/inst.d/056_logpackages.pm
@@ -0,0 +1,22 @@
+#!/usr/bin/perl -w
+use strict;
+use base "basetest";
+use bmwqemu;
+
+sub is_applicable()
+{
+        my $self=shift;
+        $self->SUPER::is_applicable && !$ENV{LIVECD} && !$ENV{NICEVIDEO};
+}
+
+sub run()
+{ my $self=shift;
+	waitidle;
+	sendkey "ctrl-alt-shift-x"; sleep 3;
+	sendautotype "(cat .timestamp ; echo .packages.initrd: ; cat .packages.initrd)>/dev/$serialdev\n";
+	sendautotype "(echo .packages.root: ; cat .packages.root)>/dev/$serialdev\n";
+	waitidle;
+	sendkey "ctrl-d"; sleep 3;
+}
+
+1;
