a343b93fed0117198c1dc093ee7490f3b275e18a
Stephan Kulow <coolo@suse.de>
2013-03-27 17:19:40 +0100

    make sure we only sleep till the rest of the interval

--- inst/screenshot.pm
+++ inst/screenshot.pm
@@ -1,14 +1,19 @@
 #!/usr/bin/perl -w
 use strict;
 use warnings;
-use Time::HiRes "sleep";
+use Time::HiRes qw( sleep gettimeofday );
 use bmwqemu;
 use threads;
 
 sub screenshotsub
 {
-	while(bmwqemu::alive() && sleep($ENV{SCREENSHOTINTERVAL}||5)) {
-		bmwqemu::take_screenshot('q');
+        my $interval = $ENV{SCREENSHOTINTERVAL}||5;
+       	while(bmwqemu::alive()) {
+	  my ($s1, $ms1) = gettimeofday();
+	  bmwqemu::take_screenshot('q');
+	  my ($s2, $ms2) = gettimeofday();
+	  my $rest = $interval - ($s2*1000.+$ms2/1000.-$s1*1000.-$ms1/1000.)/1000.;
+	  sleep($rest) if ($rest > 0);
 	}
 }
 
