339ed2dc857dddecb6b8d3d9268f060f447569c8
nelg <nelg@zed.linuxsolutions.co.nz>
2013-04-05 09:26:47 +1300

    improvements

--- distri/mageia/inst.d/080_package_selection.pm
+++ distri/mageia/inst.d/080_package_selection.pm
@@ -4,14 +4,46 @@ use bmwqemu;
 
 sub run()
 {
-	waitidle(10);
-	waitstillimage(10,20);
+	waitidle(300);
+	waitstillimage(10,150);
+
+if($ENV{DESKTOP}=~/kde/) {
 	# KDE
         sendkey "tab"; # skip media check
         sendkey "tab"; # skip media check
         sendkey "tab"; # skip media check
+
+} else {
+# Custom
+	sendkey "tab";
+	sendkey "right";
+	sleep 2;
+	sendkey "right";
+	sleep 2;
+	sendkey "tab";
+	sleep 10;
+	sendkey "tab";
+	sleep 10;
+	sendkey "ret";
+	waitstillimage(15,150);
+
+# Unselect all
+	sleep 2;
+	sendkey "shift-tab";
+	sendkey "shift-tab";
+	sleep 2;
+	sendkey "ret";
+	sleep 2;
+	sendkey "tab";
+	sleep 2;
 	sendkey "ret";
 
+# minial	
+	waitstillimage(10,150);
+	sleep 2;
+	sendkey "ret";
+
+}
 
 }
 
