45303116cd3ee3900323352407e92d7a73885fb8
Ludwig Nussel <ludwig.nussel@suse.de>
2013-04-18 11:39:41 +0200

    test suite

---
+++ t/01-test_needle.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w -I..
+
+use strict;
+use Test::Simple tests => 2;
+
+use needle;
+use cv;
+
+my ($res, $needle, $img1);
+
+$img1 = tinycv::read("data/bootmenu.test.png");
+
+$needle = needle->new("data/bootmenu.ref.json");
+
+$res = $img1->search($needle);
+
+ok(defined $res, "match with exclude area");
+
+$needle = needle->new("data/bootmenu-fail.ref.json");
+$res = $img1->search($needle);
+ok(!defined $res, "no match");
