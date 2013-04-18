10c9b7f56b380a846cad2cb5e3c196aaf3eec9b0
Ludwig Nussel <ludwig.nussel@suse.de>
2013-04-18 15:19:43 +0200

    ocr using tesseract

---
+++ t/02-test_ocr.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl -w -I..
+
+use strict;
+use Test::Simple tests => 2;
+
+use needle;
+use cv;
+use ocr;
+
+my ($res, $needle, $img1);
+
+$img1 = tinycv::read("data/bootmenu.test.png");
+
+$needle = needle->new("data/bootmenu-ocr.ref.json");
+$res = $img1->search($needle);
+ok(defined $res, "ocr match 1");
+
+my $ocr;
+for my $a (@{$res->{'needle'}->{'area'}}) {
+    next unless $a->{'type'} eq 'ocr';
+
+#    my $ocr=ocr::get_ocr($img1, "-l 0",
+#	    [ $a->{'xpos'}, $a->{'ypos'}, $a->{'width'}, $a->{'height'} ]);
+
+    $ocr .= ocr::tesseract($img1, $a);
+}
+
+ok($ocr =~ /Memory Test.*Video Mode/s, "multiple OCR regions");
