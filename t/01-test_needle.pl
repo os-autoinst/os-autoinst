db4fd4a743f1056e4f2c991ff8e22670fedb9293
Alberto Planas <aplanas@suse.com>
2013-04-30 17:39:33 +0200

    Improme the matching precision.

--- t/01-test_needle.pl
+++ t/01-test_needle.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w -I..
 
 use strict;
-use Test::Simple tests => 2;
+use Test::Simple tests => 3;
 
 use needle;
 use cv;
@@ -19,3 +19,8 @@ ok(defined $res, "match with exclude area");
 $needle = needle->new("data/bootmenu-fail.ref.json");
 $res = $img1->search($needle);
 ok(!defined $res, "no match");
+
+$img1 = tinycv::read("data/welcome.test.png");
+$needle = needle->new("data/welcome.ref.json");
+$res = $img1->search($needle);
+ok(defined $res, "match with different art");
