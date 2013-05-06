e4f6e31496a9a05be5ce7f603ffd919715c12f94
Ludwig Nussel <ludwig.nussel@suse.de>
2013-05-06 16:40:47 +0200

    rewrite search results
    search() now returns a tuple of matching needle and optionally candidates when called in array context.
    'matcharea' was renamed to 'area'
    the results.json now writes a diff image of the not matched needles.

--- t/01-test_needle.pl
+++ t/01-test_needle.pl
@@ -1,12 +1,13 @@
 #!/usr/bin/perl -w -I..
 
 use strict;
-use Test::Simple tests => 3;
+use Test::Simple tests => 9;
 
 use needle;
 use cv;
+use Data::Dump;
 
-my ($res, $needle, $img1);
+my ($res, $needle, $img1, $cand);
 
 $img1 = tinycv::read("data/bootmenu.test.png");
 
@@ -16,9 +17,19 @@ $res = $img1->search($needle);
 
 ok(defined $res, "match with exclude area");
 
+($res, $cand) = $img1->search($needle);
+ok(defined $res, "match in array context");
+ok($res->{'ok'}, "match in array context ok == 1");
+ok($res->{'area'}->[-1]->{result} eq 'ok', "match in array context result == ok");
+ok(!defined $cand, "candidates must be undefined");
+
 $needle = needle->new("data/bootmenu-fail.ref.json");
 $res = $img1->search($needle);
 ok(!defined $res, "no match");
+
+($res, $cand) = $img1->search($needle);
+ok(!defined $res, "no match in array context");
+ok(defined $cand && ref $cand eq 'ARRAY', "candidates must be array");
 
 $img1 = tinycv::read("data/welcome.test.png");
 $needle = needle->new("data/welcome.ref.json");
