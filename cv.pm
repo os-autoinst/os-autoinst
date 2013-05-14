f499ae1e23ddc9940947de9c0a67c7a4047cc8a7
Ludwig Nussel <ludwig.nussel@suse.de>
2013-05-14 09:42:54 +0200

    don't auto compile
    Die and print instructions instead. Helps if called from a Makefile.

--- cv.pm
+++ cv.pm
@@ -8,26 +8,21 @@ use ExtUtils::testlib;
 
 use File::Basename;
 
-my $libdir = dirname(__FILE__);
-
-unless(-e "$libdir/ppmclibs/Makefile") {
-        $|=1;
-        print "Building C-libraries...\n";
-        system("cd $libdir/ppmclibs ; perl Makefile.PL");
-}
-
-# do not call make from make
-if (!defined $ENV{MAKELEVEL}) {
-  system("make", "-C", "$libdir/ppmclibs", "-s") == 0 || die 'make failed';
-}
-
 BEGIN {
-  my $libdir = dirname(__FILE__);
-  unshift(@INC, "$libdir/ppmclibs/blib/arch");
-  unshift(@INC, "$libdir/ppmclibs/blib/lib");
+	my $libdir = dirname(__FILE__);
+	my @s = stat("$libdir/ppmclibs/blib/lib/tinycv.pm");
+	unless(@s && -e "$libdir/ppmclibs/tinycv.pm" && $s[7] == (stat(_))[7]) {
+		$|=1;
+		print STDERR "### Please build the tinycv bindings first:\n";
+		print STDERR "cd $libdir/ppmclibs ; perl Makefile.PL\n" unless -e "$libdir/ppmclibs/Makefile";
+		print STDERR "make -C $libdir/ppmclibs\n";
+		die("tinycv outdated");
+	}
+
+	unshift(@INC, "$libdir/ppmclibs/blib/arch");
+	unshift(@INC, "$libdir/ppmclibs/blib/lib");
 }
 
 require tinycv;
-eval{ require tinycv; };
 
 1;
