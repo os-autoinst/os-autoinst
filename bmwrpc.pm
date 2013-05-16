161d8e5622f80d41c760b0b95a8477e09b12ba2d
Ludwig Nussel <ludwig.nussel@suse.de>
2013-05-16 14:14:26 +0200

    share backend among threads
    to be able to access the backend from within the rpc thread it
    needs to be shared. Since socket objects apparently are not shared
    the managment loop has to be in a separate thread with a queue.

--- bmwrpc.pm
+++ bmwrpc.pm
@@ -2,12 +2,31 @@
 
 package bmwrpc;
 
-use bmwqemu qw/$stop_waitforneedle/;
+use threads;
+use bmwqemu ();
 
 use base qw(JSON::RPC::Procedure);
 
 sub stop_waitforneedle : Public {
 	$bmwqemu::stop_waitforneedle = 1;
+}
+
+sub stop_vm : Public {
+	printf "bmwrpc stop_vm %d\n", threads->tid();
+	bmwqemu::stop_vm();
+	print "<< done\n";
+}
+
+sub cont_vm : Public {
+	bmwqemu::cont_vm();
+}
+
+sub freeze_vm : Public {
+	bmwqemu::freeze_vm();
+}
+
+sub alive : Public {
+	bmwqemu::alive();
 }
 
 sub quit : Public {
