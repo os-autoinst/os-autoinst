9340a911eeef6f29d7df1569c19cfcbb5204b990
Alberto Planas <aplanas@suse.com>
2013-06-03 16:51:34 +0200

    Update the test to set 1024x768 in UEFI mode.

--- distri/opensuse/inst.d/020_bootloader_uefi.pm
+++ distri/opensuse/inst.d/020_bootloader_uefi.pm
@@ -47,20 +47,23 @@ if(!$ENV{LIVETEST}) {
 
 # 1024x768
 if($ENV{RES1024}) { # default is 800x600
-        sendautotype("video=1280x1024-16 ");
-} elsif($ENV{VIDEOMODE} eq "text") {
+        sendautotype("video=1024x768-16 ");
+} elsif(checkEnv('VIDEOMODE', "text")) {
 	#sendkey "f3";
 	#for(1..2) {
 	#	sendkey "up";
 	#}
 	#sendkey "ret";
-} else {
-    sleep 10;
-    sendautotype("video=800x600-16 ");
 }
 
 #sendautotype("nohz=off "); # NOHZ caused errors with 2.6.26
 #sendautotype("nomodeset "); # coolo said, 12.3-MS0 kernel/kms broken with cirrus/vesa #fixed 2012-11-06
+
+# https://wiki.archlinux.org/index.php/Kernel_Mode_Setting#Forcing_modes_and_EDID
+sendautotype("vga=791 ");
+sendautotype("video=1024x768-16 ");
+sendautotype("drm_kms_helper.edid_firmware=edid/1024x768.bin ");
+waitforneedle("inst-video-typed-grub2", 13);
 if(!$ENV{NICEVIDEO}) {
 	sleep 15; sendautotype("console=ttyS0 "); # to get crash dumps as text
 	sleep 15; sendautotype("console=tty "); # to get crash dumps as text
