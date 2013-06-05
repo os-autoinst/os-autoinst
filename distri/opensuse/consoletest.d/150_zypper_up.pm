5aa1beb08ef4c87c58d4a8bd6e746353cd4b9168
Alberto Planas <aplanas@suse.com>
2013-06-05 12:00:13 +0200

    Install killall package

--- distri/opensuse/consoletest.d/150_zypper_up.pm
+++ distri/opensuse/consoletest.d/150_zypper_up.pm
@@ -3,6 +3,10 @@ use bmwqemu;
 sub run()
 {
 	my $self=shift;
+
+	# Killall is used here, make sure that is installed
+	script_sudo("zypper -n -q in psmisc");
+
 	script_sudo("killall gpk-update-icon kpackagekitsmarticon packagekitd");
 	if(!$ENV{NET} && !$ENV{TUMBLEWEED} && !$ENV{EVERGREEN} && $ENV{SUSEMIRROR}) {
 		# non-NET installs have only milestone repo, which might be incompatible.
