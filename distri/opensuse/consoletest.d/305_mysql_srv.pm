f93af56537da67492e5a3c7657b78bfdc56c2b0a
Alberto Planas <aplanas@suse.com>
2013-06-05 18:09:51 +0200

    Add two server tests.

---
+++ distri/opensuse/consoletest.d/305_mysql_srv.pm
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w
+use strict;
+use base "serverstep";
+use bmwqemu;
+
+
+sub run()
+{
+    my $self=shift;
+
+    # Install apache2
+    script_sudo("zypper -n -q in mysql");
+    waitidle(10);
+
+    # After installation, mysql is disabled
+    script_sudo("systemctl status mysql.service | tee /dev/ttyS0 -");
+    waitidle(5);
+    die unless waitserial(".*inactive.*", 2);
+
+    # Now must be enabled
+    script_sudo("systemctl start mysql.service");
+    script_sudo("systemctl status mysql.service | tee /dev/ttyS0 -");
+    waitidle(5);
+    die if waitserial(".*Syntax error.*", 2);    
+
+    $self->take_screenshot;
+}
+
+1;
