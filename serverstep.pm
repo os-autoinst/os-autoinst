a70c00ac681a25dd3d9842c680ebe0064b9bb3b6
Alberto Planas <aplanas@suse.com>
2013-06-05 18:09:51 +0200

    Add two server tests.

---
+++ serverstep.pm
@@ -0,0 +1,11 @@
+package serverstep;
+use base "basetest";
+
+# Use this class for server tests
+sub is_applicable()
+{
+    my $self=shift;
+    return $self->SUPER::is_applicable && !$ENV{NOINSTALL} && !$ENV{LIVETEST} && ($ENV{DESKTOP} eq "textmode");
+}
+
+1;
