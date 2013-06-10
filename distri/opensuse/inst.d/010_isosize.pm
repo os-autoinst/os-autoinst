e5b2f83e92e5b18a5e5a602b793740e200ef01a4
Ludwig Nussel <ludwig.nussel@suse.de>
2013-06-10 18:01:37 +0200

    create separate iso_size test

---
+++ distri/opensuse/inst.d/010_isosize.pm
@@ -0,0 +1,27 @@
+use base "basetest";
+use strict;
+use bmwqemu;
+
+sub is_applicable()
+{
+	return $ENV{ISO_MAXSIZE};
+}
+
+sub run
+{
+	my $self = shift;
+	my $iso = $ENV{ISO};
+	my $size = -s $iso;
+	diag("iso_size=$size");
+	my $result = 'ok';
+	if( $size > $ENV{ISO_MAXSIZE}) {
+		$result = 'fail';
+	}
+	$self->result($result);
+}
+
+sub test_class($) {
+	return basetest::FATAL_TEST;
+}
+
+1;
