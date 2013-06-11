b52ec0314909a33c525a75ef0d221cb5a779dbe7
Ludwig Nussel <ludwig.nussel@suse.de>
2013-06-11 14:38:11 +0200

    refactor usb install a bit

--- inst/startqemu.pm
+++ inst/startqemu.pm
@@ -25,7 +25,6 @@ $ENV{QEMUVGA}||="cirrus";
 $ENV{QEMUCPUS}||=1;
 $ENV{NUMDISKS}||=2;
 if(defined($ENV{RAIDLEVEL})) {$ENV{NUMDISKS}=4}
-my @cdrom=("-cdrom", $iso);
 
 $ENV{QEMU_AUDIO_DRV}="wav";
 $ENV{QEMU_WAV_PATH}="/dev/null";
@@ -62,13 +61,6 @@ if(!$ENV{KEEPHDDS} && !$ENV{SKIPTO}) {
 			symlink($i,"$basedir/l$i");
 		}
 	}
-	if($ENV{USBBOOT}) {
-	    $ENV{NUMDISKS}=2;
-	    # system("dd", "if=$iso", "of=$basedir/l1", "bs=1M", "conv=notrunc");
-	    unlink "$basedir/l1";
-	    symlink($iso, "$basedir/l1");
-	    @cdrom=();
-	}
 }
 if($ENV{AUTO_INST}) {
 	unlink("$basedir/autoinst.img");
@@ -92,18 +84,19 @@ if($self->{'pid'}==0) {
 	    }
 	}
 
-	if ($iso && @cdrom) {
-	    push(@params, @cdrom);
-	} elsif ($iso && $ENV{USBBOOT}) {
-	    push(@params, "-drive", "if=none,id=usbstick,file=$basedir/l1,snapshot=on");
-	    push(@params, "-device", "usb-ehci,id=ehci");
-	    push(@params, "-device", "usb-storage,bus=ehci.0,drive=usbstick,bootindex=1");
-	    push(@params, "-device", "piix3-usb-uhci,id=usb");
-	    push(@params, "-device", "usb-tablet,bus=usb.0");
+	if ($iso) {
+	    if ($ENV{USBBOOT}) {
+		push(@params, "-drive", "if=none,id=usbstick,file=$iso,snapshot=on");
+		push(@params, "-device", "usb-ehci,id=ehci");
+		push(@params, "-device", "usb-storage,bus=ehci.0,drive=usbstick,bootindex=1");
+		push(@params, "-device", "piix3-usb-uhci,id=usb");
+		push(@params, "-device", "usb-tablet,bus=usb.0");
+	    } else {
+		push(@params, "-cdrom", $iso);
+	    }
 	}
 
-	my $start_disk = ($ENV{USBBOOT} ? 2 : 1);
-	for my $i ($start_disk..$ENV{NUMDISKS}) {
+	for my $i (1..$ENV{NUMDISKS}) {
 		my $boot="";#$i==1?",boot=on":""; # workaround bnc#696890
 		push(@params, "-drive", "file=$basedir/l$i,cache=unsafe,if=$ENV{HDDMODEL}$boot");
 	}
