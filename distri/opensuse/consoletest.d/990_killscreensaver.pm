ec54178b8428cb09d20d4e90cfc406cff1d3d6e2
Ludwig Nussel <ludwig.nussel@suse.de>
2013-06-14 10:57:47 +0200

    disable killscreensaver
    not needed for gnome anymore

--- distri/opensuse/consoletest.d/990_killscreensaver.pm
+++ distri/opensuse/consoletest.d/990_killscreensaver.pm
@@ -1,9 +1,12 @@
+# should be no longer needed as consoletest_finish tests for screensaver
+
 use base "basetest";
 use bmwqemu;
 
 sub is_applicable()
 {
-	return ($ENV{DESKTOP} eq "gnome");
+#	return ($ENV{DESKTOP} eq "gnome");
+	return 0;
 }
 
 sub run()
