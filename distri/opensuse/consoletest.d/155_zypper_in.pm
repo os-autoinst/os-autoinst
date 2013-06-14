1cad9e259205c13bd359afbbfcf7cc14ca2cafbc
Ludwig Nussel <ludwig.nussel@suse.de>
2013-06-14 10:03:42 +0200

    adjust some tests

--- distri/opensuse/consoletest.d/155_zypper_in.pm
+++ distri/opensuse/consoletest.d/155_zypper_in.pm
@@ -3,17 +3,19 @@ use bmwqemu;
 sub run()
 {
 	my $self=shift;
+	become_root();
 	script_run("zypper lr -d > /dev/$serialdev");
-	script_sudo("killall gpk-update-icon kpackagekitsmarticon packagekitd");
-	script_sudo("zypper ar http://download.opensuse.org/repositories/Cloud:/EC2/openSUSE_Factory/Cloud:EC2.repo"); # for suse-ami-tools
-	script_sudo("zypper --gpg-auto-import-keys -n in screen rsync gvim suse-ami-tools");
+	script_run("killall gpk-update-icon kpackagekitsmarticon packagekitd");
+	#script_run("zypper ar http://download.opensuse.org/repositories/Cloud:/EC2/openSUSE_Factory/Cloud:EC2.repo"); # for suse-ami-tools
+	script_run("zypper --gpg-auto-import-keys -n in screen rsync gvim suse-ami-tools");
 	waitstillimage(12,90);
 	script_run('echo $?');
 	$self->check_screen;
 	sendkey "ctrl-l"; # clear screen to see that second update does not do any more
 	my $pkgname=(($ENV{DISTRI}=~/fedora/)?"vim-X11":"gvim");
-	script_sudo("rpm -e $pkgname");
+	script_run("rpm -e $pkgname");
 	script_run("rpm -q $pkgname");
+	script_run('exit');
 	$self->check_screen;
 }
 
--- distri/opensuse/consoletest.d/190_sshd.pm
+++ distri/opensuse/consoletest.d/190_sshd.pm
@@ -4,15 +4,14 @@ use bmwqemu;
 sub run()
 {
 	my $self=shift;
-	script_sudo('/sbin/insserv -r SuSEfirewall2_setup'); # disable firewall to make better cloud images
-	script_sudo('/sbin/insserv -r SuSEfirewall2_init');
-	script_sudo('systemctl disable SuSEfirewall2');
-	script_sudo('/sbin/chkconfig -a sshd');
-	script_sudo('/etc/init.d/sshd restart'); # will do nothing if it is already running
+	become_root();
+	script_run('SuSEfirewall2 off');
+	script_run('chkconfig sshd on');
+	script_run('rcsshd restart'); # will do nothing if it is already running
 	$self->check_screen;
 	sendkey("ctrl-l");
 	script_run('echo $?');
-	script_sudo('/etc/init.d/sshd status');
+	script_sudo('rcsshd status');
 	$self->check_screen;
 }
 
