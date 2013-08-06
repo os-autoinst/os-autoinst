b045ec62a8fe5c51639add0fbf0542aa945c069b
Ludwig Nussel <ludwig.nussel@suse.de>
2013-08-06 08:31:34 +0200

    dump qemu command line as script

--- inst/startqemu.pm
+++ inst/startqemu.pm
@@ -71,7 +71,13 @@ for my $i (1..4) { # create missing symlinks
 $self->{'pid'}=fork();
 die "fork failed" if(!defined($self->{'pid'}));
 if($self->{'pid'}==0) {
-	my @params=($qemubin, qw(-m 1024 -net user -qmp), "unix:qmp_socket,server,nowait", "-monitor", "unix:hmp_socket,server,nowait", "-net", "nic,model=$ENV{NICMODEL},macaddr=52:54:00:12:34:56", "-serial", "file:serial0", "-soundhw", "ac97", "-vga", $ENV{QEMUVGA}, "-S");
+	my @params=('-m', '1024',
+	       '-net', 'user', "-net", "nic,model=$ENV{NICMODEL},macaddr=52:54:00:12:34:56",
+	       "-serial", "file:serial0",
+	       "-soundhw", "ac97",
+	       "-vga", $ENV{QEMUVGA}
+       );
+
 	if ($ENV{LAPTOP}) {
 	    for my $f (<$ENV{LAPTOP}/*.bin>) {
 		push @params, '-smbios', "file=$f";
@@ -96,19 +102,34 @@ if($self->{'pid'}==0) {
 
 	push(@params, "-boot", "once=d,menu=on,splash-time=5000");
 
-	if($ENV{VNC}) {
-		if($ENV{VNC}!~/:/) {$ENV{VNC}=":$ENV{VNC}"}
-		push(@params, "-vnc", $ENV{VNC});
-		push(@params, "-k", $ENV{VNCKB}) if($ENV{VNCKB});
-	}
 	if($ENV{QEMUCPU}) { push(@params, "-cpu", $ENV{QEMUCPU}); }
 	if($ENV{UEFI}) { push(@params, "-L", $ENV{UEFI_BIOS_DIR}); }
 	if($ENV{MULTINET}) {push(@params, qw"-net nic,vlan=1,model=virtio,macaddr=52:54:00:12:34:57 -net none,vlan=1")}
 	push(@params, "-usb", "-usbdevice", "tablet");
 	push(@params, "-smp", $ENV{QEMUCPUS});
 	push(@params, "-enable-kvm");
-	if(-e "/usr/bin/eatmydata") { unshift(@params, "/usr/bin/eatmydata") }
+
+	if (open(my $cmdfd, '>', 'runqemu')) {
+		print $cmdfd "#!/bin/bash";
+		my @args = map { s,\\,\\\\,g; s,\$,\\\$,g; s,\",\\\",g; s,\`,\\\`,g; "\"$_\"" } @params;
+		printf $cmdfd "%s \\\n  %s\n", $qemubin, join(" \\\n  ", @args);
+		close $cmdfd;
+		chmod 0755, 'runqemu';
+	}
+
+	if($ENV{VNC}) {
+		if($ENV{VNC}!~/:/) {$ENV{VNC}=":$ENV{VNC}"}
+		push(@params, "-vnc", $ENV{VNC});
+		push(@params, "-k", $ENV{VNCKB}) if($ENV{VNCKB});
+	}
+
+	push @params, '-qmp',  "unix:qmp_socket,server,nowait", "-monitor", "unix:hmp_socket,server,nowait", "-S";
+
+	unshift(@params, $qemubin);
+	unshift(@params, "/usr/bin/eatmydata") if (-e "/usr/bin/eatmydata");
+
 	bmwqemu::diag("starting: ".join(" ", @params));
+
 	exec(@params);
 	die "exec $qemubin failed";
 }
