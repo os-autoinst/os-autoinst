59f04716aa17e7eaabcca3ec8433bebf3af28a79
Stephan Kulow <coolo@suse.de>
2013-09-18 11:17:47 +0200

    gvim is troublesom, use xdelta as test package

--- distri/opensuse/consoletest.d/157_yast2_i.pm
+++ distri/opensuse/consoletest.d/157_yast2_i.pm
@@ -5,7 +5,7 @@ sub run()
 	my $self=shift;
 	script_sudo("/sbin/yast2 -i");
 	waitstillimage(16,60);
-	sendautotype("gvim\n");sleep 3;
+	sendautotype("xdelta\n");sleep 3;
 	sendkey "spc";   # select for install
 	sleep 1;
 	$self->check_screen;
@@ -16,10 +16,10 @@ sub run()
 	script_run('echo $?');
 	$self->check_screen;
 	sendkey "ctrl-l"; # clear screen to see that second update does not do any more
-	script_sudo("rpm -e  gvim"); # extra space to have different result images than for zypper_in test
-	script_run("rpm -q gvim");
+	script_sudo("rpm -e  xdelta"); # extra space to have different result images than for zypper_in test
+	script_run("rpm -q xdelta");
 	# make sure we go out of here
-	waitforneedle('test-yast2_i-gvim-not-installed', 1);
+	waitforneedle('test-yast2_i-xdelta-not-installed', 1);
 }
 
 1;
