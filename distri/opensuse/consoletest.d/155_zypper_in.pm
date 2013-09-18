065c337cbe0d2c9fa181ba8dbd1f7f6e44aaf2c9
Stephan Kulow <coolo@suse.de>
2013-09-18 11:17:47 +0200

    gvim is troublesom, use xdelta as test package

--- distri/opensuse/consoletest.d/155_zypper_in.pm
+++ distri/opensuse/consoletest.d/155_zypper_in.pm
@@ -5,14 +5,14 @@ sub run()
 	my $self=shift;
 	become_root();
 	script_run("zypper lr -d > /dev/$serialdev");
-	script_run("killall gpk-update-icon kpackagekitsmarticon packagekitd");
+	script_run("killall gpk-update-icon packagekitd");
 	#script_run("zypper ar http://download.opensuse.org/repositories/Cloud:/EC2/openSUSE_Factory/Cloud:EC2.repo"); # for suse-ami-tools
-	script_run("zypper --gpg-auto-import-keys -n in screen rsync gvim suse-ami-tools");
+	script_run("zypper --gpg-auto-import-keys -n in screen rsync xdelta suse-ami-tools");
 	waitstillimage(12,90);
 	script_run('echo $?');
 	$self->check_screen;
 	sendkey "ctrl-l"; # clear screen to see that second update does not do any more
-	my $pkgname=(($ENV{DISTRI}=~/fedora/)?"vim-X11":"gvim");
+	my $pkgname="xdelta";
 	script_run("rpm -e $pkgname");
 	script_run("rpm -q $pkgname");
 	script_run('exit');
