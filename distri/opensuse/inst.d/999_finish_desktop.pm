72ec1d6c4e68c54f4ddd1421bd922dfe842d450e
Ludwig Nussel <ludwig.nussel@suse.de>
2013-10-02 14:11:28 +0200

    apply finish_desktop only for live test

--- distri/opensuse/inst.d/999_finish_desktop.pm
+++ distri/opensuse/inst.d/999_finish_desktop.pm
@@ -1,11 +1,25 @@
 use base "basetest";
 use bmwqemu;
 
+# using this as base class means only run when an install is needed
+sub is_applicable()
+{
+	my $self=shift;
+	return $self->SUPER::is_applicable && $ENV{LIVETEST};
+}
+
 sub run()
 {
 	my $self = shift;
 
 	waitforneedle("desktop-at-first-boot", 60);
 }
+
+sub test_flags() {
+	return { 'fatal' => 1, 'important' => 1 };
+}
+
+
+
 
 1;
