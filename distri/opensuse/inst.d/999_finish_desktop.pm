bb0a053c54e0d534fd9fa7743d423c4e82ed6293
Ludwig Nussel <ludwig.nussel@suse.de>
2013-10-09 14:30:48 +0200

    fix displaymanager login on rescue cd

--- distri/opensuse/inst.d/999_finish_desktop.pm
+++ distri/opensuse/inst.d/999_finish_desktop.pm
@@ -13,7 +13,19 @@ sub run()
 	my $self = shift;
 
 	# live may take ages to boot
-	waitforneedle("desktop-at-first-boot", 300);
+	my $timeout = 300;
+	if ($ENV{'RESCUECD'}) {
+		waitforneedle('displaymanager', $timeout);
+		sendkey("tab");
+		sleep 2;
+		sendkey("tab");
+		sleep 2;
+		sendkey("tab");
+		sleep 2;
+		sendkey("ret");
+		$timeout = 60;
+	}
+	waitforneedle("desktop-at-first-boot", $timeout);
 
 	## duplicated from second stage, combine!
 	if (checkEnv('DESKTOP', 'kde')) {
