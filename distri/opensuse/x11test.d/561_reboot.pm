8ac4b495a0ed05c8857e97480684866c9c37f5c5
Christopher Hofmann <cwh@webeve.de>
2013-10-10 16:32:10 +0200

    Merged x11test reboot into one single test

--- distri/opensuse/x11test.d/561_reboot.pm
+++ distri/opensuse/x11test.d/561_reboot.pm
@@ -9,6 +9,54 @@ sub is_applicable()
 sub run()
 {
 	my $self=shift;
+
+        # 550_reboot_kde
+	if( $ENV{DESKTOP} eq "kde" ) {
+            waitidle;
+            sendkey "ctrl-alt-delete"; # reboot
+            waitidle(15);
+            sendautotype "\t\t";
+            sleep 1;
+            $self->check_screen;
+            sendautotype "\n";
+        }
+
+        # 550_reboot_gnome
+	if( $ENV{DESKTOP} eq "gnome" ) {
+            waitidle;
+            if($ENV{GNOME2}) {
+		sendkey "ctrl-alt-delete"; # reboot
+		sleep 2;
+		sendkey "down"; # reboot
+		sleep 2;
+		sendkey "ret"; # confirm 
+		sleep 2;
+		sendpassword;
+		sendkey "ret";
+            } else {
+		sendkey "ctrl-alt-f4"; sleep 2; # goto console so that gnome does not catch CAD
+		sendkey "ctrl-alt-delete"; # reboot
+            }
+        }
+        
+        # 550_reboot_xfce
+	if( $ENV{DESKTOP} eq "xfce" ) {
+            waitidle;
+            sendkey "alt-f4"; # open popup
+            waitidle;
+            sendkey "tab"; # reboot
+            waitidle;
+            sendkey "ret"; # confirm 
+        }
+
+        # 550_reboot_lxde
+	if( $ENV{DESKTOP} eq "lxde" ) {
+            waitidle;
+            #sendkey "ctrl-alt-delete"; # does open task manager instead of reboot
+            x11_start_program("xterm");
+            script_sudo "/sbin/reboot",0;
+        }
+
 	waitforneedle( "bootloader", 100); # wait until reboot
 	if ($ENV{ENCRYPT}) {
 	  wait_encrypt_prompt;
@@ -16,9 +64,30 @@ sub run()
 
 	waitinststage "reboot-after-installation", 150; # wait until booted again
 	mouse_hide(1);
+
+        # 580_kde_reboot_plasmatheme
+	if( $ENV{DESKTOP} eq "kde" ) {
+            waitinststage "KDE", 20; # wait until reboot is finished
+        }
+
+        # 570_xfce_login_after_reboot
+	if( $ENV{NOAUTOLOGIN} || $ENV{XDMUSED} ) {
+            my $ok=waitinststage "dm-loginscreen", 80;
+            if($ok) {
+		waitidle;
+
+		# log in
+		sendautotype $username."\n";
+		sleep 1;
+		sendautotype $password."\n";
+		waitinststage "XFCE", 40;
+		sleep 5;
+            }
+        }
 }
 
 sub test_flags() {
         return {'milestone' => 1, 'fatal' => 1};
 }
 1;
+
