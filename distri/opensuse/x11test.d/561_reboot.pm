67867339d670ea82e06ab421fd8b428d0c18c66d
Christopher Hofmann <cwh@webeve.de>
2013-10-11 14:45:07 +0200

    Reboottest improvement

--- distri/opensuse/x11test.d/561_reboot.pm
+++ distri/opensuse/x11test.d/561_reboot.pm
@@ -11,41 +11,24 @@ sub run()
 	my $self=shift;
 
         # 550_reboot_kde
-	if( $ENV{DESKTOP} eq "kde" ) {
+	if( $ENV{DESKTOP} eq "kde" || $ENV{DESKTOP} eq "gnome" ) {
             waitidle;
             sendkey "ctrl-alt-delete"; # reboot
-            waitidle(15);
+            waitforneedle 'logoutdialog', 15;
             sendautotype "\t\t";
             sleep 1;
-            $self->check_screen;
             sendautotype "\n";
         }
 
-        # 550_reboot_gnome
-	if( $ENV{DESKTOP} eq "gnome" ) {
-            waitidle;
-            if($ENV{GNOME2}) {
-		sendkey "ctrl-alt-delete"; # reboot
-		sleep 2;
-		sendkey "down"; # reboot
-		sleep 2;
-		sendkey "ret"; # confirm 
-		sleep 2;
-		sendpassword;
-		sendkey "ret";
-            } else {
-		sendkey "ctrl-alt-f4"; sleep 2; # goto console so that gnome does not catch CAD
-		sendkey "ctrl-alt-delete"; # reboot
-            }
-        }
-        
         # 550_reboot_xfce
 	if( $ENV{DESKTOP} eq "xfce" ) {
-            waitidle;
-            sendkey "alt-f4"; # open popup
-            waitidle;
+            sendkey "ctrl-alt-delete"; # reboot
+            waitforneedle 'logoutdialog', 15;
+            #waitidle;
+            #sendkey "alt-f4"; # open popup
+            #waitidle;
             sendkey "tab"; # reboot
-            waitidle;
+            sleep 1;
             sendkey "ret"; # confirm 
         }
 
@@ -62,28 +45,19 @@ sub run()
 	  wait_encrypt_prompt;
 	}
 
-	waitinststage "reboot-after-installation", 150; # wait until booted again
-	mouse_hide(1);
-
-        # 580_kde_reboot_plasmatheme
-	if( $ENV{DESKTOP} eq "kde" ) {
-            waitinststage "KDE", 20; # wait until reboot is finished
-        }
-
         # 570_xfce_login_after_reboot
 	if( $ENV{NOAUTOLOGIN} || $ENV{XDMUSED} ) {
-            my $ok=waitinststage "dm-loginscreen", 80;
-            if($ok) {
-		waitidle;
+            waitforneedle('displaymanager', 200);
+            waitidle;
 
-		# log in
-		sendautotype $username."\n";
-		sleep 1;
-		sendautotype $password."\n";
-		waitinststage "XFCE", 40;
-		sleep 5;
-            }
+            # log in
+            sendautotype $username."\n";
+            sleep 1;
+            sendautotype $password."\n";
         }
+
+        waitforneedle 'test-consoletest_finish-1', 300;
+	mouse_hide(1);
 }
 
 sub test_flags() {
