c90f08368d6f933940181a10eb00978860e2c7a7
Max Lin <mlin@suse.com>
2013-10-23 14:25:39 +0800

    gnome miss gnucash-docs when want to open Help

--- distri/opensuse/x11test.d/840_gnucash.pm
+++ distri/opensuse/x11test.d/840_gnucash.pm
@@ -5,6 +5,7 @@ sub run()
 {
   my $self=shift;
   ensure_installed("gnucash");
+  ensure_installed("gnucash-docs");
   x11_start_program("gnucash");
   $self->check_screen;
   sendkey "alt-o"; # open new user tutorial
@@ -12,6 +13,7 @@ sub run()
   sendkey "alt-o";
   sendkey "spc";
   sendkey "f1"; # open Help
+  sleep 2;
   $self->check_screen;
   sendkey "alt-f4"; # Exit
 }
