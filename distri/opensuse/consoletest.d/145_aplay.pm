438ab5b77872acbd91dde78f550dc4e84cce32bc
Michal Hrusecky <Michal@Hrusecky.net>
2013-10-24 08:54:30 +0000

    Fix aplay test
    Make sure that we are not muted and we have some reasonable volume levels
    before trying to play anything and check for volume levels afterwards.

--- distri/opensuse/consoletest.d/145_aplay.pm
+++ distri/opensuse/consoletest.d/145_aplay.pm
@@ -4,14 +4,22 @@ use bmwqemu;
 sub run()
 {
 	my $self=shift;
-	script_sudo("zypper -n in alsa-utils");
-	script_run('cd /tmp;wget openqa.opensuse.org/opensuse/audio/bar.wav');
+	script_sudo("zypper -n in alsa-utils; echo aplay_installed > /dev/$serialdev");
+	waitserial('aplay_installed', 600);
+	script_run("cd /tmp;wget openqa.opensuse.org/opensuse/audio/bar.wav");
 	$self->check_screen;
+	script_run('clear');
+	script_run('set_default_volume -f');
 	$self->start_audiocapture;
 	script_run("aplay bar.wav ; echo aplay_finished > /dev/$serialdev");
-	$self->take_screenshot;
 	waitserial('aplay_finished');
+	$self->take_screenshot;
 	$self->check_DTMF('123A456B789C*0#D');
+	script_run('alsamixer');
+	sleep 1;
+	$self->check_screen;
+	$self->sendkey('esc');
+	$self->sendkey('esc');
 }
 
 1;
