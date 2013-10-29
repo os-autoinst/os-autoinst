fe56ff24788d517772cbec2e68b416cca420de2a
Ludwig Nussel <ludwig.nussel@suse.de>
2013-10-29 13:08:24 +0100

    fix wrong function calls in kontact test

--- distri/opensuse/x11test.d/520_kontact.pm
+++ distri/opensuse/x11test.d/520_kontact.pm
@@ -10,11 +10,11 @@ sub run()
 {
 	my $self=shift;
 	x11_start_program("kontact");
-	wait_for_needle("kontact-assistant", 20);
+	waitforneedle("kontact-assistant", 20);
 	sendkey "alt-f4"; 
-	wait_for_needle("test-kontact-1", 3); # tips window
+	waitforneedle("test-kontact-1", 3); # tips window
 	sendkey "alt-f4";
-	wait_for_needle("kontact-window", 3);
+	waitforneedle("kontact-window", 3);
 	sendkey "alt-f4";
 }
 
