9b9a801bfec859e7eb5a042cf966eac359de6cba
Michal Hrusecky <Michal@Hrusecky.net>
2013-10-31 15:10:49 +0100

    Simplification of glibc test

--- distri/opensuse/consoletest.d/146_glibc_i686.pm
+++ distri/opensuse/consoletest.d/146_glibc_i686.pm
@@ -1,31 +1,13 @@
 use base "basetest";
 use bmwqemu;
 
+# this part contains the steps to run this test
 sub run()
 {
-    my $self=shift;
-    if($ENV{ISO}=~m/i586/) {
-	script_sudo("zypper ref; echo zypper_ref_finished > /dev/$serialdev");
-	waitserial('zypper_ref_finished');
+	my $self=shift;
 	script_run("clear");
-	script_run("uname -m");
-	$my_script = "echo 'Checking i686 glibc...';\\\n";
-	$my_script.= 'if [ "`uname -m | grep i686`" ]; then' . "\\\n";
-	$my_script.= '   if [ "`rpm -q glibc    | grep i686`" ]  &&  [ "`zypper se -s glibc | grep i686`" ]; then' . "\\\n";
-	$my_script.= '      echo "Looks ok"' . ";\\\n";
-	$my_script.= "   else \\\n";
-	$my_script.= '      echo "rpm""' . ";\\\n";
-	$my_script.= "      rpm -q glibc       | grep i686;\\\n";
-	$my_script.= '      echo "zypper""' . ";\\\n";
-	$my_script.= "      zypper se -s glibc | grep i686;\\\n";
-	$my_script.= "   fi; \\\n";
-	$my_script.= "else \\\n";
-	$my_script.= '   [ -z "`zypper se -s glibc | grep i686`" ]  || echo "Looks ok"' . ";\\\n";
-	$my_script.= "fi; \\\n echo glibc_finished > /dev/$serialdev";
-	script_run($my_script);
-	waitserial('glibc_finished');
+	script_run("/lib/libc.so.*");
 	$self->check_screen;
-    }
 }
 
 1;
