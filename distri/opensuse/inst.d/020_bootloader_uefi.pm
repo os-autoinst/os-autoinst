786a87d1b33abfbee8a6fc18c7d0768a1671e3de
Max Lin <mlin@suse.com>
2013-11-06 16:19:20 +0800

    fix promo uefi bootloader slection and considers if its OSP edition

--- distri/opensuse/inst.d/020_bootloader_uefi.pm
+++ distri/opensuse/inst.d/020_bootloader_uefi.pm
@@ -34,9 +34,11 @@ sub run()
 	}
 	if ($ENV{PROMO}) {
 		if(checkEnv("DESKTOP", "gnome")) {
+			sendkey "down" unless $ENV{OSP_SPECIAL};
 			sendkey "down";
 		} elsif(checkEnv("DESKTOP", "kde")) {
-			# KDE is first entry
+			# KDE is first entry for OSP image
+			sendkey "down" unless $ENV{OSP_SPECIAL};
 		} else {
 			die "unsupported desktop $ENV{DESKTOP}\n";
 		}
