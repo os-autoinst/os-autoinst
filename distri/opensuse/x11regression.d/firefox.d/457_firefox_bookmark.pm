22fcdc68ba07adf11adf30d2cee223b57a03a4f6
xjin <xjin@suse.com>
2013-11-13 17:30:36 +0800

    case improvement for pull request

--- distri/opensuse/x11regression.d/firefox.d/457_firefox_bookmark.pm
+++ distri/opensuse/x11regression.d/firefox.d/457_firefox_bookmark.pm
@@ -6,6 +6,10 @@ sub run()
 {
   my $self=shift;
   mouse_hide(1);
+
+# to clear all of previous settings and then open the app
+  x11_start_program("rm -rf .mozilla");
+  x11_start_program("pkill -9 firefox");
   x11_start_program("firefox"); sleep 10;
 
 # first confirm www.baidu.com has not been bookmarked yet.
@@ -15,17 +19,17 @@ sub run()
   sendautotype "www.baidu.com";
   sendkey "ret"; sleep 3;
 
-  waitforneedle("not-bookmark-yet",2);
+  checkneedle("bookmark-not-yet",2);
   sendkey "alt-f4";
 
 # bookmark the page
   sendkey "ctrl-l";
   sendautotype "www.baidu.com"; sleep 1;
   sendkey "ret"; sleep 6;
-  waitforneedle("mainpage-baidu",3);
+  checkneedle("bookmark-baidu-main",3);
 
   sendkey "ctrl-d"; sleep 2;
-  waitforneedle("bookmarking",3);
+  checkneedle("bookmarking",3);
   sendkey "ret"; sleep 2;
 
 # check all bookmarked page and open baidu mainpage in a new tab
@@ -33,10 +37,10 @@ sub run()
   sendkey "ctrl-shift-o"; sleep 1;
 
 ## check toolbar menu and unsorted section displayed; and baidu mainpage in menu section
-  waitforneedle("all-bookmark-menu",3);
+  checkneedle("bookmark-all-bookmark-menu",3);
   sendkey "down"; sleep 1;
   sendkey "ret";
-  waitforneedle("baidu-under-bookmark-menu",3);
+  checkneedle("bookmark-baidu-under-bookmark-menu",3);
 
 ## open baidu page
   sendkey "tab";
@@ -49,12 +53,12 @@ sub run()
   sendkey "tab";
   sendkey "ret"; sleep 2;
 
-  waitforneedle("mainpage-baidu",2);
+  checkneedle("bookmark-baidu-main",2);
 
 # close the bookmark lib page and then close firefox
   sendkey "alt-tab"; sleep 2;
   sendkey "alt-f4"; sleep 5;
-  waitforneedle("bookmark-closed-after",3);
+  checkneedle("bookmark-menu-closed",3);
 
 ## close firefox
   sendkey "alt-f4"; sleep 1;
