cccf783e32e81d401af373f71b3593249a90e2f9
Glen Ogilvie <nelg@mageia.org>
2013-12-28 15:08:35 +1300

    improvements

--- distri/mageia-4/inst.d/090_setup_users.pm
+++ distri/mageia-4/inst.d/090_setup_users.pm
@@ -7,17 +7,30 @@ sub run()
 	waitidle(2000);
 	waitstillimage(60,3600);
 	mouse_hide();
-	waitinststage ("mageia4-setupusers1",100);
+	my $pic = 1;
+	if (waitinststage('mageia4-setupusers-nopic',10)) {
+	$pic = 0;
+	$ENV{DESKTOP} = 'none';
+	print "No user picture, as no desktop installed";
+	} elsif (waitinststage('mageia4-setupusers-pic',10) || waitinststage('mageia4-setupusers-pic1',10)) {
+	print "user picture is avilable";
+	$pic = 2;
+	} else {
+	print "Sorry, can't recoginse user input stage";
+	die ("no matching user input stage identified, please update md5sums");
+	}
 #	waitgoodimage(1200);
         sendautotype "$password\t"; # root PW
 	sleep 1;
         sendautotype "$password"; # root PW
 	sleep 1;
 
-	unless ($ENV{DESKTOP}=~/none/) {
+	if ($pic == '2') {
+	print "Skipping past users picture";
         sendkey "tab"; # User icon
 	sleep 1;
-	}
+	} 
+	
         sendkey "tab"; # skip media check
 	sleep 1;
         sendautotype "$realname\t"; # Test user
