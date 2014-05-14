89d0aab7ca589287beec90cd372e269407246ebf
Ludwig Nussel <ludwig.nussel@suse.de>
2014-05-14 16:59:19 +0200

    open as early as possible
    avoids losing log messages from qemu startup

--- bmwqemu.pm
+++ bmwqemu.pm
@@ -127,9 +127,12 @@ our %cmd;
 our %charmap;
 
 sub init {
+    open( $logfd, ">>", "autoinst-log.txt" );
 
-    cv::init();
-    require tinycv;
+    # set unbuffered so that send_key lines from main thread will be written
+    my $oldfh = select($logfd);
+    $| = 1;
+    select($oldfh);
 
     our $testedversion = $vars{NAME};
     unless ($testedversion) {
@@ -138,6 +141,11 @@ sub init {
         $testedversion =~ s/\.iso$//;
         $testedversion =~ s{-Media1?$}{};
     }
+
+    result_dir(); # init testresults dir
+
+    cv::init();
+    require tinycv;
 
     die "DISTRI undefined\n" unless $vars{DISTRI};
 
@@ -434,12 +442,6 @@ sub init_backend($) {
     my $name = shift;
     require "backend/$name.pm";
     $backend = "backend::$name"->new();
-    open( $logfd, ">>", "autoinst-log.txt" );
-
-    # set unbuffered so that send_key lines from main thread will be written
-    my $oldfh = select($logfd);
-    $| = 1;
-    select($oldfh);
 }
 
 sub start_vm() {
