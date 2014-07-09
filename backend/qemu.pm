d739c820e505adf211bc77ea02c59d535add0351
Stephan Kulow <coolo@suse.de>
2014-07-09 09:29:15 +0200

    translate prints in qemu backend to diag calls, so they appear in log file

--- backend/qemu.pm
+++ backend/qemu.pm
@@ -51,11 +51,11 @@ sub send_key($) {
     $self->send( { "execute" => "send-key", "arguments" => { 'keys' => $keys } } );
 }
 
-# warning: will not work due to https://bugs.launchpad.net/qemu/+bug/752476
 sub mouse_set($$) {
     my $self = shift;
     my ( $x, $y ) = @_;
 
+    $self->send({ 'VNC' => "mouse_set", 'arguments' => { 'x' => $x, 'y' => $y } } );
     # ... assuming size of screen is 800x600
     # (FIXME: size from last screenshot?)
     my ( $rx, $ry ) = ( 800, 600 );
@@ -172,19 +172,19 @@ sub do_stop_vm($) {
 sub do_savevm($) {
     my ( $self, $vmname ) = @_;
     my $rsp = $self->send("savevm $vmname")->{return};
-    print STDERR "SAVED $vmname $rsp\n";
+    bmwqemu::diag "SAVED $vmname $rsp";
     die unless ( $rsp eq "savevm $vmname" );
 }
 
 sub do_loadvm($) {
     my ( $self, $vmname ) = @_;
     my $rsp = $self->send("loadvm $vmname")->{return};
-    print STDERR "LOAD $vmname '$rsp'\n";
+    bmwqemu::diag "LOAD $vmname '$rsp'\n";
     die unless ( $rsp eq "loadvm $vmname" );
     $rsp = $self->send("stop")->{return};
-    print STDERR "stop $rsp\n";
+    bmwqemu::diag "stop $rsp\n";
     $rsp = $self->send("cont")->{return};
-    print STDERR "cont $rsp\n";
+    bmwqemu::diag "cont $rsp\n";
 }
 
 sub do_delvm($) {
@@ -246,7 +246,7 @@ sub _read_json($) {
         }
         my $qbuffer;
         my $bytes = sysread( $socket, $qbuffer, 1 );
-        if ( !$bytes ) { print STDERR "sysread failed: $!\n"; return undef; }
+        if ( !$bytes ) { bmwqemu::diag("sysread failed: $!"); return undef; }
         $rsp .= $qbuffer;
         if ( $rsp !~ m/\n/ ) { next; }
         $hash = eval { JSON::decode_json($rsp); };
@@ -285,8 +285,8 @@ sub start {
     $self->{to_parent}  = $p2;
     $self->{from_child} = $p1;
 
-    printf STDERR "$$: to_child %d, from_child %d\n", fileno( $self->{to_child} ), fileno( $self->{from_child} );
-    printf STDERR "$$: VNC %d\n", $bmwqemu::vars{VNC};
+    #printf STDERR "$$: to_child %d, from_child %d\n", fileno( $self->{to_child} ), fileno( $self->{from_child} );
+    #printf STDERR "$$: VNC %d\n", $bmwqemu::vars{VNC};
 
     my $tid = shared_clone( threads->create( \&_run, fileno( $self->{from_parent} ), fileno( $self->{to_parent} ), $bmwqemu::vars{VNC} ) );
     $self->{runthread} = $tid;
@@ -300,9 +300,9 @@ sub stop {
 
     $self->send('quit');
 
-    print STDERR " waiting for console read thread to quit...\n";
+    bmwqemu::diag " waiting for console read thread to quit...";
     $self->{runthread}->join();
-    print STDERR "done\n";
+    bmwqemu::diag "done";
     $self->{runthread} = undef;
     close( $self->{to_child} );
     $self->{to_child} = undef;
@@ -353,7 +353,7 @@ sub send {
         #print STDERR "from_child got $bytes\n";
         return undef unless ($bytes);
         if ( !$rsp && $buffer eq $MAGIC_PIPE_CLOSE_STRING ) {
-            print STDERR "got quit from management thread\n";
+            bmwqemu::diag "got quit from management thread";
             return undef;
         }
         $rsp .= $buffer;
@@ -428,7 +428,7 @@ sub handle_vnc_command($) {
 
     my $cmd = shift;
 
-    #print STDERR "VNC ". JSON::to_json($cmd) . "\n";
+    bmwqemu::diag "VNC ". JSON::to_json($cmd);
 
     if ($cmd->{VNC} eq 'capture') {
         my $img = $vnc->capture();
@@ -447,7 +447,7 @@ sub handle_vnc_command($) {
         $xpos -= $border_offset;
         $ypos -= $border_offset;
 
-        print STDERR "mouse_move $xpos, $ypos\n";
+        bmwqemu::diag "mouse_move $xpos, $ypos";
         $vnc->mouse_move_to($xpos, $ypos);
         return {};
     }
