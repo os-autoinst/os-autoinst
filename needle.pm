4a9abc10109ec7277c062a3fe6ba0384a38dd78c
Alberto Planas <aplanas@suse.com>
2014-08-12 11:53:43 +0200

    Fix Cwd import.

--- needle.pm
+++ needle.pm
@@ -2,7 +2,7 @@ package needle;
 
 use strict;
 use warnings;
-use Cwd;
+use Cwd qw/abs_path/;
 use File::Find;
 use File::Spec;
 use Data::Dump;
@@ -153,7 +153,7 @@ sub wanted_($) {
 sub init(;$) {
     $needledir = shift if @_;
     $needledir ||= "$bmwqemu::vars{CASEDIR}/needles/";
-    $needledir = Cwd::realpath($needledir);
+    $needledir = abs_path($needledir);
     %needles   = ();
     %tags      = ();
     bmwqemu::diag("init needles from $needledir");
