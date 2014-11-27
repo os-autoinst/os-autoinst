e30fa8337262bb3828f9104ee79eb903a21ee607
Stephan Kulow <coolo@suse.de>
2014-11-27 10:43:11 +0100

    add autoinst_url and move more functions into basetest
    having the functions in basetest allows us to overwrite them in the distribution
    and have different logic in the future

--- basetest.pm
+++ basetest.pm
@@ -460,8 +460,11 @@ sub ocr_checklist {
 }
 
 sub x11_start_program($$$) {
-    my ($program, $timeout, $options) = @_;
-    bmwqemu::mydie("TODO: implement x11 start for your distri " . get_var('DISTRI'));
+  my ($program, $timeout, $options) = @_;
+  $timeout ||= 6;
+  $options ||= {};
+
+  bmwqemu::mydie("TODO: implement x11 start for your distri " . get_var('DISTRI'));
 }
 
 sub ensure_installed {
@@ -480,10 +483,60 @@ sub ensure_installed {
     wait_still_screen( 7, 90 );    # wait for install
 }
 
-sub standstill_detected($) {
-    my ($lastscreenshot) = @_;
+sub become_root() {
+    my ($self) = @_;
 
-    $bmwqemu::current_test->record_screenfail(
+    testapi::script_sudo( "bash", 0 );    # become root
+    testapi::script_run("echo 'imroot' > /dev/$serialdev");
+    testapi::wait_serial( "imroot", 5 ) || die "Root prompt not there";
+    testapi::script_run("cd /tmp");
+}
+
+=head2 script_run
+
+script_run($program, [$wait_seconds])
+
+Run $program (by assuming the console prompt and typing it).
+Wait for idle before  and after.
+
+=cut
+
+sub script_run($;$) {
+
+    # start console application
+    my ($self, $name, $wait) = @_;
+
+    testapi::wait_idle();
+
+    testapi::type_string "$name\n";
+    testapi::wait_idle($wait);
+}
+
+=head2 script_sudo
+
+script_sudo($program, $wait_seconds)
+
+Run $program. Handle the sudo timeout and send password when appropriate.
+
+$wait_seconds
+
+=cut
+
+sub script_sudo($$) {
+    my ($self, $prog, $wait) = @_;
+
+    testapi::type_string "sudo $prog\n";
+    if ( testapi::check_screen "sudo-passwordprompt", 3 ) {
+        testapi::type_password;
+        testapi::send_key "ret";
+    }
+    testapi::wait_idle($wait);
+}
+
+sub standstill_detected($) {
+    my ($self, $lastscreenshot) = @_;
+
+    $self->record_screenfail(
         img     => $lastscreenshot,
         result  => 'fail',
         overall => 'fail'
