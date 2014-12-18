9f70ac9ce480eff1559228ce7edc32dbaf3f702c
Richard Brown <rbrownccb@opensuse.org>
2014-12-18 16:32:52 +0100

    Add addon ISOs if addons being tested

--- backend/qemu.pm
+++ backend/qemu.pm
@@ -178,8 +178,8 @@ sub start_qemu($) {
         }
         
 		for my $i ( 1 .. 6 ) {  # check for up to 6 ADDON ISOs
-			if ( $vars->{"ADDONISO_$i"} ) {
-				my $addoniso = $vars->{"ADDONISO_$i"};
+			if ( $vars->{"ISO_$i"} && $vars->{"ADDONS"}) {
+				my $addoniso = $vars->{"ISO_$i"};
 				push( @params, "-drive", "if=ide,id=addon_$i,file=$addoniso,media=cdrom" );
 			}
 		}
