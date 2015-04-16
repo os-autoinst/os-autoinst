c33ba95cf452fca29abe548c75b7aeae0be11192
Bernhard M. Wiedemann <bwiedemann@suse.de>
2015-04-16 11:36:32 +0200

    add HID client+server
    this is using Mojo's JSON RPC functions
    and allows to send keystrokes and to change the CDROM

---
+++ tools/rpc_hid.pl
@@ -0,0 +1,168 @@
+#!/usr/bin/perl -w
+use strict;
+# usage: $0 daemon --listen http://:::3000
+
+use Mojolicious::Lite;
+use MojoX::JSON::RPC::Service;
+use Fcntl;
+
+my $hidfile="/dev/hidg0";
+open(HID, "+<", $hidfile) or die "error opening $hidfile: $!";
+
+my $modifier = {
+    'ctrl'    => 0x01,
+    'shift'   => 0x02,
+    'alt'     => 0x04,
+    'meta'    => 0x08,
+    'r_ctrl'  => 0x10,
+    'r_shift' => 0x20,
+    'r_alt'   => 0x40,
+    'r_meta'  => 0x80,
+};
+
+my $keymap_usb = {
+#    'ctrl'  => 0xe0,
+#    'shift' => 0xe1,
+#    'alt'   => 0xe2,
+    'win'   => 0xe3,
+    'caps_lock' => 0x39,
+
+    'print'  => 0x46,
+    'scroll_lock' => 0x47,
+    'pause'  => 0x48,
+    'end'    => 0x4d,
+    'delete' => 0x4c,
+    'home'   => 0x4a,
+    'insert' => 0x49,
+
+    'pgup' => 0x4b,
+    'pgdn' => 0x4e,
+
+    'left'  => 0x50,
+    'right' => 0x4f,
+    'up'    => 0x52,
+    'down'  => 0x51,
+    'num_lock'    => 0x53,
+    'kp_divide'   => 0x54,
+    'kp_multiply' => 0x55,
+    'kp_subtract' => 0x56,
+    'kp_add'      => 0x57,
+    'kp_enter'    => 0x58,
+    'kp_0'        => 0x62,
+    'kp_decimal'  => 0x63,
+    'dunno1'      => 0x64,
+    'application' => 0x65,
+    'power'       => 0x66,
+    'kp_equals'   => 0x67,
+
+    '0'     => 0x27,
+    'ret'   => 0x28,
+    'esc'   => 0x29,
+    'backspace' => 0x2a,
+    'tab'   => 0x2b,
+    'spc'   => 0x2c,
+    'minus' => 0x2d,
+    '='     => 0x2e,
+    '['     => 0x2f,
+    ']'     => 0x30,
+    '\\'    => 0x31,
+    '#'     => 0x32,
+    ';'     => 0x33,
+    '\''    => 0x34,
+    '`'     => 0x35,
+    ','     => 0x36,
+    '.'     => 0x37,
+    '/'     => 0x38,
+};
+
+sub init_usb_keymap {
+    my $keymap;
+    $keymap=$keymap_usb;
+    for my $key ("a" .. "z") {
+        my $code = 0x4 + ord($key) - ord('a');
+        $keymap->{$key} = $code;
+        $keymap->{uc($key)} = $code;
+    }
+    for my $key ("1" .. "9") {
+        $keymap->{$key} = 0x1e + ord($key) - ord('1');
+        $keymap->{"kp_$key"} = 0x59 + ord($key) - ord('1');
+    }
+    for my $key (1 .. 12) {
+        $keymap->{"f$key"} = 0x3a + $key - 1,;
+    }
+    return $keymap;
+}
+our $keymap = init_usb_keymap();
+
+sub send_A() {
+    my $data = pack("C*", $modifier->{'shift'},0,$keymap->{"a"},0,0,0,0,0);
+    my $releasedata = pack("C*", 0,0,0,0,0,0,0,0);
+    syswrite(HID, $data);
+    syswrite(HID, $releasedata);
+}
+
+sub key_code($) {
+    my $key = shift;
+    my $mod = 0;
+    my @codes = ();
+    foreach my $part (split("-", $key)) {
+        if(my $m = $modifier->{$part}) {
+            $mod |= $m;
+        } elsif((my $code = $keymap->{$part})) {
+            if(@codes>=6) {die "too many keys at a time in $key"}
+            push(@codes, $code);
+        } else {
+            die "invalid key $part in $key";
+        }
+    }
+    unshift(@codes, $mod, 0);
+    return @codes;
+}
+
+sub send_key($) {
+    my @codes = key_code($_[0]);
+    print "@codes\n"; # debug
+    my $data = pack("C*", @codes);
+    syswrite(HID, $data); # key-press
+    foreach(@codes) {$_=0}
+    $data = pack("C*", @codes);
+    syswrite(HID, $data); # key-release
+}
+
+sub change_cd($) {
+    my $filename = shift;
+    my $b="/sys/kernel/config/usb_gadget/usbarmory";
+    my $c="$b/configs/c.1/mass_storage.usb0";
+    my $f="$b/functions/mass_storage.usb0";
+    unlink($c);
+    open(my $fh, ">", "$f/lun.0/file") or die $!;
+    print $fh $filename;
+    close $fh;
+    symlink($f, $c) or die $!;
+    system("ls /sys/class/udc > $b/UDC");
+}
+
+sub read_serial() {
+    my $dev = "/dev/ttyGS0";
+    open(my $fh, "<", $dev) # FIXME blocking is not good
+        or die "Can't open $dev: $!";
+    my $data=<$fh>;
+    #my $nr = read($fh, $data, 1);
+    close $fh;
+    return $data;# if $nr>0;
+    return undef;
+}
+
+#send_key("shift-a");
+#while(<>) { chomp; send_key($_); }
+
+my $svc = MojoX::JSON::RPC::Service->new;
+$svc->register('send_key', \&send_key);
+$svc->register('change_cd', \&change_cd);
+$svc->register('read_serial', \&read_serial);
+
+plugin 'json_rpc_dispatcher' => {
+    services => { '/jsonrpc' => $svc }
+};
+
+app->start;
---
+++ tools/rpc_hidclient.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl -w
+use strict;
+use MojoX::JSON::RPC::Client;
+
+my $client = MojoX::JSON::RPC::Client->new;
+my $url    = 'http://beagle.d.zq1.de:3000/jsonrpc';
+
+sub RPCwrap($) {
+    my $callobj = shift;
+    #$callobj->{id} = 1;
+    my $res = $client->call($url, $callobj);
+
+    if($res) {
+        if ($res->is_error) { # RPC ERROR
+            print 'Error : ', $res->error_message;
+        }
+        else {
+            return $res->result;
+        }
+    }
+    else {
+        my $tx_res = $client->tx->res; # Mojo::Message::Response object
+        print 'HTTP response '.$tx_res->code.' '.$tx_res->message;
+    }
+}
+
+sub define_RPC_func($)
+{
+    my $name=shift;
+    eval "sub $name {RPCwrap({method=>'$name', params => [\@_] })}"
+}
+
+for (qw(send_key change_cd read_serial)) {
+    define_RPC_func($_);
+}
+
+#change_cd("/mounts/dist/install/openSUSE-13.2-GM/iso/openSUSE-13.2-NET-x86_64.iso");
+#change_cd("/mounts/dist/install/openSUSE-13.2-GM/iso/openSUSE-13.2-NET-i586.iso");
+while(<>) { chomp; send_key($_); }
+#while(1) { print read_serial()||""; sleep 1; }
+
