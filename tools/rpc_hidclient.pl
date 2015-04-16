8e098846831428dba29105ced993cf51679ccce0
Bernhard M. Wiedemann <bwiedemann@suse.de>
2015-04-16 19:05:42 +0200

    move most RPC client functions into perl module

--- tools/rpc_hidclient.pl
+++ tools/rpc_hidclient.pl
@@ -1,41 +1,11 @@
 #!/usr/bin/perl -w
 use strict;
 
-use MojoX::JSON::RPC::Client;
-use Time::HiRes qw(gettimeofday tv_interval);
-
-my $client = MojoX::JSON::RPC::Client->new;
-my $url    = 'http://beagle.d.zq1.de:3000/jsonrpc';
-
-sub RPCwrap($) {
-    my $callobj = shift;
-    #$callobj->{id} = 1;
-    my $t = [gettimeofday()];
-    my $res = $client->call($url, $callobj);
-    print "time: " . tv_interval($t) . "\n";
-
-    if ($res) {
-        if ($res->is_error) {    # RPC ERROR
-            print 'Error : ', $res->error_message;
-        }
-        else {
-            return $res->result;
-        }
-    }
-    else {
-        my $tx_res = $client->tx->res;
-        print 'HTTP response ' . $tx_res->code . ' ' . $tx_res->message;
-    }
+BEGIN {
+    my ($wd) = $0 =~ m-(.*)/-;
+    push(@INC, $wd);
 }
-
-sub define_RPC_func($) {
-    my $name = shift;
-    eval "sub $name {RPCwrap({method=>'$name', params => [\@_] })}"
-}
-
-for (qw(init_usb_gadget send_key change_cd read_serial)) {
-    define_RPC_func($_);
-}
+use rpc_hidclient;
 
 init_usb_gadget();
 
