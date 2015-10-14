8eb191243ec75f33be88f4af0c891710cc4ef618
Ludwig Nussel <ludwig.nussel@suse.de>
2015-10-14 13:40:04 +0200

    refactor multipath params a bit
    also remove trails of ide, not used and probably broken nowadays anyways

--- backend/qemu.pm
+++ backend/qemu.pm
@@ -249,7 +249,7 @@ sub start_qemu() {
     for my $var (qw/HDDMODEL CDMODEL/) {
         if ($vars->{$var} =~ /virtio-scsi.*/) {
             $virtio_scsi_controller = $vars->{$var};
-            $vars->{$var} = sprintf "scsi-%sd", lc substr $var, 0, 1;;
+            $vars->{$var} = sprintf "scsi-%sd", lc substr $var, 0, 1;
         }
     }
     # network settings
@@ -348,7 +348,7 @@ sub start_qemu() {
                 symlink($i, "$basedir/l$i") or die "$!\n";
             }
             else {
-                die "$!\n" unless runcmd($qemuimg, "create", "$basedir/$i", "-f", "$vars->{HDDFORMAT}", $vars->{HDDSIZEGB} . "G") == 0;
+                die "$!\n" unless runcmd($qemuimg, "create", "$basedir/$i", "-f", $vars->{HDDFORMAT}, $vars->{HDDSIZEGB} . "G") == 0;
                 symlink($i, "$basedir/l$i") or die "$!\n";
             }
         }
@@ -416,18 +416,18 @@ sub start_qemu() {
         }
 
         for my $i (1 .. $vars->{NUMDISKS}) {
-            my $boot = "";    #$i==1?",boot=on":""; # workaround bnc#696890
             if ($vars->{MULTIPATH}) {
                 for my $c (1 .. $vars->{PATHCNT}) {
                     # pathname is a .. d
-                    my $pathname = chr(96 + $c);
-                    push(@params, "-drive", "file=$basedir/l$i,cache=none,if=none$boot,id=hd${i}${pathname},serial=mpath$i");
-                    push(@params, "-device", "$vars->{HDDMODEL},drive=hd${i}${pathname},bus=scsi" . ($c % 2 ? "1" : "0") . ".0");
+                    my $bus = sprintf "scsi%d.0", ($c - 1) % 2;    # alternate between scsi0 and scsi1
+                    my $id = sprintf 'hd%d%c', $i, 96 + $c;
+                    push(@params, "-device", "$vars->{HDDMODEL},drive=$id,bus=$bus");
+                    push(@params, "-drive",  "file=$basedir/l$i,cache=none,if=none,id=$id,serial=mpath$i,format=$vars->{HDDFORMAT}");
                 }
             }
             else {
-                push(@params, "-device", "$vars->{HDDMODEL},drive=hd$i" . ($vars->{HDDMODEL} =~ /ide-hd/ ? ",bus=ide.@{[$i-1]}" : ''));
-                push(@params, "-drive", "file=$basedir/l$i,cache=unsafe,if=none$boot,id=hd$i");
+                push(@params, "-device", "$vars->{HDDMODEL},drive=hd$i");
+                push(@params, "-drive",  "file=$basedir/l$i,cache=unsafe,if=none,id=hd$i,format=$vars->{HDDFORMAT}");
             }
         }
 
