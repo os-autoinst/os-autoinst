d25e39fb5efe8254b0c629705c261a457f719f86
Bernhard M. Wiedemann <bwiedemann@suse.de>
2015-12-07 11:34:36 +0100

    add python test module example
    this code works.
    It is a simplified rewrite of part of the USBBOOT path
    in openSUSE's bootloader.pm

---
+++ doc/example-bootloader.py
@@ -0,0 +1,19 @@
+import perl
+
+def run(self):
+    print "This python code ran"
+    #perl.assert_screen("fooweiuhfiu", 1) # to test how it dies
+    if perl.get_var("USBBOOT"):
+        perl.assert_screen("boot-menu", 1)
+        perl.send_key("f12")
+        perl.assert_screen("boot-menu-usb", 4)
+        perl.send_key(2 + perl.get_var("NUMDISKS"))
+    perl.assert_screen("inst-bootmenu", 15)
+    perl.send_key_until_needlematch('inst-oninstallation', 'down', 10, 5)
+    perl.type_string("qwertyuiopasdfghjkl")
+    print "TODO: more code"
+    perl.send_key('ret')
+
+def test_flags(self):
+    return dict([("fatal", 1)])
+
