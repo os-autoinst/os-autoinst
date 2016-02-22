ae0df965d4f5f5eda76a14b4548b82dd159aca09
Yong Sun <yosun@suse.com>
2016-02-22 15:57:14 +0800

    Modify the PR 415 back, because the code in PR 415 will make new created HDD have two .qcow2 format mark.

--- backend/qemu.pm
+++ backend/qemu.pm
@@ -186,14 +186,15 @@ sub do_extract_assets {
     my $hdd_num = $args->{hdd_num};
     my $name    = $args->{name};
     my $img_dir = $args->{dir};
-    my $format  = $args->{format};
+    $name =~ /\.([[:alnum:]]+)$/;
+    my $format  = $1;
     if (!$format || $format !~ /^(raw|qcow2)$/) {
         bmwqemu::diag "do_extract_assets: only raw and qcow2 formats supported $name $format\n";
     }
     elsif (-f "raid/l$hdd_num") {
-        bmwqemu::diag "preparing hdd $hdd_num for upload as $name in $format\n";
+        bmwqemu::diag "preparing hdd $hdd_num for upload as $name\n";
         mkpath($img_dir);
-        my @cmd = ('nice', 'ionice', 'qemu-img', 'convert', '-O', $format, "raid/l$hdd_num", "$img_dir/$name.$format");
+        my @cmd = ('nice', 'ionice', 'qemu-img', 'convert', '-O', $format, "raid/l$hdd_num", "$img_dir/$name");
         if ($format eq 'raw') {
             runcmd(@cmd);
         }
@@ -204,7 +205,7 @@ sub do_extract_assets {
                 runcmd(@cmd);
             }
             else {
-                symlink("../raid/l$hdd_num", "$img_dir/$name.$format");
+                symlink("../raid/l$hdd_num", "$img_dir/$name");
             }
         }
     }
