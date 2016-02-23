e5ed11a3776b98efc999880dfb1b2db6ce57d122
Oliver Kurz <okurz@suse.de>
2016-02-23 18:35:44 +0100

    Test POD correctness and coverage for testapi

---
+++ t/06-pod-coverage.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Test::More;
+use Pod::Coverage;
+use File::Basename;
+
+# an alternative to checking Pod::Coverage might be Test::Pod::Coverage but as
+# os-autoinst does not really feature full perl modules better just check
+# explicitly what we care about right now.
+
+my $dirname = dirname(__FILE__);
+chdir($dirname . '/..');
+
+my $pc = Pod::Coverage->new(
+    package  => 'testapi',
+    pod_from => 'testapi.pm',
+);
+is($pc->coverage, 1, 'Everything in testapi covered') || diag('Uncovered: ', join(', ', $pc->uncovered), "\n");
+done_testing();
