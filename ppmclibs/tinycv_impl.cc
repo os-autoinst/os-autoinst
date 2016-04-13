aaaa3208d028c8d53554d9375fa5300dde0b5ce8
Stephan Kulow <coolo@suse.de>
2016-04-13 14:40:03 +0200

    Get rid of the s390 brown
    (it's quite embarassing actually, but we never had 32bit VNC so far using ZLRE)

--- ppmclibs/tinycv_impl.cc
+++ ppmclibs/tinycv_impl.cc
@@ -604,9 +604,9 @@ cv::Vec3b VNCInfo::read_cpixel(const unsigned char *data, size_t &offset)
     green = (pixel >> green_shift & green_mask) * green_skale;
     blue = (pixel >> blue_shift  & blue_mask ) * blue_skale;
   } else {
-    blue = data[offset++];
-    green = data[offset++];
     red = data[offset++];
+    green = data[offset++];
+    blue = data[offset++];
   }
   return cv::Vec3b(blue, green, red);
 }
