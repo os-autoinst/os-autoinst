os-autoinst/os-autoinst

commit 18791e64f8bd956920bcff3f39f9084a3af4f4b6

Explicity show skiped tests.

autotest.pm
@@ -47,6 +47,7 @@ sub runalltests {
 	if ($vmloaded) {
 	    $t->runtest;
 	} else {
+	    diag "skiping $t->{fullname}";
 	    $t->skip_if_not_running;
 	}
     }

commit 56c1d11793fdfcdb0f972158d82cc2b0d8c423fd

Fix spelling error 'skiping'
Introduced with 18791e64

autotest.pm
@@ -171,7 +171,7 @@ sub runalltests {
             }
         }
         else {
-            bmwqemu::diag "skiping $fullname";
+            bmwqemu::diag "skipping $fullname";
             $t->skip_if_not_running();
             $t->save_test_result();
         }