623e6f8df387f8ac21f1473d8aefc7ba595f750e
Oliver Kurz <okurz@suse.de>
2016-05-31 15:35:55 +0200

    Check for xdotool presence to improve feedback

--- consoles/sshXtermVt.pm
+++ consoles/sshXtermVt.pm
@@ -30,13 +30,15 @@ sub fullscreen {
     my $display     = $self->{DISPLAY};
     my $window_name = $args->{window_name};
 
+    my $xdotool = which "xdotool";
+    die "Missing 'xdotool'" unless $xdotool;
     # search for YaST Window and grab the id
-    my $window_id = qx"DISPLAY=$display xdotool search --sync --limit 1 --name $window_name";
+    my $window_id = qx"DISPLAY=$display $xdotool search --sync --limit 1 --name $window_name";
     $window_id =~ s/\D//g;
 
     # resize and move window to fit in icewm
-    system("DISPLAY=$display xdotool windowsize $window_id 100% 100%");
-    system("DISPLAY=$display xdotool windowmove $window_id 0 0");
+    system("DISPLAY=$display $xdotool windowsize $window_id 100% 100%");
+    system("DISPLAY=$display $xdotool windowmove $window_id 0 0");
 }
 
 sub activate {
