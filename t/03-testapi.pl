57f22b1136fa7a757437238b7a29f22fdcd8de3a
Stephan Kulow <coolo@suse.de>
2016-06-27 20:33:57 +0200

    Fix testapi test

--- t/03-testapi.pl
+++ t/03-testapi.pl
@@ -11,37 +11,56 @@ BEGIN {
 }
 
 require bmwqemu;
-require t::test_driver;
 
-$bmwqemu::backend = t::test_driver->new;
+my $cmds;
+use Test::MockModule;
+my $mod       = new Test::MockModule('myjsonrpc');
+my $fake_exit = 0;
+
+sub fake_send_json {
+    my ($to_fd, $cmd) = @_;
+    push(@$cmds, $cmd);
+}
+sub fake_read_json {
+    my ($fd) = @_;
+    my $lcmd = $cmds->[-1];
+    if ($lcmd->{cmd} eq 'backend_wait_serial') {
+        my $str = $lcmd->{regexp};
+        $str =~ s,\\d\+,$fake_exit,;
+        return {ret => {matched => 1, string => $str}};
+    }
+    return {};
+
+}
+$mod->mock(send_json => \&fake_send_json);
+$mod->mock(read_json => \&fake_read_json);
 
 use testapi;
 
-my $cmd = 't::test_driver::type_string';
 type_string 'hallo';
-is_deeply($bmwqemu::backend->{cmds}, [$cmd, {max_interval => 250, text => 'hallo'}]);
-$bmwqemu::backend->{cmds} = [];
+is_deeply($cmds, [{cmd => 'backend_type_string', max_interval => 250, text => 'hallo'}]);
+$cmds = [];
 
 type_string 'hallo', 4;
-is_deeply($bmwqemu::backend->{cmds}, [$cmd, {max_interval => 4, text => 'hallo'}]);
-$bmwqemu::backend->{cmds} = [];
+is_deeply($cmds, [{cmd => 'backend_type_string', max_interval => 4, text => 'hallo'}]);
+$cmds = [];
 
 type_string 'hallo', secret => 1;
-is_deeply($bmwqemu::backend->{cmds}, [$cmd, {max_interval => 250, text => 'hallo'}]);
-$bmwqemu::backend->{cmds} = [];
+is_deeply($cmds, [{cmd => 'backend_type_string', max_interval => 250, text => 'hallo'}]);
+$cmds = [];
 
 type_string 'hallo', secret => 1, max_interval => 10;
-is_deeply($bmwqemu::backend->{cmds}, [$cmd, {max_interval => 10, text => 'hallo'}]);
-$bmwqemu::backend->{cmds} = [];
+is_deeply($cmds, [{cmd => 'backend_type_string', max_interval => 10, text => 'hallo'}]);
+$cmds = [];
 
 $testapi::password = 'stupid';
 type_password;
-is_deeply($bmwqemu::backend->{cmds}, [$cmd, {max_interval => 100, text => 'stupid'}]);
-$bmwqemu::backend->{cmds} = [];
+is_deeply($cmds, [{cmd => 'backend_type_string', max_interval => 100, text => 'stupid'}]);
+$cmds = [];
 
 type_password 'hallo';
-is_deeply($bmwqemu::backend->{cmds}, [$cmd, {max_interval => 100, text => 'hallo'}]);
-$bmwqemu::backend->{cmds} = [];
+is_deeply($cmds, [{cmd => 'backend_type_string', max_interval => 100, text => 'hallo'}]);
+$cmds = [];
 
 is($autotest::current_test->{dents}, undef, 'no soft failures so far');
 stderr_like(\&record_soft_failure, qr/record_soft_failure\(reason=undef\)/, 'soft failure recorded in log');
@@ -67,7 +86,6 @@ subtest 'script_run' => sub {
     }
     $autotest::current_test = t::test->new();
 
-    use Test::MockModule;
     my $module = new Test::MockModule('bmwqemu');
     # just save ourselves some time during testing
     $module->mock('wait_for_one_more_screenshot', sub { sleep 0; });
@@ -75,14 +93,14 @@ subtest 'script_run' => sub {
     require distribution;
     testapi::set_distribution(distribution->new());
     is(assert_script_run('true'), undef, 'nothing happens on success');
-    $bmwqemu::backend->mock_exit_code(1);
+    $fake_exit = 1;
     like(exception { assert_script_run 'false', 42; }, qr/command.*false.*failed at/, 'with timeout option (deprecated mode)');
     like(exception { assert_script_run 'false', 0, 'my custom fail message'; }, qr/command.*false.*failed: my custom fail message at/, 'custom message on die (deprecated mode)');
     like(exception { assert_script_run('false', fail_message => 'my custom fail message'); }, qr/command.*false.*failed: my custom fail message at/, 'using named arguments');
     like(exception { assert_script_run('false', timeout => 0, fail_message => 'my custom fail message'); }, qr/command.*false.*failed: my custom fail message at/, 'using two named arguments');
-    $bmwqemu::backend->mock_exit_code(0);
+    $fake_exit = 0;
     is(script_run('true'), '0', 'script_run with no check of success, returns exit code');
-    $bmwqemu::backend->mock_exit_code(1);
+    $fake_exit = 1;
     is(script_run('false'), '1', 'script_run with no check of success, returns exit code');
     is(script_run('false', 0), undef, 'script_run with no check of success, returns undef when not waiting');
 };
