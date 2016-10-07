e3895c3797f71eed3ac70d59b2f4cca42dfc15b6
Richard Palethorpe <rpalethorpe@suse.com>
2016-10-07 11:25:59 +0200

    Add consoles/virtio_screen.pm for virtio_terminal

---
+++ consoles/virtio_screen.pm
@@ -0,0 +1,33 @@
+package consoles::virtio_screen;
+use 5.18;
+use warnings;
+use autodie;
+
+sub new {
+    my ($class, $socket_fd) = @_;
+    my $self = bless({class => $class}, $class);
+    $self->{socket_fd} = $socket_fd;
+    return $self;
+}
+
+sub send_key {
+    ...
+}
+
+sub hold_key {
+    ...
+}
+
+sub release_key {
+    ...
+}
+
+sub type_string {
+    ...
+}
+
+sub current_screen {
+    # TODO: We could generate a bitmap of the terminal text, but I think it would be misleading.
+    #       Instead we should use a text terminal viewer in the browser if possible.
+    return undef;
+}
