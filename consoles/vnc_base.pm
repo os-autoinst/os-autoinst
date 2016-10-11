25491a78eb68dfa85f34e5de543c343bd0495079
Stephan Kulow <coolo@kde.org>
2016-10-11 16:57:30 +0200

    Don't call check_assert_screen more than once a second (#614)
    So far it called it *at least* once a second, but also whenever
    the event loop was interrupted, e.g. by the worker calling into /status

--- consoles/vnc_base.pm
+++ consoles/vnc_base.pm
@@ -1,5 +1,5 @@
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2015 SUSE LLC
+# Copyright © 2012-2016 SUSE LLC
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
