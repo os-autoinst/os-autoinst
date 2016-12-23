06e847d2a9cdbc1d2d7faf66e0572c0239563e14
Stephan Kulow <coolo@suse.de>
2016-12-23 07:09:13 +0100

    Make travis setup reusable
    moved into autogen.sh so I can reuse it in openQA full stack testing

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+
+set -ex
+
+if test -n "$SETUP_FOR_TRAVIS" ; then
+  # for recent opencv
+  sudo add-apt-repository -y ppa:kubuntu-ppa/backports
+  sudo apt-get -y update
+  sudo apt-get -y install libdbus-1-dev libssh2-1-dev libopencv-dev libtheora-dev libcv-dev libhighgui-dev tesseract-ocr libsndfile1-dev libfftw3-dev qemu-system
+  cpanm -nq --installdeps --with-feature=coverage .
+  find . -type f | xargs -n1 sed -i -e 's,#!/usr/bin/perl,#!/usr/bin/env perl,'
+fi
+
 mkdir --parent --verbose m4
 aclocal --install -Im4
 autoreconf --verbose --install --symlink --force
+
+./configure "$@"
