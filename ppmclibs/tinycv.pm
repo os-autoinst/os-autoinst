7aa05a045dd4f9590effb5e5d2d243aab448b917
Stefan Brüns <stefan.bruens@rwth-aachen.de>
2017-05-19 05:30:31 +0200

    tinycv: Cleanup output of processing durations in needle search
    - Suppress output for exclusion if there are no exclude areas
    - Report area height and margin, make formatting easier to grasp
    
    Signed-off-by: Stefan Brüns <stefan.bruens@rwth-aachen.de>

--- ppmclibs/tinycv.pm
+++ ppmclibs/tinycv.pm
@@ -87,15 +87,15 @@ sub search_ {
             $img->replacerect($a->{xpos}, $a->{ypos}, $a->{width}, $a->{height});
             $stopwatch->lap("**++-- search__: rectangle replacement") if $stopwatch;
         }
+        $stopwatch->lap("**++ search__: areas exclusion") if $stopwatch;
     }
-    $stopwatch->lap("**++ search__: areas exclusion") if $stopwatch;
     my $ret = {ok => 1, needle => $needle, area => []};
     for my $area (@match) {
         my $margin = int($area->{margin} + $search_ratio * (1024 - $area->{margin}));
 
         ($sim, $xmatch, $ymatch) = $img->search_needle($needle_image, $area->{xpos}, $area->{ypos}, $area->{width}, $area->{height}, $margin);
 
-        $stopwatch->lap("**++ tinycv::search_needle $area->{xpos}x$area->{ypos}x$area->{width}") if $stopwatch;
+        $stopwatch->lap("**++ tinycv::search_needle $area->{width}x$area->{height} + $margin @ $area->{xpos}x$area->{ypos}") if $stopwatch;
         my $ma = {
             similarity => $sim,
             x          => $xmatch,
