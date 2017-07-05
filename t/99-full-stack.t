os-autoinst/os-autoinst

commit 126bf5ac545d2b6b119d6345ac1184eb8fe1e3ca

Convert the needle tags to string.
The input parameter mustmatch can be a scalar or an array ref.
In case of failure to find the need the needles must be converted
to string.

New test: assert_screen_fail_test.pm

t/99-full-stack.t
@@ -41,9 +41,29 @@ open($var, '>', 'live_log');
 close($var);
 system("perl $toplevel_dir/isotovideo -d 2>&1 | tee autoinst-log.txt");
 is(system('grep -q "\d*: EXIT 0" autoinst-log.txt'), 0, 'test executed fine');
-for my $result (glob("testresults/result*.json")) {
+
+my $ignore_results_re = qr/fail/;
+for my $result (grep { $_ !~ $ignore_results_re } glob("testresults/result*.json")) {
     my $json = from_json(Mojo::File->new($result)->slurp);
     is($json->{result}, 'ok', "Result in $result is ok");
 }
+
+for my $result (glob("testresults/result*fail*.json")) {
+    my $json = from_json(Mojo::File->new($result)->slurp);
+    is($json->{result}, 'fail', "Result in $result is fail");
+}
+
+subtest 'Assert screen failure' => sub {
+    plan tests => 1;
+    open my $ifh, '<', 'autoinst-log.txt';
+    my $regexp = qr /(?<=no candidate needle with tag\(s\)) '(no_tag, no_tag2|no_tag3)'/;
+    my $count  = 0;
+    while (<$ifh>) {
+        $count++ if $_ =~ $regexp;
+    }
+    close $ifh;
+
+    is($count, 2, 'Assert screen failures');
+};
 
 done_testing();

t/data/tests/tests/assert_screen_fail_test.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2016-2017 SUSE LLC
+# Copyright (C) 2017 SUSE LLC
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,14 +13,19 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, see <http://www.gnu.org/licenses/>.
 
-use strict;
+use 5.018;
+use base 'basetest';
 use testapi;
 
-autotest::loadtest "tests/boot.pm";
-autotest::loadtest "tests/assert_screen_fail_test.pm";
-autotest::loadtest "tests/shutdown.pm";
+sub run {
+    # Test done this way, because:
+    eval { assert_screen ['no_tag', 'no_tag2'], timeout => 1, no_wait => 1; };
+    bmwqemu::diag($@) if ($@);
 
+    eval { assert_screen 'no_tag3', timeout => 1, no_wait => 1; };
+    bmwqemu::diag($@) if ($@);
+
+
+}
 
 1;
-
-# vim: set sw=4 et:

testapi.pm
@@ -275,7 +275,15 @@ sub _check_backend_response {
             }
         }
         if (!$check && !$rsp->{saveresult}) {
-            OpenQA::Exception::FailedNeedle->throw(error => "no candidate needle with tag(s) '$mustmatch' matched", tags => $mustmatch);
+            # Must match can be only scalar or array ref.
+            my $needletags = $mustmatch;
+            if (ref($mustmatch) eq 'ARRAY') {
+                $needletags = join(', ', @$mustmatch);
+            }
+            OpenQA::Exception::FailedNeedle->throw(
+                error => "no candidate needle with tag(s) '$needletags' matched",
+                tags  => $mustmatch
+            );
         }
         if ($rsp->{saveresult}) {
             $autotest::current_test->save_test_result();

commit f90a1bfac8721180d63929164a97db57076da2b1

Add a new control variable to full stack tests
Use INTEGRATION_TESTS to toggle the assert_screen_failt_test so that
when used by the full stack test of openQA, it can be enabled on demand.

This addresses the problems introduced by 126bf5ac on the openQA side.

t/99-full-stack.t
@@ -66,4 +66,28 @@ subtest 'Assert screen failure' => sub {
     is($count, 2, 'Assert screen failures');
 };
 
+open($var, '>', 'vars.json');
+print $var <<EOV;
+{
+   "ARCH" : "i386",
+   "BACKEND" : "qemu",
+   "QEMU" : "i386",
+   "QEMU_NO_KVM" : "1",
+   "QEMU_NO_TABLET" : "1",
+   "QEMU_NO_FDC_SET" : "1",
+   "CASEDIR" : "$data_dir/tests",
+   "PRJDIR"  : "$data_dir",
+   "ISO" : "$data_dir/Core-7.2.iso",
+   "CDMODEL" : "ide-cd",
+   "HDDMODEL" : "ide-drive",
+   "INTEGRATION_TESTS" : "1",
+   "VERSION" : "1",
+}
+EOV
+
+system("perl $toplevel_dir/isotovideo -d 2>&1 | tee autoinst-log.txt");
+isnt(system('grep -q "assert_screen_fail_test" autoinst-log.txt'), 0, 'assert screen test not scheduled');
+is(system('grep -q "isotovideo done" autoinst-log.txt'), 0, 'isotovideo is done');
+is(system('grep -q "EXIT 0" autoinst-log.txt'),          0, 'Test finished as expected');
+
 done_testing();

t/data/tests/main.pm
@@ -17,9 +17,12 @@ use strict;
 use testapi;
 
 autotest::loadtest "tests/boot.pm";
-autotest::loadtest "tests/assert_screen_fail_test.pm";
-autotest::loadtest "tests/shutdown.pm";
 
+unless (get_var('INTEGRATION_TESTS')) {
+    autotest::loadtest "tests/assert_screen_fail_test.pm";
+}
+
+autotest::loadtest "tests/shutdown.pm";
 
 1;
 

t/data/tests/tests/assert_screen_fail_test.pm
@@ -25,7 +25,6 @@ sub run {
     eval { assert_screen 'no_tag3', timeout => 1, no_wait => 1; };
     bmwqemu::diag($@) if ($@);
 
-
 }
 
 1;