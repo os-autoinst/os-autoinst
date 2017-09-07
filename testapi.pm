os-autoinst/os-autoinst

commit b9045f43a3c8186db352c7a5de8eea501d861a84

Replace 'wait_idle' by the only real scalable solution (#838)
Related progress issue: https://progress.opensuse.org/issues/5830

testapi.pm
@@ -1675,38 +1675,33 @@ sub parse_junit_log {
 
 =head2 wait_idle
 
-=for stopwords IDLETHRESHOLD qemu
-
   wait_idle([$timeout_sec]);
 
-Wait until the system becomes idle (as configured by IDLETHRESHOLD) or timeout.
-This function only works on qemu backend and will sleep on other backends. As
-such it's wasting a lot of time and should be avoided as such. Take it
-as last resort if there is nothing else you can assert on.
+B<Deprecated: > It is recommended to avoid the use of C<wait_idle>. See
+L<https://progress.opensuse.org/issues/5830> for details.
+
+This function will basically just sleep some time as this is the only viable
+option considering all backend implementations. Previously this function tried
+to wait until a test system becomes idle or timeout which only reliably worked
+on qemu backend and was just sleeping on other backends. As such it is wasting
+a lot of time and should be avoided as such.
+
 Default timeout is 19s.
 
 =cut
 
 sub wait_idle {
-    my $timeout = shift || $bmwqemu::idle_timeout;
+    my $timeout = shift || 19;
     $timeout = bmwqemu::scale_timeout($timeout);
 
     # report wait_idle calls while we work on
     # https://progress.opensuse.org/issues/5830
-    cluck "Wait_idle called";
+    cluck "DEPRECATED: wait_idle called, update your test code";
 
     bmwqemu::log_call(timeout => $timeout);
 
-    my $args = {
-        timeout   => $timeout,
-        threshold => get_var('IDLETHRESHOLD', 18)};
-    my $rsp = query_isotovideo('backend_wait_idle', $args);
-    if ($rsp && $rsp->{idle}) {
-        bmwqemu::fctres("idle detected");
-    }
-    else {
-        bmwqemu::fctres("timed out after $timeout");
-    }
+    my $rsp = query_isotovideo('backend_wait_idle', timeout => $timeout);
+    bmwqemu::fctres("slept $timeout seconds");
     return;
 }
 

backend/qemu.pm
@@ -1000,35 +1000,6 @@ sub check_socket {
     return $self->SUPER::check_socket($fh);
 }
 
-sub wait_idle {
-    my ($self, $args) = @_;
-    my $timeout       = $args->{timeout};
-    my $idlethreshold = $args->{threshold};
-    my $prev;
-    my $timesidle       = 0;
-    my $timesidleneeded = 1;
-
-    for my $n (1 .. $timeout) {
-        my ($stat, $systemstat) = @{$self->cpu_stat()};
-        $self->run_capture_loop(1);
-        next unless $stat;
-        $stat += $systemstat;
-        if ($prev) {
-            my $diff = $stat - $prev;
-            bmwqemu::diag("wait_idle $timesidle d=$diff");
-            if ($diff < $idlethreshold) {
-                if (++$timesidle > $timesidleneeded) {    # idle for $x sec
-                                                          #if($diff<2000000) # idle for one sec
-                    return {idle => 1};
-                }
-            }
-            else { $timesidle = 0 }
-        }
-        $prev = $stat;
-    }
-    return;
-}
-
 sub freeze_vm {
     my ($self) = @_;
     # qemu specific - all other backends will crash

bmwqemu.pm
@@ -46,7 +46,6 @@ $| = 1;
 
 
 our $default_timeout = 30;    # assert timeout, 0 is a valid timeout
-our $idle_timeout    = 19;    # wait_idle 0 makes no sense
 
 my @ocrrect;
 

commit 93c5834f242c035c67ea164504fe3230fabfdbe3

Use hash ref in query_isotovideo
With Perl 5.18.2 I get:

```
18:50:30.2820 480 <<< testapi::wait_idle(timeout=19)
18:50:30.3698 480 # Test died: Can't use string ("timeout") as a HASH
ref while "strict refs" in use
```

Regressed in b9045f43a3c8186db352c7a5de8eea501d861a84.

testapi.pm
@@ -1700,7 +1700,7 @@ sub wait_idle {
 
     bmwqemu::log_call(timeout => $timeout);
 
-    my $rsp = query_isotovideo('backend_wait_idle', timeout => $timeout);
+    my $rsp = query_isotovideo('backend_wait_idle', {timeout => $timeout});
     bmwqemu::fctres("slept $timeout seconds");
     return;
 }