3aeee0648b08c2b9d43135bc0c98e8349bad733a
Richard Palethorpe <rpalethorpe@suse.com>
2018-06-07 15:59:41 +0200

    Remove SETUP_FOR_TRAVIS

--- docker/travis_test/run.sh
+++ docker/travis_test/run.sh
@@ -3,10 +3,10 @@
 cd /opt
 # Do a clone to avoid writing to the host's volume when not running in Travis
 # and also to avoid using files not checked into the repo.
-git clone --depth 1 /opt/repo run
+git clone /opt/repo run
 
 cd run
-SETUP_FOR_TRAVIS=1 ./autogen.sh
+./autogen.sh
 make
 # 'coverage' executes all tests and checks code coverage against threshold
 # in Makefile.am
