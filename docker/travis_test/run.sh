db05cd96c5b26f5a35a5f5ff86919fa1db774154
Ettore Di Giacinto <edigiacinto@suse.com>
2018-08-08 09:21:15 +0200

    docker: allow to install modules with cpan with INSTALL_FROM_CPAN=1

--- docker/travis_test/run.sh
+++ docker/travis_test/run.sh
@@ -2,11 +2,20 @@
 
 set -e
 
+INSTALL_FROM_CPAN="${INSTALL_FROM_CPAN:-0}"
+
 # Prepare dir and chdir into it before executing the wanted action
 sudo cp -rd /opt/repo /opt/run
 sudo chown -R $NORMAL_USER:users /opt/run
 
 pushd /opt/run
+[ "$INSTALL_FROM_CPAN" -eq 1 ] && \
+  (cpanm --local-lib=~/perl5 local::lib && cpanm -n --installdeps . ) || \
+  cpanm -n --mirror http://no.where/ --installdeps .
+
+[ "$INSTALL_FROM_CPAN" -eq 1 ] && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
+[ $? -eq 0 ] || echo "Missing dependencies. Please check output above"
+
 ./autogen.sh
 
 /bin/bash -c "$*"
