4f5d3c10769d0c86c02825774a6898a1ce3a22a4
Clemens Famulla-Conrad <cfamullaconrad@suse.de>
2018-08-15 13:45:58 +0200

    Treat ssh_console as serial_terimanl
    Set ssh_console to is_serial_terminal == 1. And use
    virtio_screen as duplex communication.
    
    NET::SSH2 sock can be used with select, but we still need
    the NET::SSH2:Channel for read/write.
    Added a optional paramter to specify the fd which will be used
    in select.
    
    Redirect STDERR output to STDOUT to catch everything in read_until().

--- consoles/ssh_console.pm
+++ consoles/ssh_console.pm
@@ -57,15 +57,18 @@ sub activate {
     my $username = $self->{username};
 
     $self->{ssh} = $self->backend->new_ssh_connection(hostname => $hostname, password => $password, username => $username);
-    $self->{shell} = $self->{ssh}->channel();
-    $self->{shell}->shell();
+    my $chan = $self->{shell} = $self->{ssh}->channel();
+    $chan->pty(1);
+    $chan->shell();
+    print $chan "PS1='# '\n";
+    print $chan "exec 2>&1\n";
 
-    $self->{screen} = consoles::virtio_screen->new($self->{shell});
+    $self->{screen} = consoles::virtio_screen->new($chan, $self->{ssh}->sock);
     return;
 }
 
 sub is_serial_terminal {
-    return 0;
+    return 1;
 }
 
 1;
