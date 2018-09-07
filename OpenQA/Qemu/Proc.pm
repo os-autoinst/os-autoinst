7ce7ffba8c3b9b76b7ca8f64dc8b20389db7d6e7
Richard Palethorpe (rpalethorpe) <richiejp@f-m.fm>
2018-09-07 15:27:27 +0200

    qemu: log that state file is being saved (#1017)
    May help with debugging problems using snapshots and SKIPTO where it appears
    the state file is wrong.

--- OpenQA/Qemu/Proc.pm
+++ OpenQA/Qemu/Proc.pm
@@ -455,6 +455,7 @@ sub save_state {
     my ($self) = @_;
 
     if ($self->has_state) {
+        bmwqemu::fctinfo('Saving QEMU state to ' . STATE_FILE);
         path(STATE_FILE)->spurt($self->serialise_state());
     } else {
         bmwqemu::fctinfo('Refusing to save an empty state file to avoid overwriting a useful one');
