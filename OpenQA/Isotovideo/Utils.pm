6ea63fb3fbea90403588fc2146af8789e07babaa
Oliver Kurz <okurz@suse.de>
2019-01-07 14:22:45 +0100

    Add support to checkout git repos+refspecs
    As a test developer I want to execute an openQA test on a production
    server based on a pull request to a test distribution to not need a
    local openQA instance.
    
    So far I tested this with the included integration test as well as
    manually by specifying a real valid remote test distribution and branch
    which has shown to be successful. The next steps to test would be
    including the full openQA stack with remote workers including their
    caching setup.
    
    Related progress issue: https://progress.opensuse.org/issues/44327

--- OpenQA/Isotovideo/Utils.pm
+++ OpenQA/Isotovideo/Utils.pm
@@ -15,9 +15,9 @@
 
 package OpenQA::Isotovideo::Utils;
 use Mojo::Base -base;
+use Mojo::URL;
 
 use File::Spec;
-use File::Path;
 use Cwd;
 use testapi 'diag';
 use bmwqemu;
@@ -31,6 +31,37 @@ sub calculate_git_hash {
     chdir($dir);
     diag "git hash in $git_repo_dir: $git_hash";
     return $git_hash;
+}
+
+=head2 checkout_git_repo_and_branch
+
+    checkout_git_repo_and_branch($dir [, clone_depth => <num>]);
+
+Takes a test or needles distribution directory parameter and checks out the
+referenced git repository into a local working copy with an additional,
+optional git refspec to checkout. The git clone depth can be specified in the
+argument C<clone_depth> which defaults to 1.
+
+=cut
+sub checkout_git_repo_and_branch {
+    my ($dir, %args) = @_;
+    my $url = Mojo::URL->new($bmwqemu::vars{$dir});
+    # assume we have a remote git URL to clone only if this looks like a remote URL
+    return unless $url->scheme;
+    $args{clone_depth} //= 1;
+    my $branch     = $url->fragment;
+    my $clone_url  = $url->fragment(undef)->to_string;
+    my $local_path = $url->path->parts->[-1] =~ s/.git//r;
+    my $clone_args = "--depth $args{clone_depth}";
+    if ($branch) {
+        diag "Checking out git refspec/branch '$branch'";
+        $clone_args .= " --branch $branch";
+    }
+    if (!-e $local_path) {
+        diag "Cloning git URL '$clone_url' to use as test distribution";
+        qx{env GIT_SSH_COMMAND="ssh -oBatchMode=yes" git clone $clone_args $clone_url};
+    }
+    $bmwqemu::vars{$dir} = File::Spec->rel2abs($local_path);
 }
 
 1;
--- t/14-isotovideo.t
+++ t/14-isotovideo.t
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Test::More;
 use File::Basename;
-use File::Path;
+use File::Path 'remove_tree';
 use Cwd 'abs_path';
 
 my $toplevel_dir = abs_path(dirname(__FILE__) . '/..');
@@ -45,6 +45,20 @@ EOV
     isotovideo;
     is_in_log('\d*: EXIT 1',              'test exited early as requested');
     is_in_log('\d* scheduling.*shutdown', 'schedule has been evaluated');
+};
+
+subtest 'isotovideo with custom git repo parameters specified' => sub {
+    chdir($pool_dir);
+    unlink('vars.json');
+    mkdir('repo.git');
+    qx{git init -q --bare repo.git};
+    # Ensure the checkout folder does not exist so that git clone tries to
+    # create a new checkout on every test run
+    remove_tree('repo');
+    isotovideo(opts => "casedir=file://$pool_dir/repo.git#foo needles_dir=$data_dir _exit_after_schedule=1");
+    is_in_log('git URL.*\<repo\>', 'git repository would be cloned');
+    is_in_log('branch.*foo',       'branch in git repository would be checked out');
+    is_in_log('No scripts',        'the repo actually has no test definitions');
 };
 
 done_testing();
