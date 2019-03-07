cc26052e57bfbf1f63890328c9c88efdb50c031e
Michal Nowak <mnowak@suse.com>
2019-03-07 15:51:27 +0100

    VMware: Delay boot by 10 seconds
    Delaying boot by 10 seconds makes room for test code to configure boot
    device in BIOS Setup Utility.
    
    Validation run:
    * `media_upgrade_sles15@svirt-vmware`: http://nilgiri.suse.cz/tests/411#step/bootloader_svirt/39

--- consoles/sshVirtsh.pm
+++ consoles/sshVirtsh.pm
@@ -566,7 +566,10 @@ __END"
     $self->run_cmd("virsh $remote_vmm undefine --snapshots-metadata " . $self->name . $ignore);
 
     # define the new domain
-    $self->run_cmd("virsh $remote_vmm define $xmlfilename")  && die "virsh define failed";
+    $self->run_cmd("virsh $remote_vmm define $xmlfilename") && die "virsh define failed";
+    if ($self->vmm_family eq 'vmware') {
+        $self->get_cmd_output('echo bios.bootDelay = \"10000\" >> /vmfs/volumes/datastore1/openQA/' . $self->name . '.vmx', {domain => 'sshVMwareServer'});
+    }
     $self->run_cmd("virsh $remote_vmm start " . $self->name) && die "virsh start failed";
 
     $self->backend->start_serial_grab($self->name);
