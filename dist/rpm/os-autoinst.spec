os-autoinst/os-autoinst

commit d0c783e021bf4215b97f0b02c5406059660f0c56

Add tests for backend/qemu.pm
Needs new requirement "Test::MockObject" but we already provide this in
openQA.

dist/rpm/os-autoinst.spec
@@ -37,7 +37,7 @@ Requires:       optipng
 Requires:       perl-base
 Requires:       qemu >= 2.0.0
 Recommends:       tesseract-ocr
-%define t_requires perl(Carp::Always) perl(Data::Dump) perl(Data::Dumper) perl(Crypt::DES) perl(JSON) perl(autodie) perl(Class::Accessor::Fast) perl(Exception::Class) perl(File::Touch) perl(File::Which) perl(IPC::Run::Debug) perl(Net::DBus) perl(Net::SNMP) perl(Net::IP) perl(IPC::System::Simple) perl(Net::SSH2) perl(XML::LibXML) perl(XML::SemanticDiff) perl(Test::Exception) perl(Test::Output) perl(Test::Fatal) perl(Test::Warnings) perl(Pod::Coverage) perl(Test::Pod) perl(Test::MockModule) perl(Devel::Cover) perl(JSON::XS) perl(List::MoreUtils) perl(Mojo::IOLoop::ReadWriteProcess) perl(Test::Mock::Time) perl(Socket::MsgHdr) perl(Cpanel::JSON::XS) perl(IO::Scalar)
+%define t_requires perl(Carp::Always) perl(Data::Dump) perl(Data::Dumper) perl(Crypt::DES) perl(JSON) perl(autodie) perl(Class::Accessor::Fast) perl(Exception::Class) perl(File::Touch) perl(File::Which) perl(IPC::Run::Debug) perl(Net::DBus) perl(Net::SNMP) perl(Net::IP) perl(IPC::System::Simple) perl(Net::SSH2) perl(XML::LibXML) perl(XML::SemanticDiff) perl(Test::Exception) perl(Test::Output) perl(Test::Fatal) perl(Test::Warnings) perl(Pod::Coverage) perl(Test::Pod) perl(Test::MockModule) perl(Test::MockObject) perl(Devel::Cover) perl(JSON::XS) perl(List::MoreUtils) perl(Mojo::IOLoop::ReadWriteProcess) perl(Test::Mock::Time) perl(Socket::MsgHdr) perl(Cpanel::JSON::XS) perl(IO::Scalar)
 BuildRequires:  %t_requires
 Requires:       %t_requires
 BuildRequires:  perl(Mojolicious)

cpanfile
@@ -57,6 +57,7 @@ on 'test' => sub {
   requires 'Test::Fatal';
   requires 'Test::Mock::Time';
   requires 'Test::MockModule';
+  requires 'Test::MockObject';
   requires 'Test::Mojo';
   requires 'Test::More';
   requires 'Test::Output';

t/18-backend-qemu.t
@@ -1,0 +1,48 @@
+#!/usr/bin/perl
+
+use 5.018;
+use strict;
+use warnings;
+
+use Test::More;
+use Test::MockModule;
+use Test::MockObject;
+use Test::Warnings;
+
+BEGIN {
+    unshift @INC, '..';
+}
+
+use backend::qemu;
+
+
+my $proc = Test::MockModule->new('OpenQA::Qemu::Proc');
+$proc->mock(exec_qemu            => undef);
+$proc->mock(connect_qmp          => undef);
+$proc->mock(init_blockdef_images => undef);
+ok(my $backend = backend::qemu->new(), 'backend can be created');
+# disable any graphics display in tests
+$bmwqemu::vars{QEMU_APPEND} = '-nographic';
+# as needed to start backend
+$bmwqemu::vars{VNC} = '1';
+my $jsonrpc = Test::MockModule->new('myjsonrpc');
+$jsonrpc->mock(read_json => undef);
+my $backend_mock = Test::MockModule->new('backend::qemu', no_auto => 1);
+$backend_mock->mock(handle_qmp_command => undef);
+my $distri = Test::MockModule->new('distribution');
+my %called;
+$distri->mock(add_console => sub {
+        $called{add_console}++;
+        my $ret = Test::MockObject->new();
+        $ret->set_true('backend');
+        return $ret;
+});
+$backend_mock->mock(select_console => undef);
+$testapi::distri = distribution->new;
+$backend->{select} = Test::MockObject->new();
+$backend->{select}->set_true('add');
+ok($backend->start_qemu(),      'qemu can be started');
+ok(exists $called{add_console}, 'a console has been added');
+is($called{add_console}, 1, 'one console has been added');
+
+done_testing();

t/Makefile.am
@@ -1,6 +1,6 @@
 AM_MAKEFLAGS = \
 	PERL5OPT="-MDevel::Cover=-db,$(abs_builddir)/cover_db,-ignore,^*\.t|^data\/tests\/*|^fake\/tests\/*" \
 	PERL5LIB="..:../ppmclibs:../ppmclibs/blib/arch/auto/tinycv:$$PERL5LIB"
-TESTS = 00-compile-check-all.t 01-test_needle.t 02-test_ocr.t 03-testapi.t 04-check_vars_docu.t 05-pod.t 06-pod-coverage.t 07-commands.t 08-autotest.t 09-lockapi.t 10-terminal.t 10-test-image-conversion-benchmark.t 11-image-ppm.t 12-bmwqemu.t 13-osutils.t 14-isotovideo.t 15-logging.t 16-send_with_fd.t 17-basetest.t 18-qemu.t 18-qemu-options.t 19-isotovideo-command-processing.t 20-openqa-benchmark-stopwatch-utils.t 21-needle-downloader.t 22-svirt.t 23-baseclass.t 99-full-stack.t
+TESTS = 00-compile-check-all.t 01-test_needle.t 02-test_ocr.t 03-testapi.t 04-check_vars_docu.t 05-pod.t 06-pod-coverage.t 07-commands.t 08-autotest.t 09-lockapi.t 10-terminal.t 10-test-image-conversion-benchmark.t 11-image-ppm.t 12-bmwqemu.t 13-osutils.t 14-isotovideo.t 15-logging.t 16-send_with_fd.t 17-basetest.t 18-qemu.t 18-backend-qemu.t 18-qemu-options.t 19-isotovideo-command-processing.t 20-openqa-benchmark-stopwatch-utils.t 21-needle-downloader.t 22-svirt.t 23-baseclass.t 99-full-stack.t
 
 EXTRA_DIST = $(TESTS)

commit ab7d165858d4a65fd65f92f26b362989033db048

spec: Fix tests in OBS by excluding 18-backend-qemu.t
Fixes regression introduced by d0c783e0.

dist/rpm/os-autoinst.spec
@@ -117,8 +117,9 @@ sed '/tidy/d' -i Makefile
 rm tools/lib/perlcritic/Perl/Critic/Policy/*.pm
 
 # don't require qemu within OBS
-cp t/05-pod.t t/18-qemu-options.t
-cp t/05-pod.t t/99-full-stack.t
+for i in 18-qemu-options 18-backend-qemu 99-full-stack; do
+    cp t/05-pod.t t/${i}.t
+done
 
 # should work offline
 for p in $(cpanfile-dump); do rpm -q --whatprovides "perl($p)"; done