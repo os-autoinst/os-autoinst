44f2d9989996ed5c6036a448bd9a81e1d606c396
Oliver Kurz <okurz@suse.de>
2020-03-12 22:50:26 +0100

    Add support for cloning and checking out a single git commit through CASEDIR
    Using the test variable CASEDIR we can already clone a git repository
    and checkout an arbitrary "refspec", e.g. branch or tag.
    
    This commit adds support for specifying a git commit hash as well. Then the
    repo is cloned first with a limited depth and with exponentially growing depths
    git objects are fetched until the specified git commit is found and can be
    checked out or the process stops with an error if the git commit can not be
    found at all.
    
    This feature should be helpful for openQA where TEST_GIT_REFSPEC can to be
    easily used to check out a single when multiple openQA worker instances share a
    common folder for tests regardless if the openQA caching is used for tests or
    not. But also this approach is more generic as an arbitrary repository can be
    used. Where possible TEST_GIT_REFSPEC can be still more efficient though as no
    remote git action is necessary if a git commit is already available in a local
    git working copy.
    
    Example output:
    
    ```
    $ isotovideo -d casedir=file:///home/okurz/local/os-autoinst/opensuse#cc6702c42
    [2020-03-12T22:48:21.233 CET] [debug] Current version is 9408ca4ed76304eda217e01e8617d59ac53512f6 [interface v20]
    [2020-03-12T22:48:21.236 CET] [debug] Setting forced test parameter CASEDIR -> file:///home/okurz/local/os-autoinst/opensuse#cc6702c42
    [2020-03-12T22:48:21.237 CET] [debug] Checking out git refspec/branch 'cc6702c42'
    [2020-03-12T22:48:21.237 CET] [debug] Cloning git URL 'file:///home/okurz/local/os-autoinst/opensuse' to use as test distribution
    [2020-03-12T22:48:21.258 CET] [debug] Fetching more remote objects to ensure availability of 'cc6702c42'
    Cloning into 'opensuse'...
    remote: Counting objects: 2659, done.
    remote: Compressing objects: 100% (2382/2382), done.
    remote: Total 2659 (delta 646), reused 899 (delta 209)
    Receiving objects: 100% (2659/2659), 84.39 MiB | 58.03 MiB/s, done.
    Resolving deltas: 100% (646/646), done.
    [2020-03-12T22:48:29.895 CET] [debug] git fetch: remote: Counting objects: 26, done.
    remote: Compressing objects: 100% (25/25), done.
     remote: Total 26 (delta 22), reused 4 (delta 1)
    
    [2020-03-12T22:48:29.986 CET] [debug] git fetch: remote: Counting objects: 36, done.
    remote: Compressing objects: 100% (31/31), done.
     remote: Total 36 (delta 29), reused 12 (delta 5)
    
    [2020-03-12T22:48:30.495 CET] [debug] git fetch: remote: Counting objects: 974, done.
    remote: Compressing objects: 100% (850/850), done.
    remote: Total 974 (delta 830), reused 199 (delta 110)
    Receiving objects: 100% (974/974), 198.06 KiB | 11.00 MiB/s, done.
    Resolving deltas: 100% (830/830), completed with 582 local objects.
    
    [2020-03-12T22:48:31.207 CET] [debug] git fetch: remote: Counting objects: 1259, done.
    remote: Compressing objects: 100% (907/907), done.
    remote: Total 1259 (delta 943), reused 580 (delta 339)
    Receiving objects: 100% (1259/1259), 4.32 MiB | 58.17 MiB/s, done.
    Resolving deltas: 100% (943/943), completed with 352 local objects.
    
    [2020-03-12T22:48:31.220 CET] [debug] git hash in /home/okurz/local/os-autoinst/os-autoinst/local/no_vars/opensuse: 6f11980f82e5a63889ec1654c7d15bf6d3f9fa6b
    ```
    
    Related progress issue: https://progress.opensuse.org/issues/19720

--- OpenQA/Isotovideo/Utils.pm
+++ OpenQA/Isotovideo/Utils.pm
@@ -55,17 +55,38 @@ sub checkout_git_repo_and_branch {
 
     $args{clone_depth} //= 1;
 
-    my $branch     = $url->fragment;
-    my $clone_url  = $url->fragment(undef)->to_string;
-    my $local_path = $url->path->parts->[-1] =~ s/.git//r;
-    my $clone_args = "--depth $args{clone_depth}";
+    my $branch      = $url->fragment;
+    my $clone_url   = $url->fragment(undef)->to_string;
+    my $local_path  = $url->path->parts->[-1] =~ s/.git//r;
+    my $clone_cmd   = 'env GIT_SSH_COMMAND="ssh -oBatchMode=yes" git clone';
+    my $clone_args  = "--depth $args{clone_depth}";
+    my $branch_args = '';
     if ($branch) {
         diag "Checking out git refspec/branch '$branch'";
-        $clone_args .= " --branch $branch";
+        $branch_args = " --branch $branch";
     }
     if (!-e $local_path) {
         diag "Cloning git URL '$clone_url' to use as test distribution";
-        qx{env GIT_SSH_COMMAND="ssh -oBatchMode=yes" git clone $clone_args $clone_url};
+        my @out = qx{$clone_cmd $clone_args $branch_args $clone_url 2>&1};
+        if ($branch && grep /warning: Could not find remote branch/, @out) {
+            # maybe we misspelled or maybe someone gave a commit hash instead
+            # for which we need to take a different approach by downloading the
+            # repository in the necessary depth until we can reach the commit
+            # References:
+            # * https://stackoverflow.com/questions/18515488/how-to-check-if-the-commit-exists-in-a-git-repository-by-its-sha-1
+            # * https://stackoverflow.com/questions/26135216/why-isnt-there-a-git-clone-specific-commit-option
+            diag "Fetching more remote objects to ensure availability of '$branch'";
+            qx{$clone_cmd $clone_args $clone_url};
+            while (qx[git -C $local_path cat-file -e $branch^{commit} 2>&1] =~ /Not a valid object/) {
+                $args{clone_depth} *= 2;
+                @out = qx[git -C $local_path fetch --progress --depth=$args{clone_depth} 2>&1];
+                diag "git fetch: @out";
+                die "Could not find '$branch' in complete history" if grep /remote: Total 0/, @out;
+            }
+        }
+        else {
+            diag "@out\n";
+        }
     }
     else {
         diag "Skipping to clone '$clone_url'; $local_path already exists";
