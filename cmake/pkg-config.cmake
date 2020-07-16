8a353fa0c9d334afebacec5041d8e593a7f13dbb
Marius Kittler <mkittler@suse.de>
2020-07-16 18:26:39 +0200

    CMake: Improve dependency lookup via pkg-config
    * Move code into its own module instead of duplicating it
    * Use _CFLAGS_OTHER and _LDFLAGS_OTHER instead of just e.g. _LDFLAGS to
      avoid duplicated flags
    * Add fallback for older CMake versions which are not supporting the
      target_link_options command yet
    
    The change has been tested locally by manipulating the theoraenc
    pkg-config file, e.g.: `Libs: -L${libdir} -ltheoraenc -foo -bar`
    
    The flags are used as expected in both branches:
    ```
    c++: error: unrecognized command-line option '-foo'
    c++: error: unrecognized command-line option '-bar'
    ```

---
+++ cmake/pkg-config.cmake
@@ -0,0 +1,18 @@
+cmake_minimum_required(VERSION 3.3.0)
+
+include(FindPkgConfig)
+
+function (target_use_pkg_config_module TARGET PACKAGE)
+    pkg_check_modules(PKG_CONFIG "${PACKAGE}")
+    target_link_libraries("${TARGET}" PRIVATE opencv_core opencv_imgcodecs ${PKG_CONFIG_LIBRARIES})
+    target_include_directories("${TARGET}" PRIVATE ${PKG_CONFIG_INCLUDE_DIRS})
+    target_compile_options("${TARGET}" PRIVATE ${PKG_CONFIG_CFLAGS_OTHER})
+    if (COMMAND target_link_options)
+        target_link_options("${TARGET}" PRIVATE ${PKG_CONFIG_LDFLAGS_OTHER})
+    elseif (PKG_CONFIG_LDFLAGS_OTHER)
+        # fallback for older CMake versions not supporting target_link_options
+        foreach (FLAG ${PKG_CONFIG_LDFLAGS_OTHER})
+            set_property(TARGET "${TARGET}" APPEND_STRING PROPERTY LINK_FLAGS " ${FLAG}")
+        endforeach ()
+    endif ()
+endfunction ()
