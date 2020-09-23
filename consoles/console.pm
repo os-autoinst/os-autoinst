os-autoinst/os-autoinst

commit c2ba84c4d1cc477cd3b75c7033606497df32fae6

Delete some redundant whitespace in consoles

consoles/console.pm
@@ -50,14 +50,11 @@ sub new {
 
 sub init {
     my ($self) = @_;
-    if ($self->{args}->{tty}) {
-        if (check_var('VIRSH_VMM_FAMILY', 'hyperv')) {
-            $self->{console_key} = "alt-f" . $self->{args}->{tty};
-        }
-        else {
-            $self->{console_key} = "ctrl-alt-f" . $self->{args}->{tty};
-        }
-    }
+    return unless $self->{args}->{tty};
+    # Special keys like Ctrl-Alt-Fx are not passed to the VM by xfreerdp.
+    # That means switch from graphical to console is not possible on Hyper-V.
+    my $hotkey = check_var('VIRSH_VMM_FAMILY', 'hyperv') ? 'alt-f' : 'ctrl-alt-f';
+    $self->{console_key} = $hotkey . $self->{args}->{tty};
 }
 
 # SUT was e.g. rebooted
@@ -74,8 +71,7 @@ sub screen {
 }
 
 # to be overloaded
-sub trigger_select {
-}
+sub trigger_select { }
 
 sub select {
     my ($self) = @_;
@@ -91,22 +87,15 @@ sub select {
     return $activated;
 }
 
-sub activate {
-    my ($self) = @_;
-    return;
-}
+sub activate { }
 
-sub is_serial_terminal {
-    return 0;
-}
+sub is_serial_terminal { 0 }
 
 sub set_args {
     my ($self, %args) = @_;
 
     my $my_args = $self->{args};
-    for my $arg (keys %args) {
-        $my_args->{$arg} = $args{$arg};
-    }
+    $self->{args}->{$_} = $args{$_} for (keys %args);
     # no need to send changes to right process; console proxy already takes care
     # that this method is called in the right process
 }

consoles/ttyConsole.pm
@@ -1,5 +1,5 @@
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2015 SUSE LLC
+# Copyright © 2012-2020 SUSE LLC
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -25,21 +25,9 @@ use base 'consoles::console';
 require IPC::System::Simple;
 use testapi 'check_var';
 
-# to be overloaded
 sub trigger_select {
     my ($self) = @_;
-    my $key;
-
-    # Special keys like Ctrl-Alt-Fx are not passed to the VM by xfreerdp.
-    # That means switch from graphical to console is not possible on Hyper-V.
-    if (check_var('VIRSH_VMM_FAMILY', 'hyperv')) {
-        $key = "alt-f" . $self->{args}->{tty};
-    }
-    else {
-        $key = "ctrl-alt-f" . $self->{args}->{tty};
-    }
-
-    $self->screen->send_key({key => $key});
+    $self->screen->send_key({key => $self->{console_key}});
     return;
 }
 

consoles/VNC.pm
@@ -1161,13 +1161,8 @@ sub _receive_colour_map {
     return 1;
 }
 
-sub _receive_bell {
-    my $self = shift;
-
-    # And discard it...
-
-    return 1;
-}
+# Discard the bell signal
+sub _receive_bell { 1 }
 
 sub _receive_ikvm_session {
     my $self = shift;
@@ -1237,3 +1232,5 @@ under the same terms as Perl itself.
 
 Copyright (C) 2014-2017 Stephan Kulow (coolo@suse.de)
 adapted to be purely useful for qemu/openqa
+
+Copyright © 2017-2020 SUSE LLC

consoles/network_console.pm
@@ -1,5 +1,5 @@
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2019 SUSE LLC
+# Copyright © 2019-2020 SUSE LLC
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -38,7 +38,6 @@ sub activate {
 }
 
 # to be overwritten
-sub connect_remote {
-}
+sub connect_remote { }
 
 1;

consoles/remoteVnc.pm
@@ -1,5 +1,5 @@
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2015 SUSE LLC
+# Copyright © 2012-2020 SUSE LLC
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -42,7 +42,6 @@ sub activate {
 }
 
 # override
-sub select {
-}
+sub select { }
 
 1;

consoles/s3270.pm
@@ -1,5 +1,5 @@
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2016 SUSE LLC
+# Copyright © 2012-2020 SUSE LLC
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -71,9 +71,7 @@ sub finish {
 sub send_3270 {
     my ($self, $command, %arg) = @_;
     $command //= '';
-    if (!exists $arg{command_status}) {
-        $arg{command_status} = "ok";
-    }
+    $arg{command_status} //= "ok";
     confess "command_status must be 'ok' or 'error' or 'any', got $arg{command_status}."
       unless (grep $arg{command_status}, ['ok', 'error', 'any']);
 
@@ -170,9 +168,7 @@ sub expect_3270 {
 
     if ($arg{clear_buffer}) {
         my $n = $self->{raw_expect_queue}->pending();
-        if ($n) {
-            $self->{raw_expect_queue}->dequeue_nb($n);
-        }
+        $self->{raw_expect_queue}->dequeue_nb($n) if $n;
     }
     my $result     = [];
     my $start_time = time();
@@ -193,10 +189,7 @@ sub expect_3270 {
             my $input_line  = pop @$co;
             my @output_area = @$co;
 
-
-            if (defined $arg{delete_lines}) {
-                @output_area = grep !/$arg{delete_lines}/, @output_area;
-            }
+            @output_area = grep !/$arg{delete_lines}/, @output_area if defined $arg{delete_lines};
 
             if (@output_area > 0) {
                 $self->{raw_expect_queue}->enqueue(@output_area);
@@ -214,11 +207,7 @@ sub expect_3270 {
             if ($status_line !~ /$arg{expected_status}/) {
                 # if the timeout is not over, wait for more output
                 my $elapsed_time = time() - $start_time;
-                if ($elapsed_time < $arg{timeout}) {
-                    if ($self->wait_output($arg{timeout} - $elapsed_time)) {
-                        next;
-                    }
-                }
+                next if $elapsed_time < $arg{timeout} && $self->wait_output($arg{timeout} - $elapsed_time);
 
                 # flush the buffer for debugging:
                 while (my $line = $self->{raw_expect_queue}->dequeue_nb()) {
@@ -249,15 +238,11 @@ sub expect_3270 {
         my $line;
         while ($line = $self->{raw_expect_queue}->dequeue_nb()) {
             push @$result, $line;
-            if (!defined $line || $line =~ /$arg{output_delim}/) {
-                last;
-            }
+            last if !defined $line || $line =~ /$arg{output_delim}/;
         }
 
         # If we matched the 'output_delim', we are done.
-        if (defined $line) {
-            last;
-        }
+        last if defined $line;
 
         # The queue is empty. If we got so far and we had some output on the
         # screen the last time, clear the screen so we don't grab the same
@@ -268,10 +253,7 @@ sub expect_3270 {
         # last Snap(Ascii) and to thus avoid duplicate lines.
 
         # For now we have to live with having a clear screen.
-
-        if ($we_had_new_output) {
-            $self->ensure_screen_update();
-        }
+        $self->ensure_screen_update() if $we_had_new_output;
 
         # wait for new output from the host.
         my $elapsed_time = time() - $start_time;
@@ -291,25 +273,16 @@ sub wait_output {
     my ($self, $timeout) = @_;
     $timeout //= 0;    # just poll
     my $r = $self->send_3270("Wait($timeout,Output)", command_status => 'any');
-
-    if ($r->{command_status} eq 'ok') {
-        return 1;
-    }
-    else {
-        return 0
-          if $r->{command_output}[0] eq 'Wait: Timed out';
-        confess "has the s3270 wait timeout failure response changed?\n" . Dumper $r;
-    }
+    return 1 if $r->{command_status} eq 'ok';
+    return 0 if $r->{command_output}[0] eq 'Wait: Timed out';
+    confess "has the s3270 wait timeout failure response changed?\n" . Dumper $r;
 }
 
 ###################################################################
 
 sub sequence_3270 {
     my ($self, @commands) = @_;
-
-    foreach my $command (@commands) {
-        $self->send_3270($command);
-    }
+    $self->send_3270($_) for (@commands);
 }
 
 # map the terminal status of x3270 to a hash
@@ -373,19 +346,10 @@ sub _connect_3270 {
     my ($self, $host) = @_;
 
     my $r = $self->send_3270("Connect($host)");
-
-    if ($r->{terminal_status} !~ / C\($host\) /) {
-        confess "connect to host >$host< failed.\n" . join("\n", @$r);
-    }
-
+    confess "connect to host >$host< failed.\n" . join("\n", @$r) if $r->{terminal_status} !~ / C\($host\) /;
     $self->send_3270("Wait(InputField)");
-
     $r = $self->expect_3270();
-
-    if (!grep /Fill in your USERID and PASSWORD and press ENTER/, @$r) {
-        confess "doesn't look like zVM login prompt.";
-    }
-
+    confess "doesn't look like zVM login prompt." unless grep /Fill in your USERID and PASSWORD and press ENTER/, @$r;
     return $r;
 }
 
@@ -434,9 +398,7 @@ sub connect_and_login {
     ###################################################################
     # try to connect exactly trice
     for (my $count = 0; $count += 1;) {
-
         $r = $self->_connect_3270($self->{zVM_host});
-
         $r = $self->_login_guest($self->{guest_user}, $self->{guest_login});
 
         # bail out if the host is in use

consoles/serial_screen.pm
@@ -1,4 +1,4 @@
-# Copyright © 2016-2019 SUSE LLC
+# Copyright © 2016-2020 SUSE LLC
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -332,12 +332,8 @@ sub peak {
     return $self->{carry_buffer};
 }
 
-sub current_screen {
-    return 0;
-}
+sub current_screen { 0 }
 
-sub request_screen_update {
-    return;
-}
+sub request_screen_update { }
 
 1;

consoles/virtio_terminal.pm
@@ -1,4 +1,4 @@
-# Copyright © 2016-2019 SUSE LLC
+# Copyright © 2016-2020 SUSE LLC
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -186,8 +186,6 @@ sub activate {
     return;
 }
 
-sub is_serial_terminal {
-    return 1;
-}
+sub is_serial_terminal { 1 }
 
 1;

commit 7cb84b7ec21c29ddcf3576697167a6bafb1d0874

Fix tty key definition after initialization
This fixes a regression introduced in c2ba84c4 when tests update the
"tty" of a console after initialization. By introducing a method to read
the console key combination we can evaluate the configuration
dynamically.

consoles/console.pm
@@ -50,11 +50,9 @@ sub new {
 
 sub init {
     my ($self) = @_;
-    return unless $self->{args}->{tty};
     # Special keys like Ctrl-Alt-Fx are not passed to the VM by xfreerdp.
     # That means switch from graphical to console is not possible on Hyper-V.
-    my $hotkey = check_var('VIRSH_VMM_FAMILY', 'hyperv') ? 'alt-f' : 'ctrl-alt-f';
-    $self->{console_key} = $hotkey . $self->{args}->{tty};
+    $self->{console_hotkey} = check_var('VIRSH_VMM_FAMILY', 'hyperv') ? 'alt-f' : 'ctrl-alt-f';
 }
 
 # SUT was e.g. rebooted
@@ -106,6 +104,12 @@ sub set_tty {
     $self->{args}->{tty} = $tty;
     # no need to send changes to right process; console proxy already takes care
     # that this method is called in the right process
+}
+
+sub console_key {
+    my ($self) = @_;
+    return undef unless $self->{console_hotkey} && $self->{args}->{tty};
+    return $self->{console_hotkey} . $self->{args}->{tty};
 }
 
 1;

consoles/ttyConsole.pm
@@ -27,7 +27,7 @@ use testapi 'check_var';
 
 sub trigger_select {
     my ($self) = @_;
-    $self->screen->send_key({key => $self->{console_key}});
+    $self->screen->send_key({key => $self->console_key});
     return;
 }
 

t/10-terminal.t
@@ -238,7 +238,7 @@ sub test_terminal_directly {
     $tb->reset;
 
     my $term = consoles::virtio_terminal->new('unit-test-console', {tty => 3});
-    report_child_test(ok => $term->{console_key} eq "ctrl-alt-f3", 'console_key set correct');
+    report_child_test(ok => $term->console_key eq "ctrl-alt-f3", 'console_key set correct');
 
     $term->activate;
     my $scrn = $term->screen;

t/22-svirt.t
@@ -62,6 +62,7 @@ is_deeply($svirt_sut_console, {
         activated       => 0,
         args            => {},
         class           => 'consoles::sshVirtshSUT',
+        console_hotkey  => 'ctrl-alt-f',
         libvirt_domain  => 'openQA-SUT-1',
         serial_port_no  => 1,
         testapi_console => 'sut-serial',