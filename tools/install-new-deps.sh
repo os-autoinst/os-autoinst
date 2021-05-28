af8ae390557e2e706e232f02b22b842be27f8bfe
Oliver Kurz <okurz@suse.de>
2021-05-28 19:38:27 +0200

    Fix all shell style issues reported by ShellCheck

--- tools/install-new-deps.sh
+++ tools/install-new-deps.sh
@@ -1,23 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script is used for CI testing.
 # Used to install new dependencies.
 # If there are new dependencies, they won't be installed in the docker
 # container yet, so we just install all deps again.
 
-set -e
+set -euo pipefail
 
 OLDDEPS=/tmp/deps.txt
 NEWDEPS=/tmp/new-deps.txt
 DIFFDEPS=/tmp/diff-deps.txt
 
-source ./tools/tools.sh
+. ./tools/tools.sh
 
 listdeps > $OLDDEPS
 
+# shellcheck disable=SC2207
 DEPS=($(getdeps_docker))
 
-sudo zypper --no-refresh install -y -C ${DEPS[@]}
+sudo zypper --no-refresh install -y -C "${DEPS[@]}"
 
 listdeps > $NEWDEPS
 
