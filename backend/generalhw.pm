d1b34b4a1437f9ab31458966d03e2ab3789fddc3
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2021-08-10 13:30:16 +0200

    Add video-stream console support to generalhw backend
    This allows to control real hardware via various KVM solutions that
    do not support VNC natively. This includes DYI one, built from a
    HDMI->IP converter and a RPi playing as USB gadget (keyboard/mouse).

--- backend/generalhw.pm
+++ backend/generalhw.pm
@@ -27,7 +27,7 @@ sub get_cmd ($self, $cmd) {
     my $dir = $bmwqemu::vars{GENERAL_HW_CMD_DIR} or die 'Need variable GENERAL_HW_CMD_DIR';
     die 'GENERAL_HW_CMD_DIR is not pointing to a directory' unless -d $dir;
 
-    my %GENERAL_HW_ARG_VARIABLES_BY_CMD = ('GENERAL_HW_FLASH_CMD' => 'GENERAL_HW_FLASH_ARGS', 'GENERAL_HW_SOL_CMD' => 'GENERAL_HW_SOL_ARGS', 'GENERAL_HW_POWERON_CMD' => 'GENERAL_HW_POWERON_ARGS', 'GENERAL_HW_POWEROFF_CMD' => 'GENERAL_HW_POWEROFF_ARGS');
+    my %GENERAL_HW_ARG_VARIABLES_BY_CMD = ('GENERAL_HW_FLASH_CMD' => 'GENERAL_HW_FLASH_ARGS', 'GENERAL_HW_SOL_CMD' => 'GENERAL_HW_SOL_ARGS', 'GENERAL_HW_INPUT_CMD' => 'GENERAL_HW_INPUT_ARGS', 'GENERAL_HW_POWERON_CMD' => 'GENERAL_HW_POWERON_ARGS', 'GENERAL_HW_POWEROFF_CMD' => 'GENERAL_HW_POWEROFF_ARGS');
     my $args = $bmwqemu::vars{$GENERAL_HW_ARG_VARIABLES_BY_CMD{$cmd}} if $bmwqemu::vars{$GENERAL_HW_ARG_VARIABLES_BY_CMD{$cmd}};
 
     $cmd = $bmwqemu::vars{$cmd} or die "Need test variable '$cmd'";
@@ -117,6 +117,24 @@ sub compute_hdd_args ($self) {
     return \@hdd_args;
 }
 
+sub reconnect_video_stream ($self, @) {
+
+    my $input_cmd;
+    $input_cmd = $bmwqemu::vars{GENERAL_HW_INPUT_CMD} if ($bmwqemu::vars{GENERAL_HW_INPUT_CMD});
+    my $vnc = $testapi::distri->add_console(
+        'sut',
+        'video-stream',
+        {
+            url => $bmwqemu::vars{GENERAL_HW_VIDEO_STREAM_URL},
+            connect_timeout => 50,
+            input_cmd => $input_cmd,
+        });
+    $vnc->backend($self);
+    $self->select_console({testapi_console => 'sut'});
+
+    return 1;
+}
+
 sub do_start_vm ($self, @) {
     $self->truncate_serial_file;
     if ($bmwqemu::vars{GENERAL_HW_FLASH_CMD}) {
@@ -128,6 +146,7 @@ sub do_start_vm ($self, @) {
     }
     $self->restart_host;
     $self->relogin_vnc if ($bmwqemu::vars{GENERAL_HW_VNC_IP});
+    $self->reconnect_video_stream if ($bmwqemu::vars{GENERAL_HW_VIDEO_STREAM_URL});
     $self->start_serial_grab if ($bmwqemu::vars{GENERAL_HW_VNC_IP} || $bmwqemu::vars{GENERAL_HW_SOL_CMD});
     return {};
 }
@@ -135,6 +154,7 @@ sub do_start_vm ($self, @) {
 sub do_stop_vm ($self, @) {
     $self->poweroff_host;
     $self->stop_serial_grab() if ($bmwqemu::vars{GENERAL_HW_VNC_IP} || $bmwqemu::vars{GENERAL_HW_SOL_CMD});
+    $self->disable_consoles;
     return {};
 }
 
