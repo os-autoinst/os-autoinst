os-autoinst/os-autoinst

commit dee5d4cf5b286a8bd173b2d515a23035dbc19bf7

mmapi: Prevent API failures due to short server unavailabilities
When retrying multi-machine API calls to a server we should wait between
retries to increase the likelyhood to encounter a server that is able to
accept the request again.

Related progress issue: https://progress.opensuse.org/issues/96010

mmapi.pm
@@ -31,8 +31,9 @@ require bmwqemu;
 use Mojo::UserAgent;
 use Mojo::URL;
 
-our $retry_count   = $ENV{OS_AUTOINST_MMAPI_RETRY_COUNT}   // 3;
-our $poll_interval = $ENV{OS_AUTOINST_MMAPI_POLL_INTERVAL} // 1;
+our $retry_count    = $ENV{OS_AUTOINST_MMAPI_RETRY_COUNT}    // 3;
+our $retry_interval = $ENV{OS_AUTOINST_MMAPI_RETRY_INTERVAL} // 3;
+our $poll_interval  = $ENV{OS_AUTOINST_MMAPI_POLL_INTERVAL}  // 1;
 
 our $url;
 
@@ -92,6 +93,8 @@ sub api_call_2 {
         $tx  = $ua->$method($ua_url);
         $res = $tx->res;
         last if $res->code && ($expected_codes // $CODES_EXPECTED_BY_DEFAULT)->{$res->code};
+        bmwqemu::diag("api_call_2 failed, retries left: $tries of $retry_count");
+        sleep $retry_interval;
     }
     return $tx;
 }

t/30-mmapi.t
@@ -24,6 +24,7 @@ BEGIN {
     $ENV{OS_AUTOINST_LOCKAPI_RETRY_COUNT}    = 1;
     $ENV{OS_AUTOINST_LOCKAPI_RETRY_INTERVAL} = 0;
     $ENV{OS_AUTOINST_MMAPI_RETRY_COUNT}      = 1;
+    $ENV{OS_AUTOINST_MMAPI_RETRY_INTERVAL}   = 0;
     $ENV{OS_AUTOINST_MMAPI_POLL_INTERVAL}    = 0;
     $ENV{MOJO_CONNECT_TIMEOUT}               = 0.01;
 }

commit 7cd23f8180370fe3633292af9cca66cbd81a8588

mmapi: Allow for longer and more retries after observed failures
The initial retry to mmapi calls as th the sleep periods introduced in
dee5d4cf seem to still run into temporary connection outages which we
should be able to pass with more and longer retries.

Related progress issue: https://progress.opensuse.org/issues/98940

mmapi.pm
@@ -30,8 +30,8 @@ require bmwqemu;
 use Mojo::UserAgent;
 use Mojo::URL;
 
-our $retry_count    = $ENV{OS_AUTOINST_MMAPI_RETRY_COUNT}    // 3;
-our $retry_interval = $ENV{OS_AUTOINST_MMAPI_RETRY_INTERVAL} // 3;
+our $retry_count    = $ENV{OS_AUTOINST_MMAPI_RETRY_COUNT}    // 30;
+our $retry_interval = $ENV{OS_AUTOINST_MMAPI_RETRY_INTERVAL} // 10;
 our $poll_interval  = $ENV{OS_AUTOINST_MMAPI_POLL_INTERVAL}  // 1;
 
 our $url;