27920044de4a1f3236e02e6fb51d229b5ef0d533
Oliver Kurz <okurz@suse.de>
2021-09-29 18:01:34 +0200

    Replace all possible occurences of docker with generic version

--- tools/tools.sh
+++ tools/tools.sh
@@ -2,8 +2,8 @@
 
 # Installation tools
 
-# Get all dependencies needed for the docker environment
-getdeps_docker() {
+# Get all dependencies needed for the container environment
+getdeps_container() {
     perl -MYAML::PP=Load -0wE'
         my $d = Load<>;
         say for sort grep {!/^%/} map { keys %{$d->{$_."_requires"}} }
