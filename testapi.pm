03fe3bce13bc6d331cd66181822cd2d24a76477c
Clemens Famulla-Conrad <cfamullaconrad@suse.de>
2021-10-07 12:37:49 +0200

    testapi: Add die_on_timeout option to testapi::script::run()
    The testapi function `script_run()` is widely used to execute one command
    after the other, without checking for exit code. But it could also
    happen that the command timed out and in such situation the
    test-developer need to handle it separately.
    To solve this, introduce a new option `die_on_timeout`. The default is now like
    we have it currently **don't die**. But we will see a log message like:
    ```
    [2021-11-22T14:47:47.739305+01:00] [warn] !!! testapi::script_run:
    DEPRECATED call of script_run() in tests/clemix/script_run.pm:16 add
    `die_on_timeout => ?` to avoid this warning
    ```
    if the user doesn't specified `die_on_timeout=>(0|1)` explicit.
    
    ==== Typical miss usage of script_run() ====
    
    Example of possible miss usage of `script_run()`:
    ```perl
    script_run('command_a');
    script_run('command_b');
    ```
    if the first call just times out, it is very likely that the shell is
    _not_ at the prompt for command_b. Thus something unexpected happen,
    even if we don't care of the exit value from `command_a`.
    
    Also widely used is:
    ```perl
    if (script_run(...) != 0) {
      some_sane_error_handling();
    }
    ```
    But in case of timeout, this error handling isn't triggered and the test
    just continue.
    
    With this change, we also need to increase the API version specified in
    OpenQA::Isotovideo::Interface::version .
    
    == testapi.t: Add test for script_run() timeout behavior ==
    
    Changed other `script_run()` calls to avoid unexpected warnings.
    
    We add `quiet => 1` to the script_run() calls, because otherwise the test
    `result (to create a new needl from) has been added` failed, as the
    detail picture has the next number.

--- testapi.pm
+++ testapi.pm
@@ -991,22 +991,25 @@ sub assert_script_run {
 
 =head2 script_run
 
-  script_run($cmd [, timeout => $timeout] [, output => ''] [, quiet => $quiet]);
+  script_run($cmd [, timeout => $timeout] [, output => ''] [, quiet => $quiet] [, die_on_timeout => -1]);
 
 Deprecated mode
 
   script_run($cmd [, $timeout]);
 
-Run C<$cmd> (in the default implementation, by assuming the console prompt and
-typing the command). If C<$timeout> is greater than 0, wait for that length of
-time for execution to complete (otherwise, returns undef immediately). See
-C<< distri->script_run >> for default timeout.
+Run C<$cmd> (in the default implementation, by assuming the console prompt and typing
+the command). If C<$timeout> is greater than 0, wait for that length of time for
+execution to complete.
 
 C<$output> can be used as an explanatory text that will be displayed with the execution of
 the command.
 
-<Returns> exit code received from I<$cmd>, or undef if $timeout is 0 or execution
-does not complete within $timeout.
+With C<die_on_timeout> -1 (default) a warning will be printed to log. To avoid
+this warning, set it explicit to 0. With C<die_on_timeout> equal to 1, this command
+throw an exception, if timeout expires.
+
+<Returns> exit code received from I<$cmd> or undef if C<$timeout> is 0 or timeout 
+expired and C<die_on_timeout> is not C<1>.
 
 I<The implementation is distribution specific and not always available.>
 
@@ -1020,13 +1023,31 @@ sub script_run {
     my $cmd = shift;
     my %args = compat_args(
         {
-            timeout => undef,
+            timeout => $bmwqemu::default_timeout,
             output => '',
-            quiet => testapi::get_var('_QUIET_SCRIPT_CALLS')
+            quiet => testapi::get_var('_QUIET_SCRIPT_CALLS'),
+            die_on_timeout => -1,
         }, ['timeout'], @_);
 
     bmwqemu::log_call(cmd => $cmd, %args);
-    return $distri->script_run($cmd, %args);
+    my $die_on_timeout = delete $args{die_on_timeout};
+    my $ret = $distri->script_run($cmd, %args);
+    if ($args{timeout} > 0) {
+        if ($die_on_timeout > 0) {
+            croak("command '$cmd' timed out") if !defined($ret);
+        } else {
+            # This is to warn users of script_run(), if they do not use
+            # die_on_timeout => 0 explicit.
+            if ($die_on_timeout < 0) {
+                my ($package, $filename, $line) = caller;
+                my $casedir = testapi::get_var(CASEDIR => '');
+                $filename =~ s%^\Q$casedir\E/%%;
+                bmwqemu::fctwarn("DEPRECATED call of script_run() in $filename:$line " .
+                      'add `die_on_timeout => ?` to avoid this warning');
+            }
+        }
+    }
+    return $ret;
 }
 
 =head2 background_script_run
@@ -2218,7 +2239,7 @@ sub upload_logs {
     $cmd .= autoinst_url("/uploadlog/$basename");
     if ($failok) {
         # just use script_run so we don't care if the upload fails
-        script_run($cmd, $timeout);
+        script_run($cmd, $timeout, die_on_timeout => 1);
     }
     else {
         assert_script_run($cmd, $timeout);
