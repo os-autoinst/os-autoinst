007c183757d08dc1288a9aa053e0360764bf2e82
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2021-12-25 05:40:37 +0100

    Extract base class video_base for vnc_base+video_stream
    This extracts common functionality (mostly related to keyboard/mouse
    handling) into a new base class, to avoid code duplication.
    No functional change.

--- consoles/vnc_base.pm
+++ consoles/vnc_base.pm
@@ -4,14 +4,14 @@
 
 package consoles::vnc_base;
 
-use Mojo::Base 'consoles::network_console', -signatures;
+use Mojo::Base 'consoles::video_base', -signatures;
 use consoles::VNC;
 use Time::HiRes qw(usleep);
 
 use bmwqemu ();
 
 # speed limit: 30 keys per second
-use constant VNC_TYPING_LIMIT_DEFAULT => 30;
+use constant VNC_TYPING_LIMIT_DEFAULT => consoles::video_base::TYPING_LIMIT_DEFAULT;
 
 sub screen ($self) { $self }
 
@@ -67,53 +67,8 @@ sub current_screen ($self) {
 
 sub _typing_limit () { $bmwqemu::vars{VNC_TYPING_LIMIT} // VNC_TYPING_LIMIT_DEFAULT || 1 }
 
-sub type_string ($self, $args) {
-    my $seconds_per_keypress = 1 / _typing_limit;
-
-    # further slow down if being asked for.
-    # 250 = magic default from testapi.pm
-
-    # Note: the intended use of max_interval is the bootloader. The bootloader
-    # prompt drops characters when typing quickly. This problem mostly occurs
-    # in the bootloader. Humans notice because they look at the screen while
-    # typing. So this loop should be replaced by some true 'looking at the
-    # screen while typing', e.g. waiting for no more screen updates 'in the
-    # right area'.  For now, just waiting is good enough: The slow-down only
-    # affects the bootloader sequence.
-    if (($args->{max_interval} // 250) < 250) {
-        # according to 	  git grep "type_string.*, *[0-9]"  on
-        #   https://github.com/os-autoinst/os-autoinst-distri-opensuse,
-        # typical max_interval values are
-        #   4ish:  veeery slow
-        #   15ish: slow
-        $seconds_per_keypress = $seconds_per_keypress + 1 / sqrt($args->{max_interval});
-    }
-
-    for my $letter (split("", $args->{text})) {
-        next if ($letter eq "\r");
-        my $charmap = {
-            "-" => 'minus',
-            "\t" => 'tab',
-            "\n" => 'ret',
-            "\b" => 'backspace',
-            "\e" => 'esc'
-        };
-        $letter = $charmap->{$letter} || $letter;
-        # 25% is spent hitting the key, 25% releasing it, 50% searching the next key
-        $self->{vnc}->map_and_send_key($letter, undef, $seconds_per_keypress * 0.25);
-        $self->{backend}->run_capture_loop($seconds_per_keypress * 0.5);
-    }
-    return {};
-}
-
-sub send_key ($self, $args) {
-    # send_key rate must be limited to take into account VNC_TYPING_LIMIT- poo#55703
-    # map_and_send_key: do not be faster than default
-    my $press_release_delay = 1 / _typing_limit;
-
-    $self->{vnc}->map_and_send_key($args->{key}, undef, $press_release_delay);
-    $self->backend->run_capture_loop(.2);
-    return {};
+sub send_key_event ($self, $key, $press_release_delay) {
+    $self->{vnc}->map_and_send_key($key, undef, $press_release_delay);
 }
 
 sub hold_key ($self, $args) {
@@ -128,49 +83,15 @@ sub release_key ($self, $args) {
     return {};
 }
 
-sub _mouse_move ($self, $x, $y) {
-    die "need parameter \$x and \$y" unless (defined $x and defined $y);
+sub mouse_width ($self) { return $self->{vnc}->width; }
+sub mouse_height ($self) { return $self->{vnc}->height; }
 
-    if ($self->{mouse}->{x} == $x && $self->{mouse}->{y} == $y) {
-        # in case the mouse is moved twice to the same position
-        # (e.g. in case of duplicated mouse_hide), we need to wiggle the
-        # mouse a bit to avoid qemu ignoring the repositioning
-        # because the SUT might have moved the mouse itself and we
-        # need to make sure the mouse is really where expected
-        my $delta = 5;
-        # move it to the left in case the mouse is right
-        $delta = -5 if $x > $self->{vnc}->width / 2;
-        $self->{vnc}->mouse_move_to($x + $delta, $y + $delta);
-    }
-
-    $self->{mouse}->{x} = $x;
-    $self->{mouse}->{y} = $y;
-
-    bmwqemu::diag "mouse_move $x, $y";
+sub mouse_move_to ($self, $x, $y) {
     $self->{vnc}->mouse_move_to($x, $y);
-    return;
 }
 
-sub mouse_hide ($self, $args) {
-    $args->{border_offset} //= 0;
-
-    my $x = $self->{vnc}->width - 1;
-    my $y = $self->{vnc}->height - 1;
-
-    if (defined $args->{border_offset}) {
-        my $border_offset = int($args->{border_offset});
-        $x -= $border_offset;
-        $y -= $border_offset;
-    }
-
-    $self->_mouse_move($x, $y);
-    return {absolute => $self->{vnc}->absolute};
-}
-
-sub mouse_set ($self, $args) {
-    die "Need x/y arguments" unless (defined $args->{x} && defined $args->{y});
-    $self->_mouse_move(int($args->{x}), int($args->{y}));
-    return {};
+sub mouse_absolute ($self) {
+    $self->{vnc}->absolute;
 }
 
 sub mouse_button ($self, $args) {
