os-autoinst/os-autoinst

commit 54d514df79fa18fdcdd7aac8ec2cb2ce5744221b

Prevent use of external facing API in consoles as well
Same as for the backend.

consoles/sshVirtsh.pm
@@ -14,7 +14,6 @@ use Class::Accessor 'antlers';
 use Mojo::JSON qw(decode_json);
 
 use backend::svirt;
-use testapi qw(get_var get_required_var check_var set_var);
 
 has instance => (is => "rw", isa => "Num");
 has name => (is => "rw", isa => "Str");
@@ -24,11 +23,11 @@ has vmm_type => (is => "rw", isa => "Str");
 sub new ($class, $testapi_console = undef, $args = {}) {
     my $self = $class->SUPER::new($testapi_console, $args);
 
-    $self->instance(get_var('VIRSH_INSTANCE', 1));
+    $self->instance($bmwqemu::vars{VIRSH_INSTANCE} // 1);
     # default name
     $self->name("openQA-SUT-" . $self->instance);
-    $self->vmm_family(get_var('VIRSH_VMM_FAMILY', 'kvm'));
-    $self->vmm_type(get_var('VIRSH_VMM_TYPE', 'hvm'));
+    $self->vmm_family($bmwqemu::vars{VIRSH_VMM_FAMILY} // 'kvm');
+    $self->vmm_type($bmwqemu::vars{VIRSH_VMM_TYPE} // 'hvm');
 
     return $self;
 }
@@ -58,8 +57,8 @@ sub _init_ssh ($self, $args) {
     if ($self->vmm_family eq 'vmware') {
         $self->{ssh_credentials}->{sshVMwareServer} =
           {
-            hostname => get_required_var('VMWARE_HOST'),
-            password => get_required_var('VMWARE_PASSWORD'),
+            hostname => $bmwqemu::vars{VMWARE_HOST} || die('Need variable VMWARE_HOST'),
+            password => $bmwqemu::vars{VMWARE_PASSWORD} || die('Need variable VMWARE_PASSWORD'),
             username => 'root',
           };
     }
@@ -85,19 +84,19 @@ sub _init_xml ($self, $args = {}) {
     $elem->appendTextNode($self->name);
     $root->appendChild($elem);
 
-    my $openqa_hostname = get_var('OPENQA_HOSTNAME', 'no-webui-set');
+    my $openqa_hostname = $bmwqemu::vars{OPENQA_HOSTNAME} // 'no-webui-set';
     $elem = $doc->createElement('description');
     $elem->appendTextNode("openQA WebUI: $openqa_hostname ($instance): ");
-    $elem->appendTextNode(get_var('NAME', '0-no-scenario'));
+    $elem->appendTextNode($bmwqemu::vars{NAME} // '0-no-scenario');
     $root->appendChild($elem);
 
     $elem = $doc->createElement('memory');
-    $elem->appendTextNode(get_required_var('QEMURAM'));
+    $elem->appendTextNode($bmwqemu::vars{QEMURAM} or die 'Need variable QEMURAM');
     $elem->setAttribute(unit => 'MiB');
     $root->appendChild($elem);
 
     $elem = $doc->createElement('vcpu');
-    $elem->appendTextNode(get_required_var('QEMUCPUS'));
+    $elem->appendTextNode($bmwqemu::vars{QEMUCPUS} or die 'Need variable QEMUCPUS');
     $root->appendChild($elem);
 
     my $os = $doc->createElement('os');
@@ -134,19 +133,19 @@ sub _init_xml ($self, $args = {}) {
         $root->appendChild($elem);
     }
 
-    if (get_var('UEFI') and check_var('ARCH', 'x86_64') and !get_var('BIOS') and !check_var('VIRSH_VMM_FAMILY', 'hyperv')) {
+    if ($bmwqemu::vars{UEFI} and $bmwqemu::vars{ARCH} eq 'x86_64' and !$bmwqemu::vars{BIOS} and !$bmwqemu::vars{VIRSH_VMM_FAMILY} eq 'hyperv') {
         foreach my $firmware (@bmwqemu::ovmf_locations) {
             if (!$self->run_cmd("test -e $firmware")) {
-                set_var('BIOS', $firmware);
+                $bmwqemu::vars{BIOS} = $firmware;
                 $elem = $doc->createElement('loader');
                 $elem->appendTextNode($firmware);
                 $os->appendChild($elem);
                 last;
             }
         }
-        if (!get_var('BIOS')) {
+        if (!$bmwqemu::vars{BIOS}) {
             # We know this won't go well.
-            my $virsh_hostname = get_var('VIRSH_HOSTNAME', '');
+            my $virsh_hostname = $bmwqemu::vars{VIRSH_HOSTNAME} // '';
             die "No UEFI firmware can be found on hypervisor '$virsh_hostname'. Please specify BIOS or UEFI_BIOS or install an appropriate package.";
         }
     }
@@ -224,7 +223,7 @@ sub add_pty ($self, $args) {
         my $elem = $doc->createElement('source');
         $elem->setAttribute(mode => 'bind');
         $elem->setAttribute(host => '0.0.0.0');
-        $elem->setAttribute(service => get_var('VMWARE_SERIAL_PORT'));
+        $elem->setAttribute(service => $bmwqemu::vars{VMWARE_SERIAL_PORT});
         $console->appendChild($elem);
     }
 
@@ -330,7 +329,7 @@ sub _copy_image_vmware ($self, $name, $backingfile, $file_basename, $vmware_open
     # If the file exists, make sure someone else is not copying it there right now,
     # otherwise copy image from NFS datastore.
     my $nfs_dir = $backingfile ? 'hdd' : 'iso';
-    my $vmware_nfs_datastore = get_required_var('VMWARE_NFS_DATASTORE');
+    my $vmware_nfs_datastore = $bmwqemu::vars{VMWARE_NFS_DATASTORE} or die 'Need variable VMWARE_NFS_DATASTORE';
     my $cmd =
       "if test -e $vmware_openqa_datastore$file_basename; then " .
       "while lsof | grep 'cp.*$file_basename'; do " .
@@ -432,7 +431,7 @@ sub add_disk ($self, $args) {
     my $name = $self->name;
     my $file = $name . $args->{dev_id} . ($self->vmm_family eq 'vmware' ? '.vmdk' : '.img');
     my $basedir = '/var/lib/libvirt/images/';
-    my $vmware_datastore = get_var('VMWARE_DATASTORE', '');
+    my $vmware_datastore = $bmwqemu::vars{VMWARE_DATASTORE} // '';
     my $vmware_openqa_datastore = "/vmfs/volumes/$vmware_datastore/openQA/";
     if ($args->{create}) {
         $file = $self->_create_disk($args, $vmware_openqa_datastore, $file, $name, $basedir);
@@ -468,7 +467,7 @@ sub add_disk ($self, $args) {
 
 sub virsh () {
     my $virsh = 'virsh';
-    $virsh .= ' ' . get_var('VMWARE_REMOTE_VMM') if get_var('VMWARE_REMOTE_VMM');
+    $virsh .= ' ' . $bmwqemu::vars{VMWARE_REMOTE_VMM} if $bmwqemu::vars{VMWARE_REMOTE_VMM};
     return $virsh;
 }
 
@@ -482,7 +481,7 @@ sub resume ($self) {
     bmwqemu::diag "VM " . $self->name . " resumed";
 }
 
-sub get_remote_vmm ($self) { get_var('VMWARE_REMOTE_VMM', '') }
+sub get_remote_vmm ($self) { $bmwqemu::vars{VMWARE_REMOTE_VMM} // '' }
 
 sub define_and_start ($self) {
     my $remote_vmm = "";
@@ -494,16 +493,16 @@ sub define_and_start ($self) {
         $self->run_cmd(
             "cat > $libvirtauthfilename <<__END
 [credentials-vmware]
-username=" . get_required_var('VMWARE_USERNAME') . "
-password=" . get_required_var('VMWARE_PASSWORD') . "
-[auth-esx-" . get_required_var('VMWARE_HOST') . "]
+username=" . $bmwqemu::vars{VMWARE_USERNAME} or die 'Need variable VMWARE_USERNAME' . "
+password=" . $bmwqemu::vars{VMWARE_PASSWORD} or die 'Need variable VMWARE_PASSWORD' . "
+[auth-esx-" . $bmwqemu::vars{VMWARE_HOST} or die 'Need variable VMWARE_HOST' . "]
 credentials=vmware
 __END"
         );
-        my $user = get_required_var('VMWARE_USERNAME');
-        my $host = get_required_var('VMWARE_HOST');
+        my $user = $bmwqemu::vars{VMWARE_USERNAME} or die 'Need variable VMWARE_USERNAME';
+        my $host = $bmwqemu::vars{VMWARE_HOST} or die 'Need variable VMWARE_HOST';
         $remote_vmm = "-c esx://$user\@$host/?no_verify=1\\&authfile=$libvirtauthfilename ";
-        set_var('VMWARE_REMOTE_VMM', $remote_vmm);
+        $bmwqemu::vars{VMWARE_REMOTE_VMM} = $remote_vmm;
     }
 
     my $instance = $self->instance;
@@ -548,7 +547,7 @@ sub attach_to_running ($self, $args = undef) {
 
     # Setting SVIRT_KEEP_VM_RUNNING variable prevents destruction of a perhaps valuable VM
     # outside of openQA. Set 'stop_vm' argument should the VM be destroyed at the end.
-    set_var('SVIRT_KEEP_VM_RUNNING', 1) unless $args->{stop_vm};
+    $bmwqemu::vars{SVIRT_KEEP_VM_RUNNING} = 1 unless $args->{stop_vm};
 }
 
 sub start_serial_grab ($self) { $self->backend->start_serial_grab($self->name) }

consoles/VNC.pm
@@ -10,7 +10,6 @@ use IO::Socket::INET;
 use bmwqemu qw(diag fctwarn);
 use Time::HiRes qw( sleep gettimeofday time );
 use List::Util 'min';
-use testapi 'get_var';
 use Crypt::DES;
 use Compress::Raw::Zlib;
 use Carp qw(confess cluck carp croak);
@@ -819,7 +818,7 @@ sub _send_frame_buffer {
 sub send_update_request {
     my ($self) = @_;
 
-    my $time_after_vnc_is_considered_stalled = get_var('VNC_STALL_THRESHOLD', 4);
+    my $time_after_vnc_is_considered_stalled = $bmwqemu::vars{VNC_STALL_THRESHOLD} // 4;
     # after 2 seconds: send forced update
     # after 4 seconds: turn off screen
     my $time_since_last_update = time - $self->_last_update_received;

consoles/console.pm
@@ -18,7 +18,6 @@ package consoles::console;
 
 use Mojo::Base -strict, -signatures;
 use autodie ':all';
-use testapi 'check_var';
 
 require IPC::System::Simple;
 use Class::Accessor 'antlers';
@@ -37,7 +36,7 @@ sub new ($class, $testapi_console, $args) {
 sub init ($self) {
     # Special keys like Ctrl-Alt-Fx are not passed to the VM by xfreerdp.
     # That means switch from graphical to console is not possible on Hyper-V.
-    $self->{console_hotkey} = check_var('VIRSH_VMM_FAMILY', 'hyperv') ? 'alt-f' : 'ctrl-alt-f';
+    $self->{console_hotkey} = ($bmwqemu::vars{VIRSH_VMM_FAMILY} // '') eq 'hyperv' ? 'alt-f' : 'ctrl-alt-f';
 }
 
 # SUT was e.g. rebooted

consoles/localXvnc.pm
@@ -15,7 +15,6 @@ use Socket;
 use File::Path 'mkpath';
 use File::Which;
 use Time::Seconds;
-use testapi qw(get_var);
 
 # helper function
 # Keep ssh session for the maximum of ServerAliveCountMax x ServerAliveInterval seconds
@@ -24,8 +23,8 @@ use testapi qw(get_var);
 # long-time run test without these options. TCPKeepAlive ensures that if network goes down
 # or the remote host dies, machines will be properly noticed
 sub sshCommand ($self, $username, $host, $gui = undef) {
-    my $server_alive_count_max = get_var('_SSH_SERVER_ALIVE_COUNT_MAX', 480);
-    my $server_alive_interval = get_var('_SSH_SERVER_ALIVE_INTERVAL', ONE_MINUTE);
+    my $server_alive_count_max = $bmwqemu::vars{_SSH_SERVER_ALIVE_COUNT_MAX} // 480;
+    my $server_alive_interval = $bmwqemu::vars{_SSH_SERVER_ALIVE_INTERVAL} // ONE_MINUTE;
     my $sshopts = "-o TCPKeepAlive=yes -o ServerAliveCountMax=$server_alive_count_max -o ServerAliveInterval=$server_alive_interval -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o PubkeyAuthentication=no $username\@$host";
     $sshopts = "-X $sshopts" if $gui;
     return "ssh $sshopts; read";

consoles/s3270.pm
@@ -12,7 +12,6 @@ use base 'consoles::localXvnc';
 use Class::Accessor 'antlers';
 use Data::Dumper 'Dumper';
 use Carp qw(confess cluck carp croak);
-use testapi 'get_required_var';
 require IPC::Run;
 use IPC::Run::Debug;    # set IPCRUNDEBUG=data in shell environment for trace
 use Thread::Queue;
@@ -423,9 +422,9 @@ sub new_3270_console ($self) {
 sub activate ($self) {
     $self->SUPER::activate;
 
-    $self->zVM_host(get_required_var("ZVM_HOST"));
-    $self->guest_user(get_required_var("ZVM_GUEST"));
-    $self->guest_login(get_required_var("ZVM_PASSWORD"));
+    $self->zVM_host($bmwqemu::vars{ZVM_HOST} or die 'Need variable ZVM_HOST');
+    $self->guest_user($bmwqemu::vars{ZVM_GUEST} or die 'Need variable ZVM_GUEST');
+    $self->guest_login($bmwqemu::vars{ZVM_PASSWORD} or die 'Need variable ZVM_PASSWORD');
     $self->new_3270_console;
     $self->connect_and_login;
     return;

consoles/sshIucvconn.pm
@@ -11,11 +11,10 @@ use autodie ':all';
 
 use base 'consoles::network_console';
 
-use testapi 'get_var';
 
 sub connect_remote ($self, $args) {
     my $hostname = $args->{hostname};
-    my $zvmguest = get_var('ZVM_GUEST');
+    my $zvmguest = $bmwqemu::vars{ZVM_GUEST};
 
     # ssh connection to SUT for agetty
     my $ttyconn = $self->backend->new_ssh_connection(hostname => $hostname, password => $args->{password}, username => 'root');

consoles/sshVirtshSUT.pm
@@ -7,7 +7,6 @@ use Mojo::Base -strict, -signatures;
 
 use base 'consoles::console';
 
-use testapi 'get_var';
 use backend::svirt qw(SERIAL_TERMINAL_DEFAULT_PORT SERIAL_TERMINAL_DEFAULT_DEVICE);
 use consoles::ssh_screen;
 
@@ -15,7 +14,7 @@ sub new ($class, $testapi_console, $args) {
     my $self = $class->SUPER::new($testapi_console, $args);
 
     # TODO: inherit from consoles::sshVirtsh
-    my $instance = get_var('VIRSH_INSTANCE', 1);
+    my $instance = $bmwqemu::vars{VIRSH_INSTANCE} // 1;
     $self->{libvirt_domain} = $args->{libvirt_domain} // "openQA-SUT-$instance";
     $self->{serial_port_no} = $args->{serial_port_no} // SERIAL_TERMINAL_DEFAULT_PORT;
 

consoles/sshX3270.pm
@@ -8,10 +8,9 @@ use Mojo::Base -strict, -signatures;
 
 use base 'consoles::localXvnc';
 
-use testapi 'get_var';
 
 sub activate ($self) {
-    my $sshcommand = $self->sshCommand('root', get_var("PARMFILE")->{Hostname});
+    my $sshcommand = $self->sshCommand('root', $bmwqemu::vars{PARMFILE}->{Hostname});
     my $display = $self->{backend}->{consoles}->{worker}->{DISPLAY};
     my $sshpassword = $testapi::password;
 

commit 3cf140bf2f97dc95da08b41ceccfab18911e4779

Fix wrong operator precedence introduced by 54d514df79fa1
* The `… or die …` conditions need braces, otherwise the further
  expressions after the `or` will not be executed.
* See https://progress.opensuse.org/issues/10499

consoles/sshVirtsh.pm
@@ -493,9 +493,9 @@ sub define_and_start ($self) {
         $self->run_cmd(
             "cat > $libvirtauthfilename <<__END
 [credentials-vmware]
-username=" . $bmwqemu::vars{VMWARE_USERNAME} or die 'Need variable VMWARE_USERNAME' . "
-password=" . $bmwqemu::vars{VMWARE_PASSWORD} or die 'Need variable VMWARE_PASSWORD' . "
-[auth-esx-" . $bmwqemu::vars{VMWARE_HOST} or die 'Need variable VMWARE_HOST' . "]
+username=" . ($bmwqemu::vars{VMWARE_USERNAME} or die 'Need variable VMWARE_USERNAME') . "
+password=" . ($bmwqemu::vars{VMWARE_PASSWORD} or die 'Need variable VMWARE_PASSWORD') . "
+[auth-esx-" . ($bmwqemu::vars{VMWARE_HOST} or die 'Need variable VMWARE_HOST') . "]
 credentials=vmware
 __END"
         );