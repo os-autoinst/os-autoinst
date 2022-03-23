9bf191585cbaabb36457338af4ee17a67c8f198d
Marius Kittler <mkittler@suse.de>
2022-03-23 12:21:33 +0100

    Remove limit for uploads via the command server
    This is basically a file transfer on the same physical machine so I suppose
    we can remove the upload limit altogether.
    
    See https://progress.opensuse.org/issues/108476

--- commands.pm
+++ commands.pm
@@ -225,7 +225,7 @@ sub run_daemon {
     my ($port, $isotovideo) = @_;
 
     # allow up to 20 GiB for uploads of big hdd images
-    $ENV{MOJO_MAX_MESSAGE_SIZE} //= ($bmwqemu::vars{UPLOAD_MAX_MESSAGE_SIZE_GB} // 20) * 1024**3;
+    $ENV{MOJO_MAX_MESSAGE_SIZE} //= ($bmwqemu::vars{UPLOAD_MAX_MESSAGE_SIZE_GB} // 0) * 1024**3;
     $ENV{MOJO_INACTIVITY_TIMEOUT} //= ($bmwqemu::vars{UPLOAD_INACTIVITY_TIMEOUT} // 300);
 
     # avoid leaking token
