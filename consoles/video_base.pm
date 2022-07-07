os-autoinst/os-autoinst

commit 4e66dcc0b48ff88f520130357b3d2715b4a0daf8

bmwqemu: Hide secrets on log output with log_call()
Parse given parameters for `-masked => <string>` and replace
all occurrence from the given `<string>` with `[masked]` in the
log output.

consoles/video_base.pm
@@ -30,6 +30,8 @@ sub _typing_limit () { $bmwqemu::vars{TYPING_LIMIT} // TYPING_LIMIT_DEFAULT || 1
 sub send_key_event ($key, $press_release_delay) { }
 
 sub type_string ($self, $args) {
+    bmwqemu::log_call(%$args, $args->{secret} ? (-masked => $args->{text}) : ());
+
     my $seconds_per_keypress = 1 / _typing_limit;
 
     # further slow down if being asked for.

bmwqemu.pm
@@ -203,27 +203,49 @@ sub pp (@args) {
     return $value_with_trailing_newline;
 }
 
+# Use special argument `-masked` to hide the given value in log output.
+# It can be specified multiple times and or the value can be a ARRAY_REF or
+# scalar.
 sub log_call (@args) {
     my $fname = (caller(1))[3];
     update_line_number();
+
+    # extract -masked parameter out of argument list
+    my @masked;
+    my @effective_args;
+    while (@args) {
+        my $v = shift @args;
+        if (defined($v) && $v eq '-masked' && @args) {
+            my $mval = shift @args;
+            push @masked, ref($mval) eq 'ARRAY' ? @$mval : $mval;
+        } else {
+            push @effective_args, $v;
+        }
+    }
+
     my $params;
-    if (@args == 1) {
-        $params = pp($args[0]);
+    if (@effective_args == 1) {
+        $params = pp($effective_args[0]);
     }
     else {
         # key/value pairs
         my @result;
-        my $should_hide = grep { ($_ // '') eq 'secret' } @args;
-        while (my ($key, $value) = splice(@args, 0, 2)) {
+        while (my ($key, $value) = splice(@effective_args, 0, 2)) {
             if ($key =~ tr/0-9a-zA-Z_//c) {
                 # only quote if needed
                 $key = pp($key);
             }
-            $value = "[masked]" if ($key eq 'text' && $should_hide);
             push @result, join("=", $key, pp($value));
         }
         $params = join(", ", @result);
     }
+
+    foreach (@masked) {
+        my $mask = pp($_);
+        $mask =~ s/^"(.*)"$/$1/;
+        $params =~ s/\Q$mask\E/[masked]/g;
+    }
+
     log::logger->debug('<<< ' . $fname . "($params)");
     return;
 }

consoles/serial_screen.pm
@@ -69,7 +69,7 @@ consoles.
 sub type_string ($self, $nargs) {
     my $fd = $self->{fd_write};
 
-    bmwqemu::log_call(%$nargs);
+    bmwqemu::log_call(%$nargs, $nargs->{secret} ? (-masked => $nargs->{text}) : ());
 
     my $text = $nargs->{text};
     my $term;

consoles/ssh_screen.pm
@@ -49,7 +49,7 @@ sub do_read ($self, $, %args) {
 }
 
 sub type_string ($self, $nargs) {
-    bmwqemu::log_call(%$nargs);
+    bmwqemu::log_call(%$nargs, $nargs->{secret} ? (-masked => $nargs->{text}) : ());
 
     my $text = $nargs->{text};
     my $terminate_with = $nargs->{terminate_with} // '';

t/12-bmwqemu.t
@@ -63,9 +63,27 @@ subtest 'log_call' => sub {
     stderr_like(\&log_call_indent, qr{\Q<<< main::log_call_indent(test=[\E\n\Q    "a",\E\n\Q    [\E\n\Q      "b"\E\n\Q    ]\E\n\Q  ])}, 'log_call auto indentation');
 
     sub log_call_test_secret {
-        bmwqemu::log_call(text => "passwd\n", secret => 1);
+        my (%args) = @_;
+        # Use @_ instead of %args to keep the order
+        bmwqemu::log_call(@_, ($args{secret} ? (-masked => $args{text}) : ()));
+        return;
     }
-    stderr_like(\&log_call_test_secret, qr{\Q<<< main::log_call_test_secret(text="[masked]", secret=1)}, 'log_call hides sensitive info');
+    stderr_like { log_call_test_secret(text => "password\n", secret => 1) } qr{\Q<<< main::log_call_test_secret(text="[masked]", secret=1)}, 'log_call hides sensitive info';
+    stderr_like { log_call_test_secret(text => "password\n") } qr{\Q<<< main::log_call_test_secret(text="password\n")}, 'log_call hides sensitive info';
+
+    my $do_not_show_me = '$^a{1}\n\\FooBar.';
+    stderr_like { log_call_test_secret(text => $do_not_show_me, psk => $do_not_show_me, -masked => $do_not_show_me) } qr{\Q<<< main::log_call_test_secret(text="[masked]", psk="[masked]")}, 'Hide secrets with special regex chars';
+    stderr_like { log_call_test_secret(text => "$do_not_show_me$do_not_show_me", -masked => $do_not_show_me) } qr{\Q<<< main::log_call_test_secret(text="[masked][masked]")}, 'Hide secrets if it occure multiple times';
+
+    stderr_like { log_call_test_secret(text => "a666b42c", -masked => ['666', '42']) } qr{\Q<<< main::log_call_test_secret(text="a[masked]b[masked]c")}, 'Hide multiple secrets given as array';
+    stderr_like { log_call_test_secret(text => "a666b42c", -masked => '666', -masked => '42') } qr{\Q<<< main::log_call_test_secret(text="a[masked]b[masked]c")}, 'Hide multiple secrets given as multiple arguments';
+    stderr_like { log_call_test_secret(text => "a666b42c5", -masked => ['666', '5'], -masked => '42') } qr{\Q<<< main::log_call_test_secret(text="a[masked]b[masked]c[masked]")}, 'Hide multiple secrets given in mixed format';
+
+    my $super_long_partly_secret_string = "Hallo world my psk is $do_not_show_me";
+    stderr_like { log_call_test_secret(output => $super_long_partly_secret_string, -masked => $do_not_show_me) } qr{\Q<<< main::log_call_test_secret(output="Hallo world my psk is [masked]")}, 'Hide secrets as part of a string';
+
+    stderr_like { log_call_test_secret(value => 0) } qr{\Q<<< main::log_call_test_secret(value=0)}, 'Value evaluate to false';
+    stderr_like { log_call_test_secret(value => undef) } qr{\Q<<< main::log_call_test_secret(value=undef)}, 'Undef as value';
 };
 
 subtest 'update_line_number' => sub {

testapi.pm
@@ -1474,7 +1474,7 @@ sub type_string {
     my $wait_timeout = $args{timeout} // 30;
     my $wait_sim_level = $args{similarity_level} // 47;
     bmwqemu::log_call(string => $string, max_interval => $max_interval, wait_screen_changes => $wait, wait_still_screen => $wait_still,
-        timeout => $wait_timeout, similarity_level => $wait_sim_level);
+        timeout => $wait_timeout, similarity_level => $wait_sim_level, $args{secret} ? (-masked => $string) : ());
     my @pieces;
     if ($wait) {
         # split string into an array of pieces of specified size

commit 04e671b66796e249d5897bc67786b512267bc6b0

Prevent passwords leaked to log by redundant log-message
Commit 4e66dcc0 introduced duplicate log messages with leaked passwords
like this:

```
susedistribution::handle_password_prompt -> lib/susedistribution.pm:70 called testapi::type_password
[2022-07-06T12:06:05.534656+02:00] [debug] <<< testapi::type_string(string="[masked]", max_interval=100, wait_screen_change=0, wait_still_screen=0, timeout=30, similarity_level=47)
[2022-07-06T12:06:05.535894+02:00] [debug] <<< consoles::video_base::type_string(max_interval=100, cmd="backend_type_string", json_cmd_token="HWQIViIt", text="nots3cr3t")
```

We should not need the second log message at all and also it's not
properly handling the password so I am suggesting to simply remove it
completely.

Related progress issue: https://progress.opensuse.org/issues/113312

consoles/video_base.pm
@@ -30,8 +30,6 @@ sub _typing_limit () { $bmwqemu::vars{TYPING_LIMIT} // TYPING_LIMIT_DEFAULT || 1
 sub send_key_event ($key, $press_release_delay) { }
 
 sub type_string ($self, $args) {
-    bmwqemu::log_call(%$args, $args->{secret} ? (-masked => $args->{text}) : ());
-
     my $seconds_per_keypress = 1 / _typing_limit;
 
     # further slow down if being asked for.