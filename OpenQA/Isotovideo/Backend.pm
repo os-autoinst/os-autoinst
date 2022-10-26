f73f10dd395b9c39ba1a0352e7e5a57d7e270f19
Cris Dywan <cdywan@suse.de>
2022-10-26 15:00:01 +0200

    Move backend process logic into new class
    See: https://progress.opensuse.org/issues/81899

---
+++ OpenQA/Isotovideo/Backend.pm
@@ -0,0 +1,33 @@
+# Copyright SUSE LLC
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+package OpenQA::Isotovideo::Backend;
+use Mojo::Base -base, -signatures;
+
+use Mojo::File qw(path);
+use backend::driver;
+use bmwqemu;
+use log qw(diag);
+
+sub new ($class, @args) {
+    $bmwqemu::vars{BACKEND} ||= "qemu";
+    $bmwqemu::backend = backend::driver->new($bmwqemu::vars{BACKEND});
+
+    path('os-autoinst.pid')->spurt("$$");
+    # might throw an exception
+    $bmwqemu::backend->start_vm;
+    $class->SUPER::new(@args);
+}
+
+sub process ($self) {
+    $bmwqemu::backend->{backend_process};
+}
+
+sub stop ($self) {
+    return undef unless my $process = $self->process;
+    diag('stopping backend process ' . $process->pid);
+    $process->stop if $process->is_running;
+    diag('done with backend process');
+}
+
+1;
