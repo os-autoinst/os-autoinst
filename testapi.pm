0e666b709fc0c0c61ef2fb3d9a95ae69a0e7e5fe
Oliver Kurz <okurz@suse.de>
2022-11-14 15:39:55 +0100

    testapi: Try to return sane default in non-qemu host_ip calls
    In case of non-qemu tests rely on host_ip, e.g. for log uploading. For
    this so far a valid WORKER_HOSTNAME would need to be provided. We can
    provide a sane default what is supplied by "hostname -f" which should
    return a valid FQDN but can still be overridden with the variable
    WORKER_HOSTNAME as necessary.
    
    Related progress issue: https://progress.opensuse.org/issues/120261

--- testapi.pm
+++ testapi.pm
@@ -2091,12 +2091,15 @@ sub diag (@args) { bmwqemu::diag(@args) }
 Return VM's host IP.
 In a kvm instance you reach the VM's host under default 10.0.2.2
 
-Optional named parameter C<inside_sut> in C<$args> will force using actual worker IP/hostname
-even on KVM guests if set to 0.
+Optional named parameter C<inside_sut> in C<$args> will force using actual
+worker IP/hostname even on KVM guests if set to 0.
+
+In case of non-qemu tries to return a sane default if the test variable
+C<WORKER_HOSTNAME> was not specified.
 
 =cut
 
-sub host_ip ($args = {}) { (check_var('BACKEND', 'qemu') && ($args->{inside_sut} // 1)) ? get_var('QEMU_HOST_IP', '10.0.2.2') : get_required_var('WORKER_HOSTNAME') }
+sub host_ip ($args = {}) { (check_var('BACKEND', 'qemu') && ($args->{inside_sut} // 1)) ? get_var('QEMU_HOST_IP', '10.0.2.2') : ($bmwqemu::vars{WORKER_HOSTNAME} //= qx{hostname -f}) }
 
 =head2 autoinst_url
 
