dabbd7f737bf860e1d2fd76253d0ebef0ebe2ecf
Bala Raman <srbala@gmail.com>
2023-01-24 11:21:10 -0500

    Fallback check for RHEL variants of qemu-kvm
    Signed-off-by: Bala Raman <srbala@gmail.com>

--- backend/qemu.pm
+++ backend/qemu.pm
@@ -610,6 +610,9 @@ sub start_qemu ($self) {
         }
     }
 
+# fallback check for RHEL variants of qemu-kvm
+    $qemubin = find_bin('/usr/libexec/', qw(kvm qemu-kvm)) unless $qemubin;
+
     die "no kvm-img/qemu-img found\n" unless $qemuimg;
     die "no Qemu/KVM found\n" unless $qemubin;
 
