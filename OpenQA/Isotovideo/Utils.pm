os-autoinst/os-autoinst

commit ee31682ef901214aabfaea3852692f78d10dc1a2

Enable assets collection for generalhw backend too
Since there are tests using PUBLISH_HDD_1 var even if the backend does
not support it directly (but the test itself implements relevant
support), enable assets collection for 'generalhw' or 'qemu' backends
only. For now.

See details at https://progress.opensuse.org/issues/104499

OpenQA/Isotovideo/Utils.pm
@@ -128,7 +128,7 @@ configuration variables.
 sub handle_generated_assets ($command_handler, $clean_shutdown) {
     my $return_code = 0;
     # mark hard disks for upload if test finished
-    return unless $bmwqemu::vars{BACKEND} eq 'qemu';
+    return unless $bmwqemu::vars{BACKEND} =~ m/^(qemu|generalhw)$/;
     my @toextract;
     my $nd = $bmwqemu::vars{NUMDISKS};
     if ($command_handler->test_completed) {

commit c4ba80154be3f5bf11a9fcf319c6ce7cfcdbd356

Isotovideo/Utils.pm: Fix undefined NUMDISKS for generalhw
NUMDISKS variable is total number of disks in QEMU, thus not defined in
generalhw, which leads to error Use of uninitialized value $nd in
foreach loop entry at /usr/lib/os-autoinst/OpenQA/Isotovideo/Utils.pm
line 223.

Fixes: ee31682e ("Enable assets collection for generalhw backend too")
Signed-off-by: Petr Vorel <pvorel@suse.cz>

OpenQA/Isotovideo/Utils.pm
@@ -197,7 +197,7 @@ sub handle_generated_assets ($command_handler, $clean_shutdown) {
     # mark hard disks for upload if test finished
     return unless $bmwqemu::vars{BACKEND} =~ m/^(qemu|generalhw)$/;
     my @toextract;
-    my $nd = $bmwqemu::vars{NUMDISKS};
+    my $nd = $bmwqemu::vars{NUMDISKS} // 1;
     if ($command_handler->test_completed) {
         for my $i (1 .. $nd) {
             my $dir = 'assets_private';