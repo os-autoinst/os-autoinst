os-autoinst/os-autoinst

commit 02c09b3b2d56a57e85cb161f79292044f2cd09aa

backend/svirt: Add constants for serial console for user (sut-serial)
This will be used in os-autoinst-distri-opensuse.git.

Implements: poo#131258
Signed-off-by: Petr Vorel <pvorel@suse.cz>

backend/svirt.pm
@@ -13,11 +13,16 @@ use bmwqemu;
 use constant SERIAL_CONSOLE_DEFAULT_PORT => 0;
 use constant SERIAL_CONSOLE_DEFAULT_DEVICE => 'console';
 
+# root-sut-serial (console for root login)
 use constant SERIAL_TERMINAL_DEFAULT_PORT => 1;
 use constant SERIAL_TERMINAL_DEFAULT_DEVICE => 'console';
 
+# sut-serial (console for user login)
+use constant SERIAL_USER_TERMINAL_DEFAULT_PORT => 2;
+use constant SERIAL_USER_TERMINAL_DEFAULT_DEVICE => 'console';
+
 use Exporter 'import';
-our @EXPORT_OK = qw(SERIAL_CONSOLE_DEFAULT_PORT SERIAL_CONSOLE_DEFAULT_DEVICE SERIAL_TERMINAL_DEFAULT_PORT SERIAL_TERMINAL_DEFAULT_DEVICE);
+our @EXPORT_OK = qw(SERIAL_CONSOLE_DEFAULT_PORT SERIAL_CONSOLE_DEFAULT_DEVICE SERIAL_TERMINAL_DEFAULT_PORT SERIAL_TERMINAL_DEFAULT_DEVICE SERIAL_USER_TERMINAL_DEFAULT_PORT SERIAL_USER_TERMINAL_DEFAULT_DEVICE);
 
 # this is a fake backend to some extend. We don't start VMs, but provide ssh access
 # to a libvirt running host (KVM for System Z in mind)

commit eedefdb1144f2301646df9a5b5efba39e7c3bab0

svirt: Fix comments for ssh-virtsh-serial constants
ssh-virtsh-serial for user was originally supposed to be 'sut-serial',
but to follow the conventions it will be 'user-sut-serial'.

Fixes: 02c09b3b ("backend/svirt: Add constants for serial console for user (sut-serial)")
Signed-off-by: Petr Vorel <pvorel@suse.cz>

backend/svirt.pm
@@ -13,11 +13,11 @@ use bmwqemu;
 use constant SERIAL_CONSOLE_DEFAULT_PORT => 0;
 use constant SERIAL_CONSOLE_DEFAULT_DEVICE => 'console';
 
-# root-sut-serial (console for root login)
+# root-sut-serial (ssh-virtsh-serial for root)
 use constant SERIAL_TERMINAL_DEFAULT_PORT => 1;
 use constant SERIAL_TERMINAL_DEFAULT_DEVICE => 'console';
 
-# sut-serial (console for user login)
+# user-sut-serial (ssh-virtsh-serial for user)
 use constant SERIAL_USER_TERMINAL_DEFAULT_PORT => 2;
 use constant SERIAL_USER_TERMINAL_DEFAULT_DEVICE => 'console';
 