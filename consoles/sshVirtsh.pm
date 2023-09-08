193cf8609c3c3937bfabb9f5a09708695986efba
Marius Kittler <mkittler@suse.de>
2023-09-08 14:33:24 +0200

    Remove wrong condition for `unxz` call on svirt host
    The check for the existence of the unextracted asset is done one
    the wrong host. The check was supposedly meant to be an
    optimization but it never worked. So it is better to remove it
    completely.
    
    This condition would have also prevented the extraction in case
    an unextracted version was present on the worker host.
    
    This commit is the same as 9defb86e4f1fa49fa15835fc3435abb3bafe967e
    which was needlessly reverted.

--- consoles/sshVirtsh.pm
+++ consoles/sshVirtsh.pm
@@ -354,7 +354,7 @@ sub _copy_image_vmware ($self, $name, $backingfile, $file_basename, $vmware_open
 sub _copy_image_else ($self, $file, $file_basename, $basedir) {
     $self->run_cmd(sprintf("rsync -av '$file' '$basedir/%s'", $file_basename)) && die 'rsync failed';
     if ($file_basename =~ /(.*)\.xz$/) {
-        $self->run_cmd(sprintf("nice ionice unxz -f -k '$basedir/%s'", $file_basename)) unless -e "$basedir$1";
+        $self->run_cmd("nice ionice unxz -f -k '$basedir/$file_basename'");
         $file_basename = $1;
     }
 }
