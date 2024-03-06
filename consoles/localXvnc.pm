40afe0cd631ff6ffdfed150c71fed6fae7751a85
Martin Doucha <mdoucha@suse.cz>
2024-03-06 14:49:44 +0100

    localXvnc: Open hardware console log in append mode
    If a test runs with non-persistent IPMI SOL console, the hardware
    console log will be wiped on every system reboot. Use append mode
    to keep console log from the whole test run.

--- consoles/localXvnc.pm
+++ consoles/localXvnc.pm
@@ -37,7 +37,7 @@ sub callxterm ($self, $command, $window_name) {
     die('Missing "xterm"') unless which('xterm');
     if ($self->{args}->{log}) {
         mkpath 'ulogs';
-        $command = "script -f ulogs/hardware-console-log.txt -c \"$command\"";
+        $command = "script -af ulogs/hardware-console-log.txt -c \"$command\"";
     }
     eval { system("DISPLAY=$display $xterm_vt_cmd -title $window_name -e bash -c '$command' & echo \"xterm PID is \$!\""); };
     die "cant' start xterm on $display (err: $! retval: $?)" if $@;
