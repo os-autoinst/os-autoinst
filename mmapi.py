e5eda2252f34e503c28539546ed4e5b4335b03c8
Aske Bækdal Møller <abm@insero.com>
2024-06-24 12:14:03 +0200

    Refactor: generate python bindings from template
    Instead of copying code use a template to generate the python bindings

--- mmapi.py
+++
@@ -1,7 +0,0 @@
-# publish all mm API methods over perl into the modules context.
-# Use with `import mmapi; mmapi.method()` or `from mmapi import *`
-import perl
-
-perl.use("mmapi")
-for i in dir(perl.mmapi):
-    locals()[i] = getattr(perl.mmapi, i)
