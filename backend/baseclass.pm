56dcea93cf0b4e4fbe9d2d7e997c2894be1194e2
Oliver Kurz <okurz@suse.de>
2025-08-14 16:21:27 +0200

    baseclass: Fix missing error details on failed SSH connection attempts
    Already since in 631d0f7a the SSHConnectionError->throw was introduced
    there was no useful content available for the error variable "$@" so
    this variable can not have useful content causing error messages as
    observed like
    
    ```
    Error connecting to root@my.machine:  at …
    ```
    
    mind the double space after ":". This commit changes the call to use
    "die_with_error". The message for the above example then will look like:
    
    ```
    Error connecting to <root@my.machine>: All
    authentication methods failed (-18 LIBSSH2_ERROR_AUTHENTICATION_FAILED)
    ```

--- backend/baseclass.pm
+++ backend/baseclass.pm
@@ -1241,7 +1241,7 @@ sub new_ssh_connection ($self, %args) {
         bmwqemu::diag "SSH connection to $con_pretty established" if $ssh->auth_ok;
         last;
     }
-    OpenQA::Exception::SSHConnectionError->throw(error => "Error connecting to <$con_pretty>: $@") unless $ssh->auth_ok;
+    $ssh->die_with_error("Error connecting to <$con_pretty>") unless $ssh->auth_ok;
 
     $self->{ssh_connections}->{$connection_key} = $ssh if ($args{keep_open});
     return $ssh;
