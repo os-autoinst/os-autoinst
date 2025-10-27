8c98e4b93e265819b639c62cdd9ce7bd08982738
Marius Kittler <mkittler@suse.de>
2025-10-27 15:11:49 +0100

    Handle OpenCV exceptions when writing an image
    Handle exceptions like it is described in the documentation of `imwrite()`.

--- ppmclibs/tinycv_impl.cc
+++ ppmclibs/tinycv_impl.cc
@@ -406,11 +406,16 @@ Image* image_from_ppm(const unsigned char* data, size_t len)
 
 bool image_write(const Image* const s, const char* filename)
 {
-    if (s->img.empty()) {
-        std::cerr << "Could not write image " << filename << ": image is empty\n";
+    try {
+        if (s->img.empty()) {
+            CV_Error(0, "image is empty");
+        }
+        return imwrite(filename, s->img);
+    }
+    catch (const cv::Exception& ex) {
+        std::cerr << "Could not write image " << filename << ": " << ex.err << '\n';
         return false;
     }
-    return imwrite(filename, s->img);
 }
 
 std::vector<uchar>* image_ppm(Image* s)
